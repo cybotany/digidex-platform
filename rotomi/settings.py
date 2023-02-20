211f5ee037d191075cedeff4f00c3cd36c148d52
Rafael <puello322@gmail.com>
2023-02-20 16:42:22 -0700

    Actual settings fixes

--- rotomi/settings.py
+++ rotomi/settings.py
@@ -19,6 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
+DEBUG = True
+
 ALLOWED_HOSTS = []
 
 
