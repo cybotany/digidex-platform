e165f8b5078b64ff1aed36a8b96c0b4a78132ced
Rafael <puello322@gmail.com>
2023-02-20 16:34:45 -0700

    Updating settings file.

--- rotomi/settings.py
+++ rotomi/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ebd_z7&am3hdu&eu4df13+l74iw(&@z56yf8rz+oghyw(rhw07'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
@@ -121,3 +115,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from private_settings import *
+except ImportError:
+    print("Error: make a local version of private_sestings.py from the template.")
