718940e0e039ac2baef4ad330932103647484f88
Rafael <puello322@gmail.com>
2023-02-26 16:01:37 -0700

    Some changes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+import pytest
 
 browser = webdriver.Firefox()
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+# User navigates to the homepage of the rotomi-app
 browser.get('http://localhost:8000')
 
+# User notices the page title and header mention rotomi
 assert 'Django' in browser.title
+
+# User is prompted to login
