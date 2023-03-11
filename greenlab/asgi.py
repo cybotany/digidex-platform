a6cd284ecdf5ea8e56cefe56c221d67dfe196c01
Raphael <rafael.e.puello@gmail.com>
2023-03-11 11:43:52 -0700

    Making more changes .

--- greenlab/asgi.py
+++ greenlab/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for rotomi project.
+ASGI config for greenlab project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rotomi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenlab.settings')
 
 application = get_asgi_application()
--- greenlab/wsgi.py
+++ greenlab/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rotomi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenlab.settings')
 
 application = get_wsgi_application()
