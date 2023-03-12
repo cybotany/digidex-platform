1fd91fcd2aa36a6f3d14b266617e33828770cef7
Raphael <puello322@gmail.com>
2023-03-12 15:49:23 -0600

    Adding pytest fixtures

--- functional_tests.py
+++ functional_tests.py
@@ -2,36 +2,54 @@ from selenium import webdriver
 from decouple import config
 import pytest
 
-browser = webdriver.Firefox()
-website = config("WEBSITE_URL")
+class TestUser:
+    def __init__(self, browser):
+        self.browser = browser
+    
+    def browser_navigation(self, website):
+        self.browser.get(config("WEBSITE_URL"))
 
-# User purchased a GL-NFC tag and scans it to register
-# it. After scanning, a web-app launches.
-browser.get(website)
+    def quit_browser(self):
+        self.browser.quit()
 
-# The user notices the page title and header mention cataloging
-# their plant.
-assert 'Plant Catalog' in browser.title
+@pytest.fixture
+def firefox_user():
+    return TestUser(webdriver.Firefox())
 
-# She is invited to enter a to-do item straight away
+def main():
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures)
+    # User purchased a GL-NFC tag and scans it to register
+    # it. After scanning, a web-app launches.
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+    def test_page_launch(firefox_user):
+        test_ff_user = TestUser(firefox_user)
+        test_ff_user.browser_navigation()
+        test_ff_user.quit()
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    # The user notices the page title and header mention cataloging
+    # their plant.
+    #assert 'Plant Catalog' in browser.title
 
-# The page updates again, and now shows both items on her list
+    # She is invited to enter a to-do item straight away
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+    # She types "Buy peacock feathers" into a text box (Edith's hobby
+    # is tying fly-fishing lures)
 
-# She visits that URL - her to-do list is still there.
+    # When she hits enter, the page updates, and now the page lists
+    # "1: Buy peacock feathers" as an item in a to-do list
 
-# Satisfied, she goes back to sleep
+    # There is still a text box inviting her to add another item. She
+    # enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-browser.quit()
+    # The page updates again, and now shows both items on her list
+
+    # Edith wonders whether the site will remember her list. Then she sees
+    # that the site has generated a unique URL for her -- there is some
+    # explanatory text to that effect.
+
+    # She visits that URL - her to-do list is still there.
+
+    # Satisfied, she goes back to sleep
+
+if __name_ == '__main__':
+    main()
