563bc037d0592e1ecf1d5d8a40825e0ceee9f5dc
Raphael <puello322@gmail.com>
2023-03-12 16:58:09 -0600

    removing warning line

--- greenlab/settings.py
+++ greenlab/settings.py
@@ -120,8 +120,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-try:
-    from private_settings import *
-except ImportError:
-    print("Error: make a local version of private_sestings.py from the template.")
