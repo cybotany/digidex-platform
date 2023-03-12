0ccf22b4e4ecd004cce7665ae0bdb51ff35bd2dc
Raphael <puello322@gmail.com>
2023-03-12 15:30:48 -0600

    Adding user story for plant catalog

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-import pytest
 from decouple import config
+import pytest
 
 browser = webdriver.Firefox()
 website = config("WEBSITE_URL")
@@ -9,3 +9,29 @@ website = config("WEBSITE_URL")
 # it. After scanning, a web-app launches.
 browser.get(website)
 
+# The user notices the page title and header mention cataloging
+# their plant.
+assert 'Plant Catalog' in browser.title
+
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box (Edith's hobby
+# is tying fly-fishing lures)
+
+# When she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do list
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. Then she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+
+browser.quit()
