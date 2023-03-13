664383b172498f1a1e0f90fccd9dd5c7bd0a34d7
Raphael <puello322@gmail.com>
2023-03-12 19:39:29 -0600

    Correcting bugs

--- greenlab/urls.py
+++ greenlab/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [ # Should always include include() when including other URL patterns
+urlpatterns = [    
     path('catalog/', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
