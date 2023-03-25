eaf2cf7d0fe87efbe8a3bdb42f858c522dcf2f2f
Raphael <puello322@gmail.com>
2023-03-25 09:05:30 -0600

    idk

--- greenlab/settings.py
+++ greenlab/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
