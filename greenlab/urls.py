1f90ebd2ef09ca7e39214a67bd1acb09e34faaf6
Raphael <puello322@gmail.com>
2023-03-26 12:18:30 -0600

    Still trying to get test_correct_title_dispalyed working

--- greenlab/urls.py
+++ greenlab/urls.py
@@ -14,7 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [    
     path('', include('cybotany.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])   
