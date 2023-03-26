ca7eaebb41c00c0ecbe5bece3902a41168c774e2
Raphael <puello322@gmail.com>
2023-03-25 19:04:35 -0600

    Adding login and signup pages.

--- cybotany/views.py
+++ cybotany/views.py
@@ -1,16 +1,38 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('cybotany:dashboard')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'cybotany/login.html', {'form': form})
+
 
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('cybotany:dashboard')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'cybotany/signup.html', {'form': form})
