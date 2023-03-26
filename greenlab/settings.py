d0a28c19fd3c247675974a2d1f0ce66a9198088f
Raphael <puello322@gmail.com>
2023-03-26 12:52:50 -0600

    Updating settings.py again

--- greenlab/settings.py
+++ greenlab/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'greenlab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'cybotany/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'cybotany/templates/cybotany')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
