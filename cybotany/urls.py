532863096aee6d101138074d8e15ff83d3326100
Raphael <puello322@gmail.com>
2023-03-30 20:32:07 -0600

    More changes

--- cybotany/urls.py
+++ cybotany/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import DashboardView, HomeView, SignupView, LoginView
+from .views import DashboardView, HomeView, SignupView, LoginView, SignupInfoView
 
 
 urlpatterns = [
     path('', HomeView.home, name='home'),
     path('signup/', SignupView.signup, name='signup'),
+    path('signup/info/', SignupInfoView.signup, name='signup'),
     path('login/', LoginView.login, name='login'),
     path('dashboard/', DashboardView.dashboard, name='dashboard'),
 ]
