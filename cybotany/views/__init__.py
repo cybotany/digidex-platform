4fcba67427c64a1be1df024871512ca99ac40dd4
Raphael <puello322@gmail.com>
2023-04-02 11:59:48 -0600

    Updating __init__.py

---
+++ cybotany/views/__init__.py
@@ -0,0 +1,4 @@
+from .account_login_view import AccountLoginView
+from .account_signup_view import AccountSignupView
+from .account_dashboard_view import AccountDashboardView
+from .home_view import HomeView
