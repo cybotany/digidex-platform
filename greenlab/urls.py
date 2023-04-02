ba597df6a58596e67f95372e47b3d93058e8c224
Raphael <puello322@gmail.com>
2023-04-02 17:08:53 -0600

    Adding superuser

--- greenlab/urls.py
+++ greenlab/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [    
+    path('admin/', admin.site.urls),
     path('', include('cybotany.urls')),
 ]
 
