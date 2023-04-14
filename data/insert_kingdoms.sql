0f6efa7ae0960d0c314711b882530e8229f3752b
Raphael <puello322@gmail.com>
2023-04-14 11:06:45 -0600

    Fixing ITIS.sql to work

---
+++ data/insert_kingdoms.sql
@@ -0,0 +1,13 @@
+COPY kingdoms (kingdom_id, kingdom_name, update_date) FROM stdin;
+1	Bacteria  	2014-08-20
+2	Protozoa  	2004-06-04
+3	Plantae   	1996-03-26
+4	Fungi     	1996-03-26
+5	Animalia  	1996-03-26
+6	Chromista 	2004-06-04
+7	Archaea   	2014-08-20
+\.
+
+
+--
+-- Data for Name: longnames; Type: TABLE DATA; Schema: public; 
