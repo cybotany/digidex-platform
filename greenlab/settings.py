b2babda4ce72314084392afc0467759efb1694e2
Raphael <puello322@gmail.com>
2023-05-21 17:50:54 -0600

    Updating rest_framework used

--- greenlab/settings.py
+++ greenlab/settings.py
@@ -142,9 +142,9 @@ AWS_S3_FILE_OVERWRITE = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    )
 }
 
 LOGIN_REDIRECT_URL = 'dashboard'
