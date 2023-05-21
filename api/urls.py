7f8732a80ea45f50eb08435defbab9969bb4863b
Raphael <puello322@gmail.com>
2023-05-21 17:18:16 -0600

    Updating URLS again

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import UserDetailView
-from rest_framework_simplejwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     path('api/user_info/', UserDetailView.as_view(), name='user_info'),
