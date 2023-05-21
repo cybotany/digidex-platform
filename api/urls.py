2729564c1b643fe328af48d3f503c95d96ca916c
Raphael <puello322@gmail.com>
2023-05-21 17:28:33 -0600

    Commenting out API urls

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import UserDetailView
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+#from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    path('api-token-auth/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
-    path('api-token-verify/', verify_jwt_token),
+    path('api/user_info/', UserDetailView.as_view(), name='user_info'),
+#    path('api-token-auth/', obtain_jwt_token),
+#    path('api-token-refresh/', refresh_jwt_token),
+#    path('api-token-verify/', verify_jwt_token),
 ]
