5887cbb389fc179596e376ab35e4f2a9763d53e7
Raphael <puello322@gmail.com>
2023-05-21 17:20:58 -0600

    more updates

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from .views import UserDetailView
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
+from rest_framework_jwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('api/user_info/', UserDetailView.as_view(), name='user_info'),
-    path('api-token-auth/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
-    path('api-token-verify/', verify_jwt_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
