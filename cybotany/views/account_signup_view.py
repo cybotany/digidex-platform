2bd60264e489b46df50ffe6ddfd95739f447fc06
Raphael <puello322@gmail.com>
2023-05-21 18:28:43 -0600

    Updating signup to include email and name.

--- cybotany/views/account_signup_view.py
+++ cybotany/views/account_signup_view.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import NewUserForm
 from django.contrib.auth import login
 from django.views import View
 
 
 class AccountSignupView(View):
     def get(self, request):
-        form = UserCreationForm()
+        form = NewUserForm()
         return render(request, 'cybotany/signup.html', {'form': form})
 
     def post(self, request):
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
