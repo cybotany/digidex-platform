d0b324fb52a8f439055b77e1053b557e82c0f771
Raphael <puello322@gmail.com>
2023-05-21 18:34:28 -0600

    Fixing path for views

--- cybotany/views/account_signup_view.py
+++ cybotany/views/account_signup_view.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import NewUserForm
+from ..forms import NewUserForm
 from django.contrib.auth import login
 from django.views import View
 
