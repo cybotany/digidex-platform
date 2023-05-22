99bd358c06c410341ac0446e7999989bb4c189bd
Raphael <puello322@gmail.com>
2023-05-21 18:03:26 -0600

    Forgot this one

--- greenlab/urls.py
+++ greenlab/urls.py
@@ -17,8 +17,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('', include('cybotany.urls')),
     path('', include('api.urls')),
