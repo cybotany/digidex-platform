4461722aa05ad78461c8a3c6fbc4add4aff9b6d5
Raphael <puello322@gmail.com>
2023-05-28 17:09:27 -0600

    More changes for site

--- cybotany/views/account_dashboard_view.py
+++ cybotany/views/account_dashboard_view.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from ..models import GrowthChamber, UserPlant
+from ..models import GrowthChamber
 
 
 class AccountDashboardView(LoginRequiredMixin, TemplateView):
@@ -10,6 +10,5 @@ class AccountDashboardView(LoginRequiredMixin, TemplateView):
     def get(self, request):
         # Get user's growth chambers and plants
         growth_chambers = GrowthChamber.objects.filter(user=request.user)
-        user_plants = UserPlant.objects.filter(user=request.user, growth_chamber=None)
 
-        return render(request, self.template_name, {'growth_chambers': growth_chambers, 'user_plants': user_plants})
+        return render(request, self.template_name, {'growth_chambers': growth_chambers})
