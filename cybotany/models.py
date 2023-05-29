4d1e00557854c50a2a4914816b37fb7168a4f19d
Raphael <puello322@gmail.com>
2023-05-29 12:02:11 -0600

    More updates

--- cybotany/models.py
+++ cybotany/models.py
@@ -14,6 +14,7 @@ class Sensor(models.Model):
     description = models.TextField()
     min_value = models.DecimalField(max_digits=6, decimal_places=2)
     max_value = models.DecimalField(max_digits=6, decimal_places=2)
+    value_unit = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
