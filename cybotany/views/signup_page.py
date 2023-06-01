4c219bf8a194f8b7d393a11a2f69630eb6912149
Raphael <puello322@gmail.com>
2023-05-31 22:17:12 -0600

    Some renaming

--- cybotany/views/signup_page.py
+++ cybotany/views/signup_page.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from ..forms import NewUserForm
+from ..forms import UserForm
 from django.contrib.auth import login
 from django.views import View
 
@@ -8,11 +8,11 @@ class AccountSignupView(View):
     template_name = 'signup.html'
 
     def get(self, request):
-        form = NewUserForm()
+        form = UserForm()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        form = NewUserForm(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
