28186497a5f8566d33a6f7d4020aa07311a7d242
Raphael <puello322@gmail.com>
2023-06-03 20:14:02 -0600

    More updates

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserList, ChatbotAPIView
 
 
 urlpatterns = [
     path('user_info/', UserList.as_view(), name='user_info'),
     path('chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- cybotany/urls.py
+++ cybotany/urls.py
@@ -17,11 +17,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
     path('authentication/', include('apps.authentication.urls')),
