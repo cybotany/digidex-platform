9fa4b3e67db7b51b6d6d4b255a86f2d4b0cab531
Raphael <puello322@gmail.com>
2023-06-06 18:36:12 -0600

    Adding home page

--- cybotany/urls.py
+++ cybotany/urls.py
@@ -5,15 +5,15 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
+from ..apps.core.views import HomePageView
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('user/', include('apps.authentication.urls')),
     path('botany/', include('apps.botany.urls')),
     path('cea/', include('apps.cea.urls')),
     path('chatbot/', include('apps.chatbot.urls')),
     path('api/', include('apps.api.urls')),
-    path('core/', include('apps.core.urls')),
 ]
 
 if settings.DEBUG:
