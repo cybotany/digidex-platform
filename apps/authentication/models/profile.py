3f034d713711dcabee163d4e57717488ab9ee8bc
Raphael <puello322@gmail.com>
2023-06-09 09:40:32 -0600

    More fixes.

--- apps/authentication/models/profile.py
+++ apps/authentication/models/profile.py
@@ -16,9 +16,8 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+    @receiver(post_save, sender=User)
+    def create_or_update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
