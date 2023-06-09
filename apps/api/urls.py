5f20ff63f0bf72ede0859e6e04263b49a11f1c81
Raphael <puello322@gmail.com>
2023-06-09 13:41:34 -0600

    Fixing import error

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserList, ChatbotAPIView, CEARegister
+from .views import UserList, ChatbotAPIView, CEAIdentification
 
 app_name = 'api'
 urlpatterns = [
     path('user_info/', UserList.as_view(), name='user_info'),
-    path('identify_cea/', CEARegister.as_view(), name='identify_cea'),
+    path('identify_cea/', CEAIdentification.as_view(), name='identify_cea'),
     path('chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
