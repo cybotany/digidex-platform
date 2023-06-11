cc234f0e1746af628c9b59ece60f7ccd2d4ed39e
Raphael <puello322@gmail.com>
2023-06-11 10:22:18 -0600

    More updates.

--- apps/botany/urls.py
+++ apps/botany/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', BotanyHomeView.as_view(), name='home'),
     path('new_label', CreateLabel.as_view(), name='new_label'),
     path('new_plant', RegisterPlant.as_view(), name='new_plant'),
-    path('a/plant_registration', AutoPlantRegistration.as_view(), name='auto_plant_form'),
-    path('m/plant_registration', ManualPlantRegistration.as_view(), name='manual_plant_form'),
+    path('auto_plant_registration', AutoPlantRegistration.as_view(), name='auto_plant_form'),
+    path('manual_plant_registration', ManualPlantRegistration.as_view(), name='manual_plant_form'),
 
 ]
