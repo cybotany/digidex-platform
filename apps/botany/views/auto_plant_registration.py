a67bcae05b9e48e8eda68c6c78929250881b42ba
Raphael <puello322@gmail.com>
2023-06-11 16:21:26 -0600

    Debugging

--- apps/botany/views/auto_plant_registration.py
+++ apps/botany/views/auto_plant_registration.py
@@ -1,9 +1,10 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
 from ..forms import PlantImageForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class AutoPlantRegistration(FormView):
+class AutoPlantRegistration(LoginRequiredMixin, FormView):
     template_name = 'botany/auto_plant_registration.html'
     form_class = PlantImageForm
     success_url = reverse_lazy('botany:home')
@@ -12,5 +13,6 @@ class AutoPlantRegistration(FormView):
         '''
         This method is called when valid form data has been POSTed.
         '''
+        form.instance.owner = self.request.user
         form.save()
         return super().form_valid(form)
