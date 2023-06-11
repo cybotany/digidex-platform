a20c783c9c273926b5867a79bfb736236c02c2f3
Raphael <puello322@gmail.com>
2023-06-11 16:07:08 -0600

    More updates

--- cybotany/settings.py
+++ cybotany/settings.py
@@ -119,7 +119,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
