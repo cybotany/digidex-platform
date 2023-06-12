4ad0bde14bd2af105f5c8c4325395895f3056f3e
Raphael <puello322@gmail.com>
2023-06-11 19:43:49 -0600

    More updates

--- cybotany/settings.py
+++ cybotany/settings.py
@@ -122,6 +122,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
