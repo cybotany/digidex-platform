329674f7fe95a265dc018f81770ed67a5ec9a033
Raphael <puello322@gmail.com>
2023-06-11 21:20:07 -0600

    More changes

--- apps/botany/models/plant.py
+++ apps/botany/models/plant.py
@@ -1,7 +1,23 @@
+import os
+import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from .label import Label
 
+def validate_file_extension(value):
+    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+    valid_extensions = ['.jpg', '.png', '.jpeg']
+    if not ext.lower() in valid_extensions:
+        raise ValidationError('Unsupported file extension.')
+
+def user_directory_path(instance, filename):
+    # get the file extension
+    ext = filename.split('.')[-1]
+    # generate a UUID for the new filename
+    filename = f'{uuid.uuid4()}.{ext}'
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return os.path.join(f'user_{instance.user.id}', filename)
 
 User = get_user_model()
 
@@ -17,7 +33,7 @@ class Plant(models.Model):
     scientific_name = models.CharField(max_length=200, null=True, blank=True)
     synonyms = models.TextField(null=True, blank=True)
     taxonomy = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='plants/', null=True, blank=True)
+    image = models.ImageField(upload_to=user_directory_path, validators=[validate_file_extension], null=True, blank=True)
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     added_on = models.DateTimeField(auto_now_add=True)
