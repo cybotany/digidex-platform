7ff6740b7da02d1d36c9659ad31619bd69c01440
Raphael <puello322@gmail.com>
2023-06-19 09:31:19 -0600

    Fixing auth class

--- cybotany/settings.py
+++ cybotany/settings.py
@@ -126,11 +126,6 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
