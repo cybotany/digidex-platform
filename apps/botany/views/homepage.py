309f4b9042ce347f3d951a0b84211c2e2a866dcf
Raphael <puello322@gmail.com>
2023-06-20 14:52:40 -0600

    Making fix

--- apps/botany/views/homepage.py
+++ apps/botany/views/homepage.py
@@ -9,7 +9,7 @@ class PlantHomepageView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # fetch labeled plant groups
-        plant_groups = Label.objects.filter(user=self.request.user).prefetch_related('plants')
+        plant_groups = Label.objects.filter(user=self.request.user).prefetch_related('plant_set')
 
         context['plant_groups'] = list(plant_groups)
         return context
