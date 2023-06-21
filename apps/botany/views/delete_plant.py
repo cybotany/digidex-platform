bebbed6befcdc614139ce3e63e06017e0afbfeba
Raphael <puello322@gmail.com>
2023-06-21 11:34:30 -0600

    More fixes

--- apps/botany/views/delete_plant.py
+++ apps/botany/views/delete_plant.py
@@ -1,20 +1,27 @@
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from apps.botany.models import Plant
 
 
-class DeletePlantView(LoginRequiredMixin, DeleteView):
-    model = Plant
-    template_name = 'botany/plant_delete_confirmation.html'
-    success_url = reverse_lazy('botany:home')
+class DeletePlantView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'botany/delete_plant.html'
 
-    def delete(self, request, *args, **kwargs):
+    def get_object(self):
+        return get_object_or_404(Plant, pk=self.kwargs['pk'])
+
+    def test_func(self):
         plant = self.get_object()
-        if self.request.user == plant.owner:
-            messages.success(self.request, 'Plant successfully deleted.')
-            return super(DeletePlantView, self).delete(request, *args, **kwargs)
-        else:
-            messages.error(self.request, 'You do not have permission to delete this plant.')
-            return reverse_lazy('botany:home')
+        return self.request.user == plant.owner
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['plant'] = self.get_object()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        plant = self.get_object()
+        plant.delete()
+        messages.success(self.request, 'Your plant was successfully deleted!')
+        return redirect('botany:home')
