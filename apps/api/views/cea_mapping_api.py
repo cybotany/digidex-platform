cb4d883541cd62a9269a9e68c441584b4c07202a
Raphael <puello322@gmail.com>
2023-06-23 15:29:47 -0600

    Updating CEA model

--- apps/api/views/cea_mapping_api.py
+++ apps/api/views/cea_mapping_api.py
@@ -3,14 +3,12 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+
+from apps.cea.models import Greenhouse
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CEAMappingAPIView(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         ip_address = request.data.get('ip_address')
@@ -19,5 +17,16 @@ class CEAMappingAPIView(APIView):
         if not ip_address or not identifier:
             return Response({'error': 'Missing ip_address or identifier.'}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Check if a Greenhouse with the given identifier already exists
+        greenhouse = Greenhouse.objects.filter(identifier=identifier).first()
+
+        if greenhouse:
+            # If it does, update the IP address
+            greenhouse.ip_address = ip_address
+            greenhouse.save()
+        else:
+            # If it doesn't, create a new Greenhouse
+            greenhouse = Greenhouse.objects.create(ip_address=ip_address, identifier=identifier)
+
         # You can do more things here like associating the IP with a user or a specific device
-        return Response({'message': 'CEA successfully registered.'}, status=status.HTTP_200_OK)
+        return Response({'message': 'CEA successfully registered.', 'greenhouse_id': greenhouse.id}, status=status.HTTP_200_OK)
--- apps/cea/models/base_cea.py
+++ apps/cea/models/base_cea.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 
 
 User = get_user_model()
@@ -8,6 +7,7 @@ User = get_user_model()
 
 class BaseCEA(models.Model):
     name = models.CharField(max_length=255)
+    identifier = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
