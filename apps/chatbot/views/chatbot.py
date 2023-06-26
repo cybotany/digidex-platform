2d6f7c3e04c136e0605cb69670858c870903b454
Raphael <puello322@gmail.com>
2023-06-25 19:55:37 -0600

    Updating login page

--- apps/chatbot/views/chatbot.py
+++ apps/chatbot/views/chatbot.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 
 
-class ChatbotView(View):
+class ChatbotView(LoginRequiredMixin, View):
     template_name = 'chatbot/chat.html'
 
     def get(self, request):
