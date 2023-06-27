3f3ea8b9fd988426cb06053daf821fed8c7f1306
Raphael <puello322@gmail.com>
2023-06-26 19:34:19 -0600

    Fixing template

--- apps/accounts/views/display_user.py
+++ apps/accounts/views/display_user.py
@@ -9,10 +9,7 @@ class DisplayUserView(LoginRequiredMixin, View):
     template_name = 'accounts/display_user.html'
 
     def get(self, request, *args, **kwargs):
-        # Fetch the activities related to the user
         recent_activities = Activity.objects.filter(user=request.user).order_by('-timestamp')
-        
-        # Include the activities in the context
         context = {
             'user': request.user,
             'recent_activities': recent_activities,
