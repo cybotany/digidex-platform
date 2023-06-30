2e993093578a58c9d669f75e84e02c9b52d4334c
Raphael <puello322@gmail.com>
2023-06-30 17:17:23 -0600

    Updating column name for plant model

--- apps/botany/models/plant.py
+++ apps/botany/models/plant.py
@@ -15,14 +15,14 @@ class Plant(models.Model):
         name (str): The name of the plant.
         label (Label): The label associated with the plant.
         description (str): The description of the plant.
-        owner (User): The user who owns the plant.
+        user (User): The user who owns the plant.
         added_on (datetime): The date and time when the plant was added.
     """
 
     name = models.CharField(max_length=100, null=True, blank=True)
     label = models.ForeignKey(Label, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True, blank=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     added_on = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
@@ -30,7 +30,7 @@ class Plant(models.Model):
         Override the save method to set a default name if not provided.
         """
         if not self.name:
-            total_plants = Plant.objects.filter(owner=self.owner).count()
+            total_plants = Plant.objects.filter(owner=self.user).count()
             self.name = f'Plant-{total_plants + 1}'
         super().save(*args, **kwargs)
 
