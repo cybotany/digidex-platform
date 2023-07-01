4afe2731bdace06e8f8a3764d68fd043cbedbbcb
Raphael <puello322@gmail.com>
2023-06-30 18:36:24 -0600

    More fixes

--- apps/botany/management/commands/create_common_labels.py
+++ apps/botany/management/commands/create_common_labels.py
@@ -2,21 +2,20 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 from apps.botany.models import Label
+from apps.utils.constants import COMMON_LABELS
 
 
 class Command(BaseCommand):
     help = 'Create common labels for existing users'
 
     def handle(self, *args, **options):
-        common_labels = ['Label 1', 'Label 2', 'Label 3']  # Replace these with your common labels
-        
         # Fetch all users
         users = get_user_model().objects.all()
 
         # Loop through all users
         for user in users:
             # For each user, create the common labels
-            for label in common_labels:
+            for label in COMMON_LABELS:
                 Label.objects.get_or_create(user=user, name=label, is_common=True)
 
         # Output completion message
