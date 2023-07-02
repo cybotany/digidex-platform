f5d06d624566f5ef91b25e46f218c19e19ddf599
Raphael <puello322@gmail.com>
2023-07-01 18:22:17 -0600

    More fixes

--- apps/accounts/views/change_profile.py
+++ apps/accounts/views/change_profile.py
@@ -10,7 +10,7 @@ class ChangeProfileView(LoginRequiredMixin, UpdateView):
     model = Profile
     form_class = ProfileChangeForm
     template_name = 'accounts/change_profile.html'
-    success_url = reverse_lazy('profile_detail')
+    success_url = reverse_lazy('accounts:profile')
 
     def get_object(self, queryset=None):
         return self.request.user.profile
