fe05b56b64a6a89955ee0d04402d98b12cf8556d
Raphael <puello322@gmail.com>
2023-07-01 18:31:42 -0600

    More fixes to profile page

--- apps/accounts/views/user_profile.py
+++ apps/accounts/views/user_profile.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.shortcuts import render
 
-from apps.accounts.models import Activity
+from apps.accounts.models import Activity, Profile
 
 
 class UserProfileView(LoginRequiredMixin, View):
@@ -10,8 +10,11 @@ class UserProfileView(LoginRequiredMixin, View):
 
     def get(self, request, *args, **kwargs):
         recent_activities = Activity.objects.filter(user=request.user).order_by('-timestamp')
+        user_profile = Profile.objects.get(user=request.user)
+
         context = {
             'user': request.user,
             'recent_activities': recent_activities,
+            'user_profile': user_profile
         }
         return render(request, self.template_name, context)
