956254b4bf4330213300a82fd43fea414e3b494d
Raphael <puello322@gmail.com>
2023-07-08 17:30:04 -0600

    More fixes

--- apps/api/views/chatbot_api.py
+++ apps/api/views/chatbot_api.py
@@ -43,28 +43,15 @@ class ChatbotAPIView(APIView):
 
     def post(self, request, *args, **kwargs):
         message = request.data.get('message')
-        user = request.user
 
         # Input validation
         if not message:
             return Response({"error": "Message is required"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Fetch user's profile and info
-        profile = Profile.objects.get(user=user)
-        user_interest = profile.interests
-        user_experience = profile.experience
-
         try:
-            # Use LangChain to handle the conversation
             output = self.conversation.predict(input=message)
         except Exception as e:
-            # You might want to log the exception here
             return Response({"error": f"Failed to process your request due to {e}. Please try again later."},
                             status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
-        # Save user input and AI's response to the database
-        ChatMessage.objects.create(user=user, content=f'Human: {message}')
-        ChatMessage.objects.create(user=user, content=f'AI: {output}')
-
-        # Send the assistant's message back to the front-end
         return Response({'message': output})
