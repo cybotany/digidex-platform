9555d4da8e35d6d391c8652d419f1c44a7906fcf
Raphael <puello322@gmail.com>
2023-07-11 23:23:05 -0600

    Trying to fix label intergrity error

--- apps/botany/views/register_label.py
+++ apps/botany/views/register_label.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.db import IntegrityError
 from django.views.generic.edit import FormView
 from django.shortcuts import redirect
 from apps.accounts.models import Activity
@@ -10,18 +11,23 @@ class RegisterLabelView(FormView):
     form_class = GrowingLabelForm
 
     def form_valid(self, form):
-        new_label = form.save(commit=False)
-        new_label.user = self.request.user
-        new_label.save()
+        try:
+            new_label = form.save(commit=False)
+            new_label.user = self.request.user
+            new_label.save()
 
-        Activity.objects.create(
-            user=self.request.user,
-            activity_status='registered',
-            activity_type='growing_label',
-            content=f'Registered a new label group: {new_label.name}',
-        )
+            Activity.objects.create(
+                user=self.request.user,
+                activity_status='registered',
+                activity_type='growing_label',
+                content=f'Registered a new label group: {new_label.name}',
+            )
 
-        messages.success(self.request, f'Label "{new_label.name}" was successfully added.')
+            messages.success(self.request, f'Label "{new_label.name}" was successfully added.')
+
+        except IntegrityError:
+            form.add_error('name', f'A label with the name "{form.cleaned_data["name"]}" already exists.')
+            return self.form_invalid(form)
 
         return redirect('botany:home')
 
