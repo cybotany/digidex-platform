f74987c8631cda238e4bae582c2cb60234fce546
Raphael <puello322@gmail.com>
2023-09-09 15:29:19 -0600

    more updates

--- apps/accounts/forms/user_signup.py
+++ apps/accounts/forms/user_signup.py
@@ -10,7 +10,7 @@ class SignupForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("email", "password1", "password2",)
+        fields = ("username", "email", "password1", "password2",)
 
     def save(self, commit=True):
         user = super(SignupForm, self).save(commit=False)
