1ee6e6fad3009e52fb5fbc6b66d62601b43c7d6a
Raphael <puello322@gmail.com>
2023-09-12 13:08:16 -0600

    More updates

--- apps/botany/forms/plant_registration.py
+++ apps/botany/forms/plant_registration.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms.widgets import TextInput
-from apps.botany.models import Plant, GrowingLabel, PlantImage
+from apps.botany.models import Plant, PlantImage
 
 
 class ReadOnlyTextInput(TextInput):
@@ -22,7 +22,6 @@ class PlantRegistrationForm(forms.ModelForm):
     """
 
     nfc_tag = forms.CharField(widget=ReadOnlyTextInput, required=False)
-    label = forms.ModelChoiceField(queryset=GrowingLabel.objects.none(), required=False)
     image = forms.ImageField(required=False)
 
     class Meta:
@@ -33,19 +32,12 @@ class PlantRegistrationForm(forms.ModelForm):
             """
             Initialize the form.
 
-            Pop the user and id from kwargs and initializes the queryset for the label
-            field to include labels specific to the user as well as common labels.
+            Pop the user and nfc_tag from kwargs.
             """
             self.user = kwargs.pop('user', None)
             self.nfc_tag = kwargs.pop('nfc_tag', None)
             super().__init__(*args, **kwargs)
 
-            if self.user:
-                user_labels = GrowingLabel.objects.filter(user=self.user)
-                self.fields['label'].queryset = user_labels
-
-            if self.nfc_tag:
-                self.fields['nfc_tag'].initial = self.nfc_tag
 
     def save(self, commit=True):
         """
