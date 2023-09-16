7b89f5cb17fbb2e1e5e8d9976b48999596cd0567
Raphael <puello322@gmail.com>
2023-09-16 16:34:07 -0600

    More updates to plant registration

--- apps/nfc/views/nfc_handling.py
+++ apps/nfc/views/nfc_handling.py
@@ -9,4 +9,4 @@ class HandleNFCView(View):
             plant = Plant.objects.get(nfc_tag=nfc_sn)
             return redirect('botany:describe_plant', pk=plant.id)
         except Plant.DoesNotExist:
-            return redirect('botany:register_plant', nfc_tag=nfc_sn)
+            return redirect('botany:register_plant/', nfc_tag=nfc_sn)
