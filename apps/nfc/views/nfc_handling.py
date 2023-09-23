9434f12834c68245de15e7076dc37b6234b43a87
Raphael <puello322@gmail.com>
2023-09-23 14:29:22 -0600

    more updates to nfc_handling

--- apps/nfc/views/nfc_handling.py
+++ apps/nfc/views/nfc_handling.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect, get_object_or_404
-from django.core.exceptions import MultipleObjectsReturned
+from django.shortcuts import redirect
 from django.views import View
 from apps.botany.models import Plant
 
@@ -11,11 +10,8 @@ class HandleNFCView(LoginRequiredMixin, View):
 
     def get(self, request, nfc_sn):
         try:
-            plant = get_object_or_404(Plant, nfc_tag=nfc_sn)
+            plant = Plant.objects.get(nfc_tag=nfc_sn)
             return redirect('botany:describe_plant', pk=plant.id)
-        except MultipleObjectsReturned:
-            # Handle the case where multiple objects are returned
-            return redirect('some_error_page')
         except Plant.DoesNotExist:
-            # Redirect to the plant registration page with the NFC tag
-            return redirect('botany:register_plant_nfc', nfc_tag=nfc_sn)
+            request.session['nfc_tag'] = nfc_sn
+            return redirect('botany:register_plant')
