523fd84b1fcba84ae5b16245103ae2f835a2ee27
Raphael <puello322@gmail.com>
2023-09-23 14:01:32 -0600

    Updating NFC to require login

--- apps/nfc/views/nfc_handling.py
+++ apps/nfc/views/nfc_handling.py
@@ -1,12 +1,21 @@
-from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, get_object_or_404
+from django.core.exceptions import MultipleObjectsReturned
 from django.views import View
 from apps.botany.models import Plant
 
 
-class HandleNFCView(View):
+class HandleNFCView(LoginRequiredMixin, View):
+    login_url = 'accounts:login'
+    redirect_field_name = 'next'
+
     def get(self, request, nfc_sn):
         try:
-            plant = Plant.objects.get(nfc_tag=nfc_sn)
+            plant = get_object_or_404(Plant, nfc_tag=nfc_sn)
             return redirect('botany:describe_plant', pk=plant.id)
+        except MultipleObjectsReturned:
+            # Handle the case where multiple objects are returned
+            return redirect('some_error_page')
         except Plant.DoesNotExist:
+            # Redirect to the plant registration page with the NFC tag
             return redirect('botany:register_plant_nfc', nfc_tag=nfc_sn)
