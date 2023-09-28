bfd4c19725ccfbfc91e3e7b90d2be767ed5e0b36
Raphael <puello322@gmail.com>
2023-09-27 20:10:52 -0600

    Fixed data dump script

---
+++ apps/itis/data_dump_parser.py
@@ -0,0 +1,24 @@
+import re
+
+def parse_and_export_sql_file(filename):
+    with open(filename, 'r', encoding='ISO-8859-1') as file:
+        content = file.read()
+
+    # Split content based on the provided pattern
+    segments = re.split(r'-- Data for Name: (\w+); Type: TABLE DATA; Schema: public; Owner: -', content)
+
+    # Iterate over segments in pairs (entity name and SQL content)
+    for idx in range(1, len(segments), 2):
+        entity_name = segments[idx]
+        sql_content = segments[idx + 1]
+
+        output_filename = f"/home/raphael/ITIS_{entity_name}.sql"
+
+        with open(output_filename, 'w', encoding='ISO-8859-1') as output_file:
+            output_file.write(f"-- Data for Name: {entity_name}; Type: TABLE DATA; Schema: public; Owner: -\n")
+            output_file.write(sql_content)
+
+    print(f"Exported {len(segments) // 2} SQL files.")
+
+filename = r'/mnt/chromeos/MyFiles/Downloads/itisPostgreSql/itisPostgreSql083123/ITIS.sql'
+parse_and_export_sql_file(filename)
