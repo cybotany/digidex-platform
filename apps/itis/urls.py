c17ccfd3a4d3f3e1ab0de299e28c8ed8b28c0573
Raphael <puello322@gmail.com>
2023-09-30 18:38:57 -0600

    More updates for urls.

--- apps/itis/urls.py
+++ apps/itis/urls.py
@@ -3,5 +3,5 @@ from apps.itis.views import KingdomDropdownView
 
 app_name = 'itis'
 urlpatterns = [
-    path('kingdom', KingdomDropdownView.as_view(), name='kingdom'),
+    path('kingdom/', KingdomDropdownView.as_view(), name='kingdom-dropdown'),
 ]
