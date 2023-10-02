06582dfc53c7d37b77fc3db15476a9c86708d10b
Raphael <puello322@gmail.com>
2023-10-01 18:47:02 -0600

    Adding pagination to plant homepage

--- apps/botany/views/homepage.py
+++ apps/botany/views/homepage.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.views.generic import TemplateView
 from apps.botany.models import Plant
 
@@ -14,7 +15,9 @@ class PlantHomepageView(LoginRequiredMixin, TemplateView):
         Return all plants for the currently logged-in user.
         """
         plants = Plant.objects.filter(user=self.request.user).order_by('name')
-        return plants
+        paginator = Paginator(plants, self.paginate_by)
+        page = self.request.GET.get('page')
+        return paginator.get_page(page)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
