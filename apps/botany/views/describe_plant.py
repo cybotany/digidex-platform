4f6b50d12fe819f07e51c7f6eae8bf71370f2c97
Raphael <puello322@gmail.com>
2023-10-01 18:10:28 -0600

    Including watering log.

--- apps/botany/views/describe_plant.py
+++ apps/botany/views/describe_plant.py
@@ -8,3 +8,8 @@ class DescribePlantView(DetailView):
     """
     model = Plant
     template_name = 'botany/describe_plant.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['watering_events'] = self.object.waterings.all().order_by('-timestamp')
+        return context
