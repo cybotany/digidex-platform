9f401546ef977653201627fca3cb6f83d3895541
Raphael <puello322@gmail.com>
2023-10-10 17:54:42 -0600

    Updating registration form.

--- apps/botany/views/register_plant.py
+++ apps/botany/views/register_plant.py
@@ -1,6 +1,7 @@
 from django.views.generic import FormView
 from django.shortcuts import redirect
 from apps.accounts.models import Activity
+from apps.botany.models import PlantImage
 from apps.botany.forms import PlantRegistrationForm
 
 
@@ -22,6 +23,10 @@ class RegisterPlantView(FormView):
         new_plant = form.save(commit=False)
         new_plant.user = self.request.user
         new_plant.save()
+
+        image = form.cleaned_data.get('image')
+        if image:
+            PlantImage.objects.create(plant=new_plant, image=image)
 
         Activity.objects.create(
             user=self.request.user,
