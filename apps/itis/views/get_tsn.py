389f53b66d7f49e62f74d0151376f8b8ff85eea2
Raphael <puello322@gmail.com>
2023-10-14 14:37:37 -0600

    More updates for ajax

--- apps/itis/views/get_tsn.py
+++ apps/itis/views/get_tsn.py
@@ -6,6 +6,6 @@ from apps.itis.models import TaxonomicUnits
 class GetTSNView(View):
     def get(self, request, *args, **kwargs):
         search_term = request.GET.get('q')
-        tsn_objects = TaxonomicUnits.objects.filter(name__icontains=search_term)[:10]  # adjust the limit as needed
+        tsn_objects = TaxonomicUnits.objects.filter(name__icontains=search_term)[:10]
         results = [{"id": tsn.id, "text": tsn.name} for tsn in tsn_objects]
         return JsonResponse({"items": results})
