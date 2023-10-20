07f902cb53c74f1501f5cc57ed8f186190556d18
Raphael <puello322@gmail.com>
2023-10-20 11:50:59 -0600

    more updates to nfc handling

--- apps/nfc/views/nfc_handling.py
+++ apps/nfc/views/nfc_handling.py
@@ -9,8 +9,9 @@ class HandleNFCView(LoginRequiredMixin, View):
     login_url = 'accounts:login'
 
     def get(self, request, nfc_sn):
-        # Get or create the NFC Tag
-        tag, created = Tag.objects.get_or_create(serial_number=nfc_sn)
+        logged_in_user = request.user
+        tag, created = Tag.objects.get_or_create(serial_number=nfc_sn, defaults={'created_by': logged_in_user})
+
         
         if created:
             # Handle the case where the Tag was newly created, if needed
