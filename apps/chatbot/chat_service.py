3f1509636a9eacb7722ed09d1f81869c198f6d77
Raphael <puello322@gmail.com>
2023-10-23 17:32:36 -0600

    More updates to get api read

--- apps/chatbot/chat_service.py
+++ apps/chatbot/chat_service.py
@@ -1,14 +1,13 @@
-from decouple import config
+from django.conf import settings
 from langchain.llms import OpenAI
 from langchain.chains import ConversationChain
 
 
 class ChatService:
     def __init__(self):
-
         self.llm = OpenAI(
             temperature=0.0,
-            openai_api_key=config('OPENAI_API_KEY'),
+            openai_api_key=settings.OPENAI_API_KEY,  # Reading the API key from Django settings
             model_name='gpt-3.5-turbo',
         )
         self.conversation = ConversationChain(
