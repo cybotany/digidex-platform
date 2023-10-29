267528596782900187324cb6a1017d7e8652aeb7
Raphael <puello322@gmail.com>
2023-10-28 21:06:46 -0600

    more updates for templates

--- cybotany/settings.py
+++ cybotany/settings.py
@@ -85,7 +85,7 @@ ROOT_URLCONF = 'cybotany.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
