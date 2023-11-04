b26c6f57ec71af27ab522463b1b202bf948218da
Raphael <puello322@gmail.com>
2023-11-04 13:47:06 -0600

    Adding groups for existing users

---
+++ apps/botany/management/commands/create_groups_for_existing_users.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from apps.botany.models import Group
+from apps.utils.constants import MAX_GROUP
+
+
+class Command(BaseCommand):
+    help = 'Create 15 default plant groups for existing users'
+
+    def handle(self, *args, **options):
+        for user in User.objects.all():
+            # Check if the user already has the 15 groups
+            existing_groups_count = Group.objects.filter(user=user).count()
+
+            if existing_groups_count < MAX_GROUP:
+                for i in range(existing_groups_count + 1, MAX_GROUP+1):
+                    Group.objects.create(user=user, name=f'Group {i}')
+                    self.stdout.write(self.style.SUCCESS(f'Created Group {i} for user {user.username}'))
+
+                self.stdout.write(self.style.SUCCESS(f'All {MAX_GROUP} groups created for user {user.username}'))
