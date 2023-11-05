705e692f039a8afbae7765fb4cc5d9a5f2846cc2
Raphael <puello322@gmail.com>
2023-11-04 23:28:16 -0600

    more fixes

--- static/js/plant_groups.js
+++ static/js/plant_groups.js
@@ -1,27 +1,50 @@
-// DOM Elements
 const plantGroupSelector = document.getElementById('plantGroupSelector');
 const plantListContainer = document.getElementById('plantListContainer');
+const leftArrow = document.getElementById('leftArrow');
+const rightArrow = document.getElementById('rightArrow');
 
-// Event Listener for when the plant group is changed
-plantGroupSelector.addEventListener('change', function() {
-    const selectedGroupId = this.value;
+if (plantGroupSelector && plantListContainer) {
+    // Event Listener for when the plant group is changed via selector
+    plantGroupSelector.addEventListener('change', function() {
+        fetchPlantsForGroup(this.value);
+    });
 
-    fetch(`/api/plants_by_group/${selectedGroupId}/`)
+    // Ensure the leftArrow exists before attaching the event
+    if (leftArrow) {
+        leftArrow.addEventListener('click', function() {
+            const prevGroupId = this.getAttribute('data-group-id');
+            if (prevGroupId) fetchPlantsForGroup(prevGroupId);
+        });
+    }
+
+    // Ensure the rightArrow exists before attaching the event
+    if (rightArrow) {
+        rightArrow.addEventListener('click', function() {
+            const nextGroupId = this.getAttribute('data-group-id');
+            if (nextGroupId) fetchPlantsForGroup(nextGroupId);
+        });
+    }
+}
+
+function fetchPlantsForGroup(groupId) {
+    fetch(`/api/get_group/${groupId}/`)
     .then(response => response.json())
     .then(data => {
-        updatePlantList(data);
+        // Check if data is an array before processing
+        if (Array.isArray(data)) {
+            updatePlantList(data);
+        } else {
+            console.error("Expected data to be an array but got:", data);
+        }
     })
     .catch(error => {
         console.error("There was an error fetching the plants:", error);
     });
-});
+}
 
-// Update the plant list on the page with new data
 function updatePlantList(data) {
     let plantHtml = '';
-
     data.forEach(plant => {
-        // You can customize the HTML structure as needed
         plantHtml += `
             <div class="plant-item">
                 <h2>${plant.name}</h2>
@@ -29,21 +52,14 @@ function updatePlantList(data) {
             </div>
         `;
     });
-
     plantListContainer.innerHTML = plantHtml;
 }
 
-// Fetch plants for the initial group on page load
 document.addEventListener('DOMContentLoaded', (event) => {
-    const initialGroupId = plantGroupSelector.value;
-    if (initialGroupId) {
-        fetch(`/api/plants_by_group/${initialGroupId}/`)
-        .then(response => response.json())
-        .then(data => {
-            updatePlantList(data);
-        })
-        .catch(error => {
-            console.error("There was an error fetching the plants:", error);
-        });
+    if (plantGroupSelector) {
+        const initialGroupId = plantGroupSelector.value;
+        if (initialGroupId) {
+            fetchPlantsForGroup(initialGroupId);
+        }
     }
 });
