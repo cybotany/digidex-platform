68b5af1260d16a8c7f775e4eaa2f3029cbd12317
Raphael <puello322@gmail.com>
2023-11-05 10:31:57 -0700

    more udpates for homepage

--- static/js/plant_groups.js
+++ static/js/plant_groups.js
@@ -1,29 +1,21 @@
-const plantGroupSelector = document.getElementById('plantGroupSelector');
 const plantListContainer = document.getElementById('plantListContainer');
 const leftArrow = document.getElementById('leftArrow');
 const rightArrow = document.getElementById('rightArrow');
 
-if (plantGroupSelector && plantListContainer) {
-    // Event Listener for when the plant group is changed via selector
-    plantGroupSelector.addEventListener('change', function() {
-        fetchPlantsForGroup(this.value);
+// Ensure the leftArrow exists before attaching the event
+if (leftArrow) {
+    leftArrow.addEventListener('click', function() {
+        const prevGroupId = this.getAttribute('data-group-id');
+        if (prevGroupId) fetchPlantsForGroup(prevGroupId);
     });
+}
 
-    // Ensure the leftArrow exists before attaching the event
-    if (leftArrow) {
-        leftArrow.addEventListener('click', function() {
-            const prevGroupId = this.getAttribute('data-group-id');
-            if (prevGroupId) fetchPlantsForGroup(prevGroupId);
-        });
-    }
-
-    // Ensure the rightArrow exists before attaching the event
-    if (rightArrow) {
-        rightArrow.addEventListener('click', function() {
-            const nextGroupId = this.getAttribute('data-group-id');
-            if (nextGroupId) fetchPlantsForGroup(nextGroupId);
-        });
-    }
+// Ensure the rightArrow exists before attaching the event
+if (rightArrow) {
+    rightArrow.addEventListener('click', function() {
+        const nextGroupId = this.getAttribute('data-group-id');
+        if (nextGroupId) fetchPlantsForGroup(nextGroupId);
+    });
 }
 
 function fetchPlantsForGroup(groupId) {
@@ -46,20 +38,13 @@ function updatePlantList(data) {
     let plantHtml = '';
     data.forEach(plant => {
         plantHtml += `
-            <div class="plant-item">
-                <h2>${plant.name}</h2>
-                <p>${plant.description}</p>
+            <div class="col-md-4 mb-4">
+                <div class="plant-card">
+                    <h2>${plant.name}</h2>
+                    <p>${plant.description}</p>
+                </div>
             </div>
         `;
     });
     plantListContainer.innerHTML = plantHtml;
 }
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    if (plantGroupSelector) {
-        const initialGroupId = plantGroupSelector.value;
-        if (initialGroupId) {
-            fetchPlantsForGroup(initialGroupId);
-        }
-    }
-});
