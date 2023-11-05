4e25793e9bbf4e75311588e954ea36c653061930
Raphael <puello322@gmail.com>
2023-11-05 09:08:54 -0700

    More updates

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('chatbot/', ChatbotAPIView.as_view(), name='chatbot'),
-    path('get_group/', GetGroupView.as_view(), name='get-group'),
+    path('get_group/int:group_id>/', GetGroupView.as_view(), name='get-group'),
     path('get_tsn/', GetTSNView.as_view(), name='get-tsn'),
 ]
