76d5cfd6f68fa022287b96de60c9763caacb18ce
Raphael <puello322@gmail.com>
2023-11-07 17:32:01 -0700

    still working

--- apps/api/views/get_chat_message.py
+++ apps/api/views/get_chat_message.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+#from rest_framework.permissions import IsAuthenticated
 from apps.chatbot.chat_service import ChatService
 
 
 class GetChatMessage(APIView):
-    permission_classes = [IsAuthenticated]
+    #permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         message = request.data.get('message')
