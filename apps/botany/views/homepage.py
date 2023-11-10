547f991bec3741951cbc4501183579d92208a95b
Raphael <puello322@gmail.com>
2023-11-09 19:11:52 -0700

    still more updates to templates

--- apps/botany/views/homepage.py
+++ apps/botany/views/homepage.py
@@ -1,25 +1,33 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.template.loader import render_to_string
 from apps.botany.models import Group
+from apps.utils.constants import MAX_GROUP_CAPACITY
 
 
 class PlantHomepageView(LoginRequiredMixin, TemplateView):
-    """
-    View for rendering the plant module homepage.
-    """
     template_name = 'botany/plant_grid.html'
+    paginate_by = MAX_GROUP_CAPACITY
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        user_groups = Group.objects.filter(user=self.request.user).order_by('position')
 
-        user_groups = Group.objects.filter(user=self.request.user).order_by('position').all()
+        # Paginator setup
+        paginator = Paginator(user_groups, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
 
-        current_group = user_groups.first()
-        prev_group = user_groups.last() if current_group.position == 1 else user_groups[current_group.position - 2]
-        next_group = user_groups.first() if current_group.position == len(user_groups) else user_groups[current_group.position]
-
-        context['current_group'] = current_group
-        context['prev_group_id'] = prev_group.id
-        context['next_group_id'] = next_group.id
-
+        context['page_obj'] = page_obj
         return context
+
+    def get(self, request, *args, **kwargs):
+        if request.is_ajax():
+            context = self.get_context_data(**kwargs)
+            html = render_to_string('botany/partials/group_list.html', context, request=request)
+            return JsonResponse({'html': html})
+        else:
+            return super().get(request, *args, **kwargs)
