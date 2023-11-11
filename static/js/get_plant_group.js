1880bb6faddbcc4d2d0ba02a02630769a3e3a9c3
Raphael <puello322@gmail.com>
2023-11-10 20:47:02 -0700

    more updates

--- static/js/get_plant_group.js
+++ static/js/get_plant_group.js
@@ -1,15 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const leftArrow = document.getElementById('leftArrow');
-    const rightArrow = document.getElementById('rightArrow');
     const pageSelector = document.getElementById('groupPageSelector');
 
     pageSelector.addEventListener('change', function() {
       window.location.href = '?page=' + this.value;
-    });
-    leftArrow.addEventListener('click', function() {
-      window.location.href = '?page=' + leftArrow.getAttribute('data-group-id');
-    });
-    rightArrow.addEventListener('click', function() {
-      window.location.href = '?page=' + rightArrow.getAttribute('data-group-id');
     });
 });
