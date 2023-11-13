81a071754022f732cb8966f860c8833ca4e0800e
Raphael <puello322@gmail.com>
2023-11-12 18:29:33 -0700

    including model

--- apps/botany/models/__init__.py
+++ apps/botany/models/__init__.py
@@ -3,3 +3,4 @@ from .plant import Plant
 from .plant_image import PlantImage
 from .plant_watering import PlantWatering
 from .plant_fertilizing import PlantFertilization
+from .plant_journal_entry import PlantJournaling
