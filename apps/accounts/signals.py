a7d8eebb53f7d36ed33d4b806184ad6f35e9b836
Raphael <puello322@gmail.com>
2023-11-23 17:49:40 -0700

    More updates

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -2,7 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 from apps.accounts.models import Profile
-from apps.utils.helpers import create_user_groups
+from apps.groups.models import Group
+from apps.utils.constants import MAX_GROUP
 
 @receiver(post_save, sender=get_user_model())
 def manage_user_creation(sender, instance, created, **kwargs):
@@ -10,6 +11,11 @@ def manage_user_creation(sender, instance, created, **kwargs):
     Signal handler to create or update a user profile whenever a user instance is created or saved.
     """
     Profile.objects.get_or_create(user=instance)
-    
+
     if created:
-        create_user_groups(instance)
+        for i in range(1, MAX_GROUP+1):
+            Group.objects.create(
+                name=f'Group {i}',
+                user=instance,
+                position=i
+            )
--- apps/utils/helpers/create_user_groups.py
+++
@@ -1,9 +0,0 @@
-from apps.groups.models import Group
-from apps.utils.constants import MAX_GROUP
-
-def create_user_groups(user, existing_groups_count=0):
-    """
-    Create default groups for a user up to the MAX_GROUP limit.
-    """
-    for i in range(existing_groups_count + 1, MAX_GROUP + 1):
-        Group.objects.get_or_create(user=user, name=f'Group {i}', position=i)
