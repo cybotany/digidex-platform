c75f23f49bcf4d962a5f92f7cf0def45c5a1d163
Raphael <puello322@gmail.com>
2023-11-23 17:10:15 -0700

    Making fixes

--- apps/accounts/forms/profile_change.py
+++ apps/accounts/forms/profile_change.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from apps.accounts.models import Profile
 
 
@@ -7,14 +6,4 @@ class ProfileChangeForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ('bio', 'location', 'birth_date', 'avatar', 'interests', 'experience',)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        interests = cleaned_data.get('interests')
-        experience = cleaned_data.get('experience')
-
-        if bool(interests) != bool(experience):
-            raise ValidationError("Both interests and experience must be provided, or both must be left empty.")
-
-        return cleaned_data
+        fields = ('bio', 'location', 'birth_date', 'avatar',)
