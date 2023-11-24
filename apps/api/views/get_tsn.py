7b0314bf4be4d6b3be4be3d92e3834e56c50f672
Raphael <puello322@gmail.com>
2023-11-24 12:55:47 -0700

    fix typo

--- apps/api/views/get_tsn.py
+++ apps/api/views/get_tsn.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 #from rest_framework.permissions import IsAuthenticated
 from apps.taxonomy.models import Units
-from apps.api.serializers import taxonomyUnitsSerializer
+from apps.api.serializers import TaxonomyUnitsSerializer
 
 
 class GetTSN(APIView):
@@ -13,7 +13,7 @@ class GetTSN(APIView):
         tsn_objects = Units.objects.filter(complete_name__icontains=search_term)[:10]
         
         # Serialize data using DRF serializer
-        serialized_data = taxonomyUnitsSerializer(tsn_objects, many=True).data
+        serialized_data = TaxonomyUnitsSerializer(tsn_objects, many=True).data
         
         results = [{"id": tsn['tsn'], "text": tsn['complete_name']} for tsn in serialized_data]
         return Response({"items": results})
