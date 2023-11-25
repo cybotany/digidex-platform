7868ca2f972c9fb88bd3d656d2cb21be475b26a0
Raphael <puello322@gmail.com>
2023-11-25 14:23:37 -0700

    more updates

--- apps/taxonomy/apps.py
+++ apps/taxonomy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ItisConfig(AppConfig):
+class TaxonomyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.taxonomy'
