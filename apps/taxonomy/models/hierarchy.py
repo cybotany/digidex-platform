636c04946779803e6f8f83efd3378e1bb096f1f1
Raphael <puello322@gmail.com>
2023-11-25 13:31:37 -0700

    more updates

--- apps/taxonomy/models/hierarchy.py
+++ apps/taxonomy/models/hierarchy.py
@@ -3,32 +3,40 @@ from django.db import models
 
 class Hierarchy(models.Model):
     """
-    Represents the hierarchy of a taxonomic unit.
-
-    Attributes:
-        tsn (ForeignKey): The taxonomic serial number.
-        geographic_value (str): The geographic value.
-        update_date (datetime): The date and time when the geographic value was added.
+    Represents the hierarchy of a taxonomic unit as per ITIS data model.
     """
-    tsn = models.ForeignKey(
-        'Units',
-        null=True,
-        blank=True,
-        on_delete=models.CASCADE,
-        verbose_name="Taxonomic Serial Number",
-        db_column='tsn'
-    )
-    geographic_value = models.CharField(
-        max_length=200,
+    hierarchy_string = models.TextField(
+        verbose_name="Hierarchy String",
         null=True,
         blank=True
     )
-    update_date = models.DateTimeField(
-        auto_now_add=True
+    tsn = models.ForeignKey(
+        'Units',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Taxonomic Serial Number",
+        db_column='tsn'
+    )
+    parent_tsn = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Parent Taxonomic Serial Number",
+        db_column='parent_tsn'
+    )
+    level = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Taxonomic Rank"
+    )
+    children_count = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name="Taxonomic Rank"
     )
 
+    class Meta:
+        verbose_name_plural = "Hierarchies"
+
     def __str__(self):
-        """
-        Returns a string representation of the division, using its geographic value.
-        """
-        return self.geographic_value
+        return self.hierarchy_string
