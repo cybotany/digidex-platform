34bab4d6618a92b31eb73c97be2c17da67a2e343
Raphael <puello322@gmail.com>
2023-11-25 15:54:43 -0700

    more fixes

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,6 +3,6 @@ from .views import DigitizationView, LandingView
 
 app_name = 'core'
 urlpatterns = [
-    path('landing', LandingView.as_view(), name='landing'),
+    path('', LandingView.as_view(), name='landing'),
     path('digitization/<str:secret_hash>/', DigitizationView.as_view(), name='digitization'),
 ]
