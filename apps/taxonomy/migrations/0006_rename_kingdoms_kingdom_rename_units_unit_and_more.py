ab2a74630cc6fb8de0e66c67351c84ea48bd15df
Raphael <puello322@gmail.com>
2023-11-25 15:38:25 -0700

    removing dependency

--- apps/taxonomy/migrations/0006_rename_kingdoms_kingdom_rename_units_unit_and_more.py
+++ apps/taxonomy/migrations/0006_rename_kingdoms_kingdom_rename_units_unit_and_more.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
         ('taxonomy', '0005_alter_hierarchy_options_and_more'),
     ]
 
