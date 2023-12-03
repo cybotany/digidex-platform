03089d0841afc867358e7e110685656c1172c714
Raphael <puello322@gmail.com>
2023-12-03 16:47:59 -0700

    more updates

--- apps/core/forms/create_digit_form.py
+++ apps/core/forms/create_digit_form.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.db import models
 from apps.core.models import Digit
+from apps.inventory.models import Link, Group
+from apps.taxonomy.models import Unit
+from django.contrib.auth.models import User
 
 
 class CreateDigitForm(forms.ModelForm):
@@ -7,9 +11,43 @@ class CreateDigitForm(forms.ModelForm):
     Form for updating an existing digit's details.
     """
 
+    name = forms.CharField(
+        max_length=50,
+        null=True,
+        blank=True,
+        help_text="A human-readable name for the digitized plant."
+    )
+    description = forms.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        help_text="A brief description of the digitized plant."
+    )
+    group = models.ForeignKey(
+        Group,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='digits',
+        help_text="The group to which the digitized plant belongs."
+    )
+    link = models.OneToOneField(
+        Link,
+        on_delete=models.CASCADE,
+        related_name='digit',
+        help_text="The NFC tag link associated with the digitized plant."
+    )
+    taxonomic_unit = forms.ModelChoiceField(
+        queryset=Unit.objects.none(),
+        required=False,
+        widget=forms.TextInput(attrs={'id': 'tsnField'})
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="User",
+        help_text="The user who created the digitized plant record."
+    )
+
     class Meta:
         model = Digit
         fields = ('name', 'description', 'group', 'link', 'taxonomic_unit', 'user')
-
-    def __init__(self, *args, **kwargs):
-        super(CreateDigitForm, self).__init__(*args, **kwargs)
