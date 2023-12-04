d227a3403d7553ce5ec4d3c4b62761f76a01557e
Raphael <puello322@gmail.com>
2023-12-03 19:35:51 -0700

    removing landing page

--- apps/core/views/__init__.py
+++ apps/core/views/__init__.py
@@ -3,7 +3,6 @@ from .digitization import DigitizationView
 from .grouping import GroupingView
 from .inventory import InventoryView
 
-from .landing_view import LandingView
 from .login_user import LoginUserView
 from .logout_user import LogoutUserView
 from .user_profile import UserProfileView
