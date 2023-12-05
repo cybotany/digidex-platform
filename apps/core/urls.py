405a980d3cd689f0bbb50e76f149ea19beaa989f
Raphael <puello322@gmail.com>
2023-12-04 19:30:32 -0700

    testing migrations

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from apps.core.views import LinkingView, DigitizationView, GroupingView, InventoryView, SignupUserView, LoginUserView, LogoutUserView, UserProfileView
+from apps.core.views import LandingView, LinkingView, DigitizationView, GroupingView, InventoryView, SignupUserView, LoginUserView, LogoutUserView, UserProfileView
 
 app_name = 'core'
 urlpatterns = [
-    path('', InventoryView.as_view(), name='landing'),
+    path('', LandingView.as_view(), name='landing'),
     path('link/<str:serial_number>/', LinkingView.as_view(), name='linking'),
     path('digitize/<int:link_id>/', DigitizationView.as_view(), name='digitization'),
     path('grouping/<int:pk>/', GroupingView.as_view(), name='grouping'),
@@ -11,5 +11,4 @@ urlpatterns = [
     path('signup/', SignupUserView.as_view(), name='signup'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
 ]
--- apps/core/views/__init__.py
+++ apps/core/views/__init__.py
@@ -2,8 +2,7 @@ from .linking import LinkingView
 from .digitization import DigitizationView
 from .grouping import GroupingView
 from .inventory import InventoryView
-
-from .login_user import LoginUserView
-from .logout_user import LogoutUserView
-from .user_profile import UserProfileView
-from .signup_user import SignupUserView
+from .landing import LandingView
+from .login import LoginUserView
+from .logout import LogoutUserView
+from .signup import SignupUserView
