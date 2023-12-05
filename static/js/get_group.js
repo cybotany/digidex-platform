f95d1b2d90bfa684584614548d3b3eab6c89db97
Raphael <puello322@gmail.com>
2023-12-04 19:20:10 -0700

    Updating to use webflow

--- static/js/get_group.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const pageSelector = document.getElementById('groupPageSelector');
-
-    pageSelector.addEventListener('change', function() {
-      window.location.href = '?page=' + this.value;
-    });
-});
