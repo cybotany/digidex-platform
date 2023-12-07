29f5c9c4dd09555cc12351b164f933b0f10dc3ed
Raphael <puello322@gmail.com>
2023-12-06 19:13:22 -0700

    More fixes

--- apps/core/views/login.py
+++ apps/core/views/login.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import AuthenticationForm
+from django.urls import reverse_lazy
 
 
 class LoginUserView(LoginView):
     form_class = AuthenticationForm
     template_name = 'login.html'
+    success_url = reverse_lazy('core:inventory')
