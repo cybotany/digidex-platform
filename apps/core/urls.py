ff8e88c25b8a91bbd769f182cc59b472f7c4614b
Raphael <puello322@gmail.com>
2023-12-09 14:26:45 -0700

    more updates

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -15,15 +15,15 @@ from apps.core.views import (ContactUsView,
 app_name = 'core'
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
-    path('garden/', GardenView.as_view(), name='garden'),
     path('signup/', SignupUserView.as_view(), name='signup'),
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
     path('contact/', ContactUsView.as_view(), name='contact'),
     path('company/', AboutUsView.as_view(), name='company'),
+    path('garden/', GardenView.as_view(), name='garden'),
     path('link/<str:serial_number>/', LinkingView.as_view(), name='linking'),
     path('digitize/<int:link_id>/', DigitizationView.as_view(), name='digitization'),
     path('digit/<int:pk>/', DigitView.as_view(), name='digit'),
     path('digit/delete/<int:pk>/', DeleteDigitView.as_view(), name='delete-digit'),
-    path('grouped_digits/<int:group_id>/', GroupedDigitsView.as_view(), name='grouped-digits'),
+    path('group/<int:group_id>/', GroupedDigitsView.as_view(), name='grouped-digits'),
 ]
