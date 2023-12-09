ecd9cde5f7e33ed3d72904a9d05e179395093b31
Raphael <puello322@gmail.com>
2023-12-08 20:20:45 -0700

    still trying

--- apps/core/views/grouping.py
+++ apps/core/views/grouping.py
@@ -1,6 +1,6 @@
 from django.views.generic.detail import DetailView
 from apps.inventory.models import Group
-from apps.core.models import Digit
+from apps.inventory.models import Link
 
 class GroupingView(DetailView):
     model = Group
@@ -9,5 +9,5 @@ class GroupingView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         group = self.object
-        context['digits'] = Digit.objects.filter(group=group)
+        context['links'] = Link.objects.filter(group=group)
         return context
