28a7787438cbbe7c2101bdcd76a98337ef6a64d8
Raphael <puello322@gmail.com>
2023-12-08 21:54:38 -0700

    more updates

--- apps/core/views/grouped_digits.py
+++ apps/core/views/grouped_digits.py
@@ -4,16 +4,14 @@ from apps.inventory.models import Link
 from apps.core.models import Digit
 
 
-class GroupedDigitsView(LoginRequiredMixin, ListView):
+class GroupedLinksView(LoginRequiredMixin, ListView):
     context_object_name = 'digits'
     template_name = 'grouped_digits.html'
 
     def get_queryset(self):
-        """ Overriding to get Digits mapped to each active link in the current group """
+        """ Overriding to get Digits mapped to active links in the current group """
         group_id = self.kwargs.get('group_id')
-        # Get the primary keys of Digits from active links in the group
         digit_pks = Link.objects.filter(group_id=group_id, active=True).values_list('digit', flat=True)
-        # Retrieve and return the associated Digit instances
         return Digit.objects.filter(pk__in=digit_pks)
 
     def get_context_data(self, **kwargs):
