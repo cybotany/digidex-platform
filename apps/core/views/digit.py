79388145ebc8ca90596fd9303c54c2840e934b13
Raphael <puello322@gmail.com>
2023-12-08 20:13:14 -0700

    More updates

--- apps/core/views/digit.py
+++ apps/core/views/digit.py
@@ -3,14 +3,10 @@ from apps.core.models import Digit
 
 
 class DigitView(DetailView):
-    """
-    View for rendering the page used to show details about a specific registered digit.
-    """
     model = Digit
     template_name = 'digit.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['watering_events'] = self.object.waterings.all().order_by('-timestamp')
-        context['fertilization_events'] = self.object.fertilizations.all().order_by('-timestamp')
+        context['journal_entries'] = self.object.journal_entries.all()
         return context
