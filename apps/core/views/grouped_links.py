ccd84c00d66c0a7d3a675814d2ed21a8e66c6624
Raphael <puello322@gmail.com>
2023-12-08 20:42:51 -0700

    Fixing group links

---
+++ apps/core/views/grouped_links.py
@@ -0,0 +1,18 @@
+from django.views.generic.list import ListView
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from apps.inventory.models import Link
+
+
+class GroupedLinksView(LoginRequiredMixin, ListView):
+    model = Link
+    template_name = 'grouped_links.html'
+
+    def get_queryset(self):
+        """ Overriding to get links for the current group """
+        group_id = self.kwargs.get('group_id')
+        return Link.objects.filter(group_id=group_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
