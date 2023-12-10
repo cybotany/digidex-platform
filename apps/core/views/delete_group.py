1526e22cfb62b02181fd278be7994fd30b3c30b3
Raphael <puello322@gmail.com>
2023-12-09 17:15:18 -0700

    ?

--- apps/core/views/delete_group.py
+++ apps/core/views/delete_group.py
@@ -1,13 +1,10 @@
 from django.views import View
 from django.shortcuts import get_object_or_404, redirect
 from apps.inventory.models import Group
-from apps.core.models import Digit
 
 
 class DeleteGroupView(View):
     def post(self, request, pk):
         group = get_object_or_404(Group, pk=pk)
-        if Digit.objects.filter(group=group).exists():
-            return redirect('core:group-detail', pk=pk)
         group.delete()
         return redirect('core:garden')
