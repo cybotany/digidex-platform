0ea72887536a0fe661bfc19a896ab57cdbf3408d
Raphael <puello322@gmail.com>
2023-12-29 22:03:16 -0700

    fixing settings again

--- apps/utils/constants.py
+++ apps/utils/constants.py
@@ -1,16 +1,2 @@
 MAX_GROUP = 15
 MAX_GROUP_CAPACITY = 20
-
-PLANT = 'plant'
-ANIMAL = 'animal'
-
-ACTIVITY_STATUS = [
-    ('register', 'Registered'),
-    ('update', 'Updated'),
-    ('delete', 'Deleted'),
-]
-
-ACTIVITY_TYPE = [
-    (PLANT, 'Plant'),
-    (ANIMAL, 'Animal'),
-]
--- digit/settings.py
+++ digit/settings.py
@@ -32,7 +32,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = 'public-read'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATIC_ROOT = 'static/'
