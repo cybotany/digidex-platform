204ad065afce8c11c88492d5cf7cfacfbb9b6fb9
Raphael <puello322@gmail.com>
2024-01-01 14:31:58 -0700

    Removing groups from api app

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from apps.api.views import GetGroup, GetTSN, CreateLink
+from apps.api.views import GetTSN, CreateLink
 
 app_name = 'api'
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('get/group/<int:group_id>/', GetGroup.as_view(), name='get-group'),
     path('get/tsn/', GetTSN.as_view(), name='get-tsn'),
     path('create/link/<str:uid>/', CreateLink.as_view(), name='create-link'),
 ]
--- apps/api/views/__init__.py
+++ apps/api/views/__init__.py
@@ -1,3 +1,2 @@
 from .get_tsn import GetTSN
-from .get_group import GetGroup
 from .create_link import CreateLink
