73eaa0c69ad04515b82e4402b89206330298ddea
Raphael <puello322@gmail.com>
2024-01-01 15:53:13 -0700

    more updates

---
+++ apps/inventory/forms/create_digit_form.py
@@ -0,0 +1,15 @@
+from django import forms
+from apps.inventory.models import Digit
+
+
+class CreateDigitForm(forms.ModelForm):
+    """
+    Form for updating an existing digit's details.
+    """
+
+    class Meta:
+        model = Digit
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(CreateDigitForm, self).__init__(*args, **kwargs)
