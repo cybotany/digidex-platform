05bcf22b94d8118a6ea316fbd9bc8916b8a22673
Raphael <puello322@gmail.com>
2024-01-01 18:01:08 -0700

    Fixing namespace for main urls.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,7 +3,7 @@ from apps.main.views import (ContactUsView,
                              AboutUsView,
                              LandingView)
 
-app_name = 'core'
+app_name = 'main'
 urlpatterns = [
     path('', LandingView.as_view(), name='landing'),
     path('contact/', ContactUsView.as_view(), name='contact'),
