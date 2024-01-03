02ef8bbe65a5ca91a3dd7b53c246f3d4cd9bd121
Raphael <puello322@gmail.com>
2024-01-02 17:07:58 -0700

    fixinig app name for NFC tags

--- apps/nfc/urls.py
+++ apps/nfc/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import LinkingView
 
-app_name = 'inventory'
+app_name = 'nfc'
 urlpatterns = [
     path('link/', LinkingView.as_view(), name='linking'),
 ]
