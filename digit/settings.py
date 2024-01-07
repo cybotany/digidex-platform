68c9f2a624943b9cbd27c00b287e251af7c8b812
Raphael <puello322@gmail.com>
2024-01-06 17:51:20 -0700

    more updates

--- digit/settings.py
+++ digit/settings.py
@@ -42,7 +42,7 @@ if DJANGO_ENV == 'production':
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
     DEBUG = True
-    ALLOWED_HOSTS = ["localhost", "10.0.0.218:8080"]
+    ALLOWED_HOSTS = ["localhost", "10.0.0.218"]
 
     # Local static files settings
     STATIC_URL = '/static/'
