147e7b7e34deec129d807bba29e1496a058cf75a
Raphael <puello322@gmail.com>
2024-01-08 21:32:07 -0700

    more updates

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('email-confirmation/', EmailConfirmationView.as_view(), name='confirm-email'),
     path('email-verification/<str:token>/', EmailVerificationView.as_view(), name='verify-email'),
-    path('reset-password/', DigitPasswordResetView.as_view(), name ='password-reset'),
-    path('reset-password-sent/', DigitPasswordResetSentView.as_view(), name ='password-reset-sent'),
-    path('reset-password/<uidb64>/<token>/', DigitPasswordResetConfirmationView.as_view(), name ='password-reset-confirm'),
-    path('reset-password-complete/', DigitPasswordResetCompleteView.as_view(), name ='password-reset-complete'),
+    path('reset-password/', DigitPasswordResetView.as_view(), name ='password_reset'),
+    path('reset-password-sent/', DigitPasswordResetSentView.as_view(), name ='password_reset_done'),
+    path('reset-password/<uidb64>/<token>/', DigitPasswordResetConfirmationView.as_view(), name ='password_reset_confirm'),
+    path('reset-password-complete/', DigitPasswordResetCompleteView.as_view(), name ='password_reset_complete'),
 ]
