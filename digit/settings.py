2bfca7c7bfd445bd4e5a2a5b0fbd809c7556ca7c
Raphael <puello322@gmail.com>
2024-01-11 18:16:25 -0700

    updating settings to use cdn

--- digit/settings.py
+++ digit/settings.py
@@ -18,14 +18,14 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', '')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL', '')
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_CUSTOM_URL = os.environ.get('AWS_S3_CUSTOM_URL', '')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = '/var/www/digidex.app/static'
 AWS_LOCATION = 'static'
-STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
+STATIC_URL = f'{AWS_S3_CUSTOM_URL}/{AWS_LOCATION}/'
 STATIC_ROOT = '/var/www/digidex.app/static'
 
 # Environment specific settings
