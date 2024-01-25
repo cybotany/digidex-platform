55689836a4a3ac8791941c735b9646dabaa0bb75
Raphael <puello322@gmail.com>
2024-01-25 10:15:41 -0700

    Refactor API URLs and update TSN functionality

--- digit/api/urls.py
+++ digit/api/urls.py
@@ -6,6 +6,6 @@ app_name = 'api'
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('tsn/', GetTSN.as_view(), name='get-tsn'),
+    path('get-tsn/', GetTSN.as_view(), name='get-tsn'),
     path('create/link/<str:serial_number>/', CreateLink.as_view(), name='create-link'),
 ]
