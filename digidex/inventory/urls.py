1714e4120d717fdb48827f20d6169f2f2db26f15
Raphael <puello322@gmail.com>
2024-01-26 13:09:41 -0700

    Update URLs in inventory/urls.py

--- digidex/inventory/urls.py
+++ digidex/inventory/urls.py
@@ -4,9 +4,10 @@ from .views import (DigitLinkView, DigitCreationView, DigitStorageView, DigitDet
 app_name = 'inventory'
 urlpatterns = [
     path('digit-storage', DigitStorageView.as_view(), name='storage'),
-    path('link-digit/<uuid:link_uuid>/', DigitLinkView.as_view(), name='linking'),
-    path('view-digit/<uuid:digit_uuid>/', DigitDetailsView.as_view(), name='details'),
-    path('create-digit/<uuid:digit_uuid>/', DigitCreationView.as_view(), name='creation'),
+    path('link/<uuid:link_uuid>/', DigitLinkView.as_view(), name='linking'),
+    path('create-digit/<uuid:link_uuid>/', DigitCreationView.as_view(), name='creation'),
     path('delete-digit/<uuid:digit_uuid>/', DigitDeletionView.as_view(), name='deletion'),
+    path('view-digit/<uuid:digit_uuid>/', DigitDetailsView.as_view(), name='details'),
     path('modify-digit/<uuid:digit_uuid>/', DigitModificationView.as_view(), name='modification'),
+
 ]
