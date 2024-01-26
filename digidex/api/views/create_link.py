02d4e2b651d5a615deff1600890794d89b4725f3
Raphael <puello322@gmail.com>
2024-01-26 10:07:27 -0700

    Update import statement for Link model

--- digidex/api/views/create_link.py
+++ digidex/api/views/create_link.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from digidex.nfc.models import Link
+from digidex.inventory.models import Link
 
 
 class CreateLink(APIView):
