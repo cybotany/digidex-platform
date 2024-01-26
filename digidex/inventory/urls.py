50014e4643a3da9c5795ee643af67815d75906f7
Raphael <puello322@gmail.com>
2024-01-26 13:42:50 -0700

    Reorder views import and update URL patterns
    Refactor DigitDeletionView to use DeleteView
    
    Update digit-modification-page.html to use new delete URL

--- digidex/inventory/urls.py
+++ digidex/inventory/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import (DigitLinkView, DigitCreationView, DigitStorageView, DigitDetailsView, DigitDeletionView, DigitModificationView)
+from .views import (DigitStorageView, DigitLinkView, DigitCreationView, DigitDetailsView, DigitModificationView, DigitDeletionView)
 
 app_name = 'inventory'
 urlpatterns = [
     path('digit-storage', DigitStorageView.as_view(), name='storage'),
     path('link/<uuid:link_uuid>/', DigitLinkView.as_view(), name='linking'),
     path('create-digit/<uuid:link_uuid>/', DigitCreationView.as_view(), name='creation'),
-    path('delete-digit/<uuid:digit_uuid>/', DigitDeletionView.as_view(), name='deletion'),
     path('view-digit/<uuid:digit_uuid>/', DigitDetailsView.as_view(), name='details'),
     path('modify-digit/<uuid:digit_uuid>/', DigitModificationView.as_view(), name='modification'),
+    path('delete-digit/<uuid:digit_uuid>/', DigitDeletionView.as_view(), name='deletion'),
 ]
--- digidex/inventory/views/digit_deletion_page.py
+++ digidex/inventory/views/digit_deletion_page.py
@@ -1,27 +1,30 @@
-from django.db import transaction
-from django.views import View
-from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
 from digidex.inventory.models import Digit
 from digidex.accounts.models import Activity
 
 
-class DigitDeletionView(LoginRequiredMixin, View):
-    def post(self, request, uuid):
-        with transaction.atomic():
-            digit = get_object_or_404(Digit, uuid=uuid)
+class DigitDeletionView(LoginRequiredMixin, DeleteView):
+    model = Digit
+    success_url = reverse_lazy('inventory:storage')
 
-            if digit.nfc_link:
-                link = digit.nfc_link
-                link.reset_to_default()
+    def get_object(self, queryset=None):
+        digit_uuid = self.kwargs.get('digit_uuid')
+        return get_object_or_404(Digit, uuid=digit_uuid)
 
-                Activity.objects.create(
-                    user=request.user,
-                    activity_type='Plant',
-                    activity_status='Deleted',
-                    content=f'Deleted Plant {digit.name}'
-                )
+    def delete(self, request, *args, **kwargs):
+        digit = self.get_object()
+        if digit.nfc_link:
+            link = digit.nfc_link
+            link.reset_to_default()
 
-                digit.delete()
+        Activity.objects.create(
+            user=request.user,
+            activity_type='Plant',
+            activity_status='Deleted',
+            content=f'Deleted Plant {digit.name}'
+        )
 
-        return redirect('inventory:storage')
+        return super(DigitDeletionView, self).delete(request, *args, **kwargs)
