812f47c12b99106aa1dffeb86808d03e719df0c1
Raphael <puello322@gmail.com>
2024-01-28 12:59:55 -0700

    Update DigitLinkView to use NFC model instead of Link model

--- digidex/inventory/views/digit_link.py
+++ digidex/inventory/views/digit_link.py
@@ -4,12 +4,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import SingleObjectMixin
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, render
-from digidex.inventory.models import Digit, Link
+from digidex.inventory.models import Digit
+from digidex.link.models import NFC
 from digidex.inventory.forms import DigitForm
 
 
 class DigitLinkView(LoginRequiredMixin, SingleObjectMixin, View):
-    model = Link
+    model = NFC
 
     def get_object(self, queryset=None):
         # Overriding the method to use 'uuid' instead of the primary key
@@ -21,27 +22,27 @@ class DigitLinkView(LoginRequiredMixin, SingleObjectMixin, View):
             # Get the single item from the filtered queryset
             obj = queryset.get(**{self.uuid: uuid})
         except queryset.model.DoesNotExist:
-            raise Http404("No Link found matching the query")
+            raise Http404("No NFC found matching the query")
         return obj
 
     def get(self, request, *args, **kwargs):
-        link = self.get_object()
-        link.increment_counter()
+        nfc = self.get_object()
+        nfc.increment_counter()
 
-        if not link.active:
-            return self.handle_digit_creation(request, link)
+        if not nfc.active:
+            return self.handle_digit_creation(request, nfc)
         else:
-            return self.handle_digit_details(request, link)
+            return self.handle_digit_details(request, nfc)
 
     def post(self, request, *args, **kwargs):
         link = self.get_object()
         # Directly call the handle_digit_creation method for POST requests
         return self.handle_digit_creation(request, link)
 
-    def handle_digit_creation(self, request, link):
+    def handle_digit_creation(self, request, nfc):
         form = DigitForm(request.POST)
         if form.is_valid():
-            digit = Digit.create_digit(form.cleaned_data, link, request.user)
+            digit = Digit.create_digit(form.cleaned_data, nfc, request.user)
             return render(request, 'main/digit-details-page.html', {'digit': digit})
         else:
             # Pass the form with errors to the context
@@ -51,11 +52,11 @@ class DigitLinkView(LoginRequiredMixin, SingleObjectMixin, View):
             }
             return render(request, 'main/digit-creation-page.html', context)
 
-    def handle_digit_details(self, request, link):
-        if not link.check_access(request.user):
+    def handle_digit_details(self, request, nfc):
+        if not nfc.check_access(request.user):
             return HttpResponseForbidden("Unauthorized access")
 
-        digit = get_object_or_404(Digit, nfc_link=link)
+        digit = get_object_or_404(Digit, nfc_link=nfc)
         # Render the digit details template (you can use a different template if needed)
         return render(request, 'main/digit-details-page.html', {'digit': digit})
 
