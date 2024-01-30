782f7f1e90845704d2b669584a4b6a794c803547
Raphael <puello322@gmail.com>
2024-01-29 19:52:40 -0700

    Refactor Lottie animation initialization

--- digidex/main/static/main/js/toggle-lottie.js
+++ digidex/main/static/main/js/toggle-lottie.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var lottieElements = document.querySelectorAll('.lottie-animation-1, .lottie-animation-2, .lottie-animation-2-blur');
+  // Function to initialize Lottie animations
+  function initLottieAnimations(selector) {
+    var lottieElements = document.querySelectorAll(selector);
 
-  lottieElements.forEach(function(element) {
-    var animationPath = element.getAttribute('data-src');
-    var loop = element.getAttribute('data-loop') === '1';
-    var autoplay = element.getAttribute('data-autoplay') === '1';
+    lottieElements.forEach(function(element) {
+      var animationPath = element.getAttribute('data-src');
+      var loop = element.getAttribute('data-loop') === '1';
+      var autoplay = element.getAttribute('data-autoplay') === '1';
 
-    var animation = lottie.loadAnimation({
-      container: element,
-      renderer: 'svg',
-      loop: loop,
-      autoplay: autoplay,
-      path: animationPath
+      lottie.loadAnimation({
+        container: element,
+        renderer: 'svg',
+        loop: loop,
+        autoplay: autoplay,
+        path: animationPath
+      });
     });
-  });
+  }
+
+  // Initialize animations for the first set of elements
+  initLottieAnimations('.lottie-animation-1, .lottie-animation-2, .lottie-animation-2-blur');
+
+  // Initialize animations for the new banner elements
+  initLottieAnimations('.lottie-animation-1-banner, .lottie-animation-2-banner, .lottie-animation-2-blur-banner');
 });
