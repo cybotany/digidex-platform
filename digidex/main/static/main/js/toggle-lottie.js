06d6a8be4150a599cf599aaed21c74c98ddf5289
Raphael <puello322@gmail.com>
2024-01-29 17:20:02 -0700

    Add Lottie animation to landing page

---
+++ digidex/main/static/main/js/toggle-lottie.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var lottieElements = document.querySelectorAll('.lottie-animation-1, .lottie-animation-2, .lottie-animation-2-blur');
+
+  lottieElements.forEach(function(element) {
+    var animationPath = element.getAttribute('data-src');
+    var loop = element.getAttribute('data-loop') === '1';
+    var autoplay = element.getAttribute('data-autoplay') === '1';
+
+    var animation = lottie.loadAnimation({
+      container: element,
+      renderer: 'svg',
+      loop: loop,
+      autoplay: autoplay,
+      path: animationPath
+    });
+  });
+});
