0aee38795ca3cd665c555db6a7dd925511cec242
Raphael <puello322@gmail.com>
2024-01-30 20:51:44 -0700

    Remove unused links from navigation and footer

--- digidex/main/urls.py
+++ digidex/main/urls.py
@@ -21,12 +21,13 @@ urlpatterns = [
     path('thank-you/', ThankYouView.as_view(), name='thanks'),
 
     path('pricing', PricingView.as_view(), name='pricing'),
-    path('solutions/', SolutionsView.as_view(), name='soltions'),
+    path('solutions/', SolutionsView.as_view(), name='solutions'),
     path('blog/', BlogView.as_view(), name='blog'),
     path('post-detail/', DetailPostView.as_view(), name='post-detail'),
 
     path('category-detail/', DetailCategoryView.as_view(), name='category-detail'),
     path('product-detail/', DetailProductView.as_view(), name='product-detail'),
+
     path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('checkout/paypal/', PayPalCheckoutView.as_view(), name='checkout-paypal'),
     path('order-confirmation/', OrderConfirmationView.as_view(), name='order-confirmation'),
