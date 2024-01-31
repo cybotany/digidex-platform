307f5d885a064ad70e02912a37118402a42e80ff
Raphael <puello322@gmail.com>
2024-01-30 22:50:58 -0700

    Refactor digit_creation_view.py

--- digidex/inventory/views/digit_creation_view.py
+++ digidex/inventory/views/digit_creation_view.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from digidex.utils.helpers import BaseNFCView
 from digidex.inventory.models import Digit
 from digidex.inventory.forms import DigitForm
@@ -13,16 +13,8 @@ class DigitCreationView(BaseNFCView, CreateView):
 
     def form_valid(self, form):
         self.object = form.save()
-        return super(DigitCreationView, self).form_valid(form)
+        return reverse_lazy('inventory:digit-details', kwargs={'serial_number': self.object.nfc_link.serial_number})
 
     def form_invalid(self, form):
         # Render the template with the invalid form.
         return render(self.request, self.template_name, {'form': form})
-
-    def get_success_url(self):
-        """
-        Returns the URL to redirect to after successfully creating a digit.
-        This URL is constructed based on the digit's associated NFC's serial number.
-        """
-        # Ensure that self.object is set before calling this method
-        return reverse_lazy('inventory:digit-details', kwargs={'serial_number': self.object.nfc_link.serial_number})
