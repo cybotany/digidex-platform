31064b163f6ee4f98940e5eb54b3d37e3acdfe43
Raphael <puello322@gmail.com>
2024-01-30 22:45:57 -0700

    Update URL paths for digit creation and details views
    Update URL path for digit journal view

--- digidex/inventory/urls.py
+++ digidex/inventory/urls.py
@@ -4,8 +4,8 @@ from digidex.inventory.views import (DigitStorageView, DigitCreationView, DigitD
 app_name = 'inventory'
 urlpatterns = [
     path('storage-system/', DigitStorageView.as_view(), name='digit-storage'),
-    path('<str:serial_number>/creation', DigitCreationView.as_view(), name='digit-creation'),
-    path('<str:serial_number>/details', DigitDetailView.as_view(), name='digit-details'),
+    path('<str:serial_number>/creation/', DigitCreationView.as_view(), name='digit-creation'),
+    path('<str:serial_number>/details/', DigitDetailView.as_view(), name='digit-details'),
     path('<str:serial_number>/modification/', DigitModificationView.as_view(), name='digit-modification'),
     path('<str:serial_number>/deletion/', DigitDeletionView.as_view(), name='digit-deletion'),
 ]
--- digidex/journal/urls.py
+++ digidex/journal/urls.py
@@ -3,5 +3,5 @@ from digidex.journal.views import DigitJournalView
 
 app_name = 'journal'
 urlpatterns = [
-    path('digit/<str:serial_number>/journal', DigitJournalView.as_view(), name='digit-journal'),
+    path('digit/<str:serial_number>/', DigitJournalView.as_view(), name='digit-journal'),
 ]
