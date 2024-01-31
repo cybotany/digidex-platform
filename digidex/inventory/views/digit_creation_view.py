ca590fb6aaf2b9b4a0d6743ba4630e9d2097c21f
Raphael <puello322@gmail.com>
2024-01-30 23:16:22 -0700

    Refactor DigitCreationView to use class-based views and handle form submission

--- digidex/inventory/views/digit_creation_view.py
+++ digidex/inventory/views/digit_creation_view.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
-from django.views.generic.edit import CreateView
 from digidex.utils.helpers import BaseNFCView
 from digidex.inventory.models import Digit
 from digidex.inventory.forms import DigitForm
 
 
-class DigitCreationView(BaseNFCView, CreateView):
-    model = Digit
-    form_class = DigitForm
+class DigitCreationView(BaseNFCView):
     template_name = 'inventory/digit-creation-page.html'
 
-    def form_valid(self, form):
-        nfc = self.get_object()
-        self.object = Digit.create_digit(form.cleaned_data, nfc, self.request.user)
-        return redirect('inventory:digit-details', kwargs={'serial_number': nfc.serial_number})
+    def get(self, request, *args, **kwargs):
+        form = DigitForm()
+        return render(request, self.template_name, {'form': form})
 
-    def form_invalid(self, form):
-        # Render the template with the invalid form.
-        return render(self.request, self.template_name, {'form': form})
+    def post(self, request, *args, **kwargs):
+            # Process the form data for a POST request
+            form = DigitForm(request.POST)
+            if form.is_valid():
+                nfc = self.get_object()
+                # Use the custom creation method from the Digit model
+                digit = Digit.create_digit(form.cleaned_data, nfc, request.user)
+                # Redirect to the success URL after creating the digit
+                return redirect('inventory:digit-details', serial_number=nfc.serial_number)
+            else:
+                # If the form is invalid, render the form with errors
+                return render(self.request, self.template_name, {'form': form})
