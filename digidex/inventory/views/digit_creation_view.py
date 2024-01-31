5c371f1f41446d477669ba0411e01feb424ba7a1
Raphael <puello322@gmail.com>
2024-01-30 23:23:12 -0700

    Refactor digit creation view to use NFC model and add login required mixin

--- digidex/inventory/views/digit_creation_view.py
+++ digidex/inventory/views/digit_creation_view.py
@@ -1,25 +1,41 @@
-from django.shortcuts import render, redirect
-from digidex.utils.helpers import BaseNFCView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.http import Http404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.detail import SingleObjectMixin
 from digidex.inventory.models import Digit
 from digidex.inventory.forms import DigitForm
+from digidex.link.models import NFC
 
 
-class DigitCreationView(BaseNFCView):
+class DigitCreationView(LoginRequiredMixin, SingleObjectMixin, View):
+    model = NFC
     template_name = 'inventory/digit-creation-page.html'
 
+    def get_object(self, queryset=None):
+        queryset = queryset or self.get_queryset()
+        serial_number = self.kwargs.get('serial_number')
+        if serial_number is None:
+            raise Http404("No serial number provided")
+        try:
+            obj = queryset.get(serial_number=serial_number)
+        except queryset.model.DoesNotExist:
+            raise Http404("No NFC found matching the query")
+        return obj
+
     def get(self, request, *args, **kwargs):
+        nfc = self.get_object()
         form = DigitForm()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
             # Process the form data for a POST request
+            nfc = self.get_object()
             form = DigitForm(request.POST)
             if form.is_valid():
-                nfc = self.get_object()
-                # Use the custom creation method from the Digit model
                 digit = Digit.create_digit(form.cleaned_data, nfc, request.user)
-                # Redirect to the success URL after creating the digit
-                return redirect('inventory:digit-details', serial_number=nfc.serial_number)
+                return reverse_lazy('inventory:digit-details', serial_number=nfc.serial_number)
             else:
                 # If the form is invalid, render the form with errors
                 return render(self.request, self.template_name, {'form': form})
