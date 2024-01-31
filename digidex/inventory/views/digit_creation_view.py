9041f895087fef973f1d36d3b437ce0e45190c88
Raphael <puello322@gmail.com>
2024-01-30 22:37:05 -0700

    Add get_success_url method to DigitCreationView

--- digidex/inventory/views/digit_creation_view.py
+++ digidex/inventory/views/digit_creation_view.py
@@ -1,5 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from digidex.utils.helpers import BaseNFCView
 from digidex.inventory.models import Digit
 from digidex.inventory.forms import DigitForm
@@ -11,13 +12,17 @@ class DigitCreationView(BaseNFCView, CreateView):
     template_name = 'inventory/digit-creation-page.html'
 
     def form_valid(self, form):
-        nfc = self.get_object()
-
-        if not nfc.active:
-            form.instance.nfc_link = nfc
-            return super().form_valid(form)
-        # Handle cases where NFC is already active
+        self.object = form.save()
+        return super(DigitCreationView, self).form_valid(form)
 
     def form_invalid(self, form):
         # Render the template with the invalid form.
         return render(self.request, self.template_name, {'form': form})
+
+    def get_success_url(self):
+        """
+        Returns the URL to redirect to after successfully creating a digit.
+        This URL is constructed based on the digit's associated NFC's serial number.
+        """
+        # Ensure that self.object is set before calling this method
+        return reverse_lazy('inventory:digit-details', kwargs={'serial_number': self.object.nfc_link.serial_number})
