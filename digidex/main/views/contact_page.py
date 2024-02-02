1778fc454b75eba8e30f265efd046568bae815dd
Raphael <puello322@gmail.com>
2024-02-02 16:53:08 -0700

    Update contact page view

--- digidex/main/views/contact_page.py
+++ digidex/main/views/contact_page.py
@@ -1,11 +1,8 @@
 from django.views import View
 from django.shortcuts import render, redirect
-from django.core.mail import send_mail
-from django.conf import settings
 from digidex.main.forms import ContactForm
 
-
-class ContactUsView(View):
+class ContactView(View):
     template_name = 'main/contact-page.html'
 
     def get(self, request, *args, **kwargs):
@@ -15,14 +12,7 @@ class ContactUsView(View):
     def post(self, request, *args, **kwargs):
         form = ContactForm(request.POST)
         if form.is_valid():
-            new_contact = form.save()
-
-        send_mail(
-            subject=f"New Contact Us Message from {new_contact.name}",
-            message=new_contact.message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[settings.EMAIL_HOST_USER],
-            fail_silently=False,
-        )
-
-        return redirect('main:thanks')
+            form.save()
+            return redirect('main:thanks')
+        else:
+            return render(request, self.template_name, {'form': form})
