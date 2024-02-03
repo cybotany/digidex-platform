f21bbd1e3cafd9388fc0d1f687fbd968f112f8ca
Raphael <puello322@gmail.com>
2024-02-03 15:56:01 -0700

    Update contact page layout and view

--- digidex/main/views/contact_page.py
+++ digidex/main/views/contact_page.py
@@ -7,15 +7,6 @@ class ContactView(FormView):
     form_class = ContactForm
     success_url = reverse_lazy('main:thanks')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'subtitle': 'Contact',
-            'heading': "Contact us for assistance",
-            'paragraph': "We're here to help!"
-        })
-        return context
-
     def form_valid(self, form):
         new_contact = form.save()
         new_contact.send_email()
