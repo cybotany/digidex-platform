a0786d124976d2aee62e5ee66791321e67e6d0be
Raphael <puello322@gmail.com>
2024-02-03 17:16:51 -0700

    Add templates directory to Django settings

--- digidex/settings.py
+++ digidex/settings.py
@@ -126,7 +126,7 @@ ROOT_URLCONF = 'digidex.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
