22b8459d104ddfed912436c8319cb5c7058bc7cd
Raphael <puello322@gmail.com>
2024-02-06 15:10:44 -0700

    Fix import statements in user.py and email_verification_view.py

--- digidex/accounts/models/user.py
+++ digidex/accounts/models/user.py
@@ -2,7 +2,8 @@ from django.db import models, transaction
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.urls import reverse
-from django.utils.http import urlencode, urlsafe_base64_encode, force_bytes
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.conf import settings
 from django.core.mail import send_mail
 import uuid
--- digidex/accounts/views/email_verification_view.py
+++ digidex/accounts/views/email_verification_view.py
@@ -1,7 +1,8 @@
 from django.views import View
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.http import urlsafe_base64_decode, force_text
+from django.utils.encoding import force_text
+from django.utils.http import urlsafe_base64_decode
 from digidex.accounts.models import User
 
 
