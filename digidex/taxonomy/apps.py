80529a89bbda96f08906a5ba9eda2eaccfdff7e3
Raphael <puello322@gmail.com>
2024-02-09 16:50:38 -0700

    Remove unused import and signal registration in TaxonomyConfig

--- digidex/taxonomy/apps.py
+++ digidex/taxonomy/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class TaxonomyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'digidex.taxonomy'
-
-    def ready(self):
-            import digidex.taxonomy.signals
