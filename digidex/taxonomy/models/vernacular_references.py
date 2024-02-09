5e9285f4be661e359ee06ff23cb24fd6f1def7f8
Raphael <puello322@gmail.com>
2024-02-09 15:03:41 -0700

    Change vernacular field to ForeignKey in VernacularReferences model

--- digidex/taxonomy/models/vernacular_references.py
+++ digidex/taxonomy/models/vernacular_references.py
@@ -8,7 +8,7 @@ class VernacularReferences(models.Model):
 
     Attributes:
         tsn (ForeignKey): The Taxonomic Serial Number (TSN) of the taxonomic unit. Links to a 'Unit' model.
-        vernacular (IntegerField): Unique identifier for the vernacular name entry.
+        vernacular (ForeignKey): Unique identifier for the vernacular name entry.
         reference_prefix (CharField): Prefix indicating the type of reference associated with the vernacular.
         reference_id (IntegerField): Identifier for the specific reference providing evidence for the vernacular.
         last_modified (DateTimeField): Date and time when the record was last modified.
@@ -19,10 +19,9 @@ class VernacularReferences(models.Model):
         db_column="tsn",
         help_text="Taxonomic Serial Number (TSN) for the Taxonomic Unit."
     )
-    vernacular = models.IntegerField(
-        #'taxonomy.Vernacular',
-        db_column="vernacular_id",
-        #on_delete=models.CASCADE,
+    vernacular = models.ForeignKey(
+        'taxonomy.Vernacular',
+        on_delete=models.CASCADE,
         help_text="Unique identifier for the vernacular name entry."
     )
     reference_prefix = models.CharField(
