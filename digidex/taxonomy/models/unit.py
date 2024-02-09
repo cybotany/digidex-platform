62a69436f588ee4f11eab7665c5c8210c8b4c501
Raphael <puello322@gmail.com>
2024-02-09 11:24:28 -0700

    Refactor parent field in Unit model to use ForeignKey

--- digidex/taxonomy/models/unit.py
+++ digidex/taxonomy/models/unit.py
@@ -147,10 +147,10 @@ class Unit(models.Model):
         verbose_name="Initial Time Stamp",
         help_text="Date and time at which an occurrence of Taxonomic Units is initially loaded into the ITIS database."
     )
-    parent = models.IntegerField(
-        #'self',
-        #on_delete=models.SET_NULL,
-        #related_name='children',
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name='children',
         null=True,
         blank=True,
         verbose_name="Parent TSN",
