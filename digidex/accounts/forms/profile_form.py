3383e5586b159e13604f5c396e77dec5a666a001
Raphael <puello322@gmail.com>
2024-02-25 09:03:15 -0700

    Refactor profile form to improve readability and maintainability

--- digidex/accounts/forms/profile_form.py
+++ digidex/accounts/forms/profile_form.py
@@ -2,28 +2,35 @@ from django import forms
 from digidex.accounts.models import Profile
 
 class ProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(
+        label='Upload Avatar',
+        widget=forms.FileInput(attrs={
+            'accept': 'image/*',
+        })
+    )
+    bio = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'text-field base-input',
+            'placeholder': 'Short biography',
+            'rows': 4,
+        }),
+        required=False
+    )
+    location = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'text-field base-input',
+            'placeholder': 'Location',
+        }),
+        required=False
+    )
+    is_public = forms.BooleanField(
+        label='Make Profile Public?',
+        widget=forms.CheckboxInput(attrs={
+            'class': 'checkbox-input',
+        }),
+        required=False 
+    )
+
     class Meta:
         model = Profile
         fields = ('avatar', 'bio', 'location', 'is_public',)
-        widgets = {
-            'avatar': forms.FileInput(attrs={
-                'accept': 'image/*',
-                'label': 'Upload Avatar',
-            }),
-            'bio': forms.Textarea(attrs={
-                'class': 'text-field base-input',
-                'placeholder': 'Short biography',
-                'rows': 4,
-            }),
-            'location': forms.TextInput(attrs={
-                'class': 'text-field base-input',
-                'placeholder': 'Location',
-            }),
-            'is_public': forms.CheckboxInput(attrs={
-                'class': 'checkbox-input',
-                'label': 'Make Profile Public?',
-            }),
-        }
-
-    def __init__(self, *args, **kwargs):
-        super(ProfileForm, self).__init__(*args, **kwargs)
