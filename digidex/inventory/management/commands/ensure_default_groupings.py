82d6819d2bda6f5cb2c6fd0d0968d578ac527c45
Raphael <puello322@gmail.com>
2024-02-26 19:26:01 -0700

    Update import statement for Grouping model

--- digidex/inventory/management/commands/ensure_default_groupings.py
+++ digidex/inventory/management/commands/ensure_default_groupings.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-from inventory.models import Grouping
+from digidex.inventory.models import Grouping
 
 User = get_user_model()
 
