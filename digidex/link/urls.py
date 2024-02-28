03cbf25df3ae80cf3354d85f5188fbe83c9d3a27
Raphael <puello322@gmail.com>
2024-02-28 16:03:12 -0700

    Update URL pattern for digit view

--- digidex/link/urls.py
+++ digidex/link/urls.py
@@ -3,5 +3,5 @@ from digidex.link.views import NTAGLink
 
 app_name = 'link'
 urlpatterns = [
-    path('digit/<str:serial_number>/', NTAGLink.as_view(), name='digit'),
+    path('digit/<slug:slug>/', NTAGLink.as_view(), name='digit'),
 ]
