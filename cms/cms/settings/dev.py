c30965a103a46ab84430afff474e9423cadf5d9b
RafaelPuello <rafael.e.puello@gmail.com>
2024-03-04 13:19:20 -0700

    Creating cms wagtail app

---
+++ cms/cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-y-_l_&4$7_crjiuw1&o48_exglt%ucjk#%i=la7^1ne6mslh*("
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
