94bf20ae86e5eb7a829483f3888e0838cdb848ad
Raphael <puello322@gmail.com>
2024-03-05 19:30:46 -0700

    Refactor blog models and add gallery image functionality

--- cms/blog/models.py
+++ cms/blog/models.py
@@ -1,15 +1,18 @@
 from django.db import models
-from wagtail.models import Page
+from modelcluster.fields import ParentalKey
+from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.search import index
 
 
 class BlogIndexPage(Page):
-    intro = RichTextField(blank=True)
+    intro = RichTextField(
+        blank=True
+    )
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro')
+        FieldPanel('intro'),
     ]
 
     def get_context(self, request):
@@ -30,9 +33,26 @@ class BlogIndexPage(Page):
 
 
 class BlogPage(Page):
-    date = models.DateField("Post date")
-    intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    date = models.DateField(
+        "Post date"
+    )
+    intro = models.CharField(
+        max_length=250
+    )
+    body = RichTextField(
+        blank=True
+    )
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+    def blog_index(self):
+        return self.get_ancestors().type(BlogIndexPage).last()
+
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
@@ -42,7 +62,28 @@ class BlogPage(Page):
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body'),
+
+        InlinePanel('gallery_images', label="Gallery images"),
     ]
 
-    def blog_index(self):
-        return self.get_ancestors().type(BlogIndexPage).last()
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(
+        BlogPage,
+        on_delete=models.CASCADE,
+        related_name='gallery_images'
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    caption = models.CharField(
+        blank=True,
+        max_length=250
+    )
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
