9bd80753cf03b598e2897ce341ac05a38ea19b04
Raphael <puello322@gmail.com>
2024-03-08 16:34:09 -0700

    Refactor home page model and remove unused header template

--- cms/home/models.py
+++ cms/home/models.py
@@ -1,52 +1,33 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.fields import RichTextField, StreamField
+from wagtail.models import Page, Orderable
+from modelcluster.fields import ParentalKey
 
-from wagtail.models import Page
-from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from base import blocks
 
-
-class HomePage(Page):
-    image = models.ForeignKey(
-        "wagtailimages.Image",
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-        help_text="Homepage image",
+class HomePageSection(Orderable):
+    page = ParentalKey(
+        "HomePage",
+        related_name="sections"
     )
-    hero_text = models.CharField(
-        blank=True,
+    title = models.CharField(
         max_length=255,
-        help_text="Write an introduction for the site"
-    )
-    hero_cta = models.CharField(
-        blank=True,
-        verbose_name="Hero CTA",
-        max_length=255,
-        help_text="Text to display on Call to Action",
-    )
-    hero_cta_link = models.ForeignKey(
-        "wagtailcore.Page",
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name="+",
-        verbose_name="Hero CTA link",
-        help_text="Choose a page to link to for the Call to Action",
-    )
-    body = RichTextField(
         blank=True
     )
+    content = StreamField(
+        blocks.BaseStreamBlock(),
+        use_json_field=True,
+        blank=True
+    )
+    lottie_animation = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="URL to the Lottie animation JSON file"
+    )
 
-    content_panels = Page.content_panels + [
-        MultiFieldPanel(
-            [
-                FieldPanel("image"),
-                FieldPanel("hero_text"),
-                FieldPanel("hero_cta"),
-                FieldPanel("hero_cta_link"),
-            ],
-            heading="Hero section",
-        ),
-        FieldPanel('body'),
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("content"),
+        FieldPanel("lottie_animation"),
     ]
