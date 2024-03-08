4f9efea88b34a9fb0622ab717d9dd3548e5e7f8a
Raphael <puello322@gmail.com>
2024-03-08 08:42:53 -0700

    Update social media links and add footer

--- cms/base/models.py
+++ cms/base/models.py
@@ -13,14 +13,14 @@ from wagtail.contrib.settings.models import (
 class NavigationSettings(BaseGenericSetting):
     twitter_url = models.URLField(verbose_name="Twitter URL", blank=True)
     github_url = models.URLField(verbose_name="GitHub URL", blank=True)
-    mastodon_url = models.URLField(verbose_name="Mastodon URL", blank=True)
+    linkedin_url = models.URLField(verbose_name="LinkedIn URL", blank=True)
 
     panels = [
         MultiFieldPanel(
             [
                 FieldPanel("twitter_url"),
                 FieldPanel("github_url"),
-                FieldPanel("mastodon_url"),
+                FieldPanel("linkedin_url"),
             ],
             "Social settings",
         )
