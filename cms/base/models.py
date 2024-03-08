c7e5cc06e1b13bd346b05049211b514f9f7dba71
Raphael <puello322@gmail.com>
2024-03-08 10:12:09 -0700

    Add FooterText model and template

--- cms/base/models.py
+++ cms/base/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
+    PublishingPanel,
 )
+from wagtail.fields import RichTextField
+from wagtail.models import (
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+)
+from wagtail.snippets.models import register_snippet
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
@@ -25,3 +33,32 @@ class NavigationSettings(BaseGenericSetting):
             "Social settings",
         )
     ]
+
+
+@register_snippet
+class FooterText(
+    DraftStateMixin,
+    RevisionMixin,
+    PreviewableMixin,
+    TranslatableMixin,
+    models.Model,
+):
+
+    body = RichTextField()
+
+    panels = [
+        FieldPanel("body"),
+        PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {"footer_text": self.body}
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name_plural = "Footer Text"
