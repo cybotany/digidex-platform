17291ac3f0c4b0e14fd0694db238a29ec2f32d88
Raphael <puello322@gmail.com>
2024-03-07 19:21:04 -0700

    Add NavigationSettings model and update INSTALLED_APPS

--- cms/cms/settings/base.py
+++ cms/cms/settings/base.py
@@ -10,6 +10,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 INSTALLED_APPS = [
+    "base",
     "blog",
     "ecommerce",
     "home",
@@ -17,6 +18,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
+    "wagtail.contrib.settings",
     "wagtail.embeds",
     "wagtail.sites",
     "wagtail.users",
@@ -62,6 +64,8 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                
+                "wagtail.contrib.settings.context_processors.settings",
             ],
         },
     },
