ecad63082774431daec7fefb8b6177ffeaa9835a
Raphael <puello322@gmail.com>
2024-03-10 15:44:23 -0600

    Add new templates and blocks for home, company, contact, pricing, and solutions pages

--- cms/home/blocks/section.py
+++ cms/home/blocks/section.py
@@ -1,15 +1,13 @@
-from cms.base.blocks import page, section
 from wagtail import blocks
-
-from base.blocks import base
-
+# Project specifice imports
+from base.blocks import base, section
 
 class HeroSection(section.BaseSectionBlock):
     lottie_animation_1 = base.LottieAnimationBlock()
     lottie_animation_2 = base.LottieAnimationBlock()
 
     class Meta:
-        template = "base/blocks/hero_section.html"
+        template = "home/blocks/hero_section.html"
 
 
 class HowItWorksSection(section.BaseSectionBlock):
@@ -21,7 +19,7 @@ class HowItWorksSection(section.BaseSectionBlock):
     )
 
     class Meta:
-        template = 'base/blocks/how_it_works_block.html'
+        template = 'home/blocks/how_it_works_block.html'
         icon = 'snippet'
 
 
@@ -30,4 +28,4 @@ class CallToActionSection(section.BaseSectionBlock):
     lottie_animation_2 = base.LottieAnimationBlock()
 
     class Meta:
-        template = "base/blocks/call_to_action_section.html"
+        template = "home/blocks/call_to_action_section.html"
