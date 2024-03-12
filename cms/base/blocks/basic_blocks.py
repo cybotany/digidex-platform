b110b919b9a970263dd4f916c3acb6677ffec852
Raphael <puello322@gmail.com>
2024-03-11 20:55:31 -0600

    Refactor base blocks to use Meta class for template configuration

--- cms/base/blocks/basic_blocks.py
+++ cms/base/blocks/basic_blocks.py
@@ -2,72 +2,55 @@ from wagtail import blocks
 from wagtail.images import blocks as img_blocks
 
 class BaseCharBlock(blocks.CharBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/char_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/char_block.html"
 
 
 class BaseListBlock(blocks.ListBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/list_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/list_block.html"
 
 
 class BaseChoiceBlock(blocks.ChoiceBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/choice_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/choice_block.html"
 
 
-class BaseFloatBlock(blocks.BaseFloatBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/float_block.html"
-        super().__init__(*args, **kwargs)
+class BaseFloatBlock(blocks.FloatBlock):
+    class Meta:
+        template = "blocks/base/float_block.html"
 
 
 class BaseBooleanBlock(blocks.BooleanBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/boolean_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/boolean_block.html"
 
 
 class BaseIntegerBlock(blocks.IntegerBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/integer_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/integer_block.html"
 
 
 class BaseTextBlock(blocks.TextBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/text_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/text_block.html"
 
 
 class BaseURLBlock(blocks.URLBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/url_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/url_block.html"
 
 
 class BaseImageBlock(img_blocks.ImageChooserBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/url_block.html"
-        super().__init__(*args, **kwargs)
+    class Meta:
+        template = "blocks/base/image_block.html"
 
 
 class BaseStructBlock(blocks.StructBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/struct_block.html"
-        super().__init__(*args, **kwargs)
-
-    def get_template(self, context=None):
-        return self.template
+    class Meta:
+        template = "blocks/base/struct_block.html"
 
 
-class BaseStructBlock(blocks.BaseStreamBlock):
-    def __init__(self, *args, **kwargs):
-        self.template = "blocks/base/stream_block.html"
-        super().__init__(*args, **kwargs)
-
-    def get_template(self, context=None):
-        return self.template
+class BaseStreamBlock(blocks.BaseStreamBlock):
+    class Meta:
+        template = "blocks/base/stream_block.html"
