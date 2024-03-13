08af797a0bc1e9e34b4b4a1d4f2e572d5850f1e8
Raphael <puello322@gmail.com>
2024-03-12 20:36:37 -0600

    Refactor navigation bar blocks

--- cms/base/blocks/page/header/navigation_bar.py
+++ cms/base/blocks/page/header/navigation_bar.py
@@ -1,47 +1,65 @@
-from cms.base.blocks import composite_blocks
-from cms.base.blocks import basic_blocks
+from cms.base.blocks import basic_blocks as _bblocks,\
+                            composite_blocks as _cblocks
 
-class NavigationLinkBlock(composite_blocks.URLBlock):
+class NavigationLinkBlock(_cblocks.URLBlock):
     pass
 
     class Meta:
         icon = 'site'
-        template = 'blocks/navigation_link_block.html'
+        template = 'blocks/base/navigation_link_block.html'
 
 
-class NavigationMenuBlock(composite_blocks.URLBlock):
-    nav_links = basic_blocks.BaseListBlock(
-        composite_blocks.URLBlock(help_text="Add navigation links")
+class NavigationButtonBlock(_cblocks.ButtonBlock):
+    pass
+
+    class Meta:
+        icon = 'site'
+        template = 'blocks/base/navigation_link_block.html'
+
+
+class NavigationLinkListBlock(_cblocks.URLBlock):
+    nav_buttons = _bblocks.BaseListBlock(
+            NavigationButtonBlock(),
+            help_text="Add navigation buttons"
+        )
+
+    class Meta:
+        icon = 'site'
+        template = 'blocks/base/navigation_menu_block.html'
+
+
+class NavigationMenuBlock(_bblocks.BaseStreamBlock):
+    nav_links = _bblocks.BaseListBlock(
+        _cblocks.URLBlock(help_text="Add navigation links")
     )
+    mobile_nav_buttons = NavigationLinkListBlock()
 
     class Meta:
         icon = 'site'
-        template = 'blocks/navigation_menu_block.html'
+        template = 'blocks/base/navigation_menu_block.html'
 
 
-class NavigationButtonBlock(composite_blocks.ButtonBlock):
+class NavigationDesktopBlock(_bblocks.BaseStreamBlock):
+    desktop_nav_buttons = NavigationLinkListBlock()
+
+    class Meta:
+        icon = 'site'
+        template = 'blocks/base/navigation_menu_block.html'
+
+
+class ShoppingCartBlock(_cblocks.ButtonBlock):
     pass
 
     class Meta:
         icon = 'site'
-        template = 'blocks/navigation_link_block.html'
+        template = 'blocks/base/navigation_bar.html'
 
 
-class ShoppingCartBlock(composite_blocks.ButtonBlock):
-    pass
-
-    class Meta:
-        icon = 'site'
-        template = 'blocks/navigation_link_block.html'
-
-
-class NavigationBarBlock(basic_blocks.BaseStructBlock):
+class NavigationBarBlock(_bblocks.BaseStructBlock):
     nav_menu = NavigationMenuBlock()
-    action_buttons = basic_blocks.BaseStreamBlock(
-        composite_blocks.ButtonBlock(help_text="Add action buttons")
-    )
+    desktop_buttons = NavigationDesktopBlock()
     shopping_cart = ShoppingCartBlock()
 
     class Meta:
         icon = 'site'
-        template = 'blocks/navbar_block.html'
+        template = 'blocks/base/navbar_block.html'
