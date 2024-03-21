bca131c74dca4e837e18e634548806715d47153c
Raphael <puello322@gmail.com>
2024-03-21 16:50:29 -0600

    Remove reCAPTCHA processor and unused imports

--- cms/cms/utils/__init__.py
+++ cms/cms/utils/__init__.py
@@ -1,5 +1,4 @@
 from .decorators import require_ajax
 from .helpers import show_message
-from .processors import recaptcha_site_key
 from .cms_storage import PublicMediaStorage, PrivateMediaStorage, PublicStaticStorage
 from .validators import validate_username
--- cms/cms/utils/processors/__init__.py
+++
@@ -1,1 +0,0 @@
-from reCAPTCHA_processor import recaptcha_site_key
