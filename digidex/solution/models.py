fabe9c527efd98732e96919f2ab423e1f2233d48
Raphael <puello322@gmail.com>
2024-03-22 00:39:41 -0600

    Refactor models.py to use FieldPanel instead of StreamFieldPanel

--- digidex/solution/models.py
+++ digidex/solution/models.py
@@ -1,7 +1,7 @@
-from wagtail.core.fields import StreamField
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
 
+from base.fields import wagtail_fields as _wfields
 from base.blocks import basic_blocks as _bblocks
 from base import models as _models
 from solution import blocks as _sblocks
@@ -10,31 +10,47 @@ class SolutionIndexPage(_models.IndexPage):
     pass
 
 class SolutionsPage(Page):
-    top_section = StreamField([
-        ('heading', _bblocks.BaseCharBlock(form_classname="full title")),
-        ('paragraph', _bblocks.BaseRichTextBlock()),
-    ], null=True, blank=True)
-
-    about_sections = StreamField([
-        ('about_section', _sblocks.AboutSectionBlock()),
-    ], null=True, blank=True)
-
-    quote = StreamField([
-        ('quote', _sblocks.QuoteBlock()),
-    ], null=True, blank=True)
-
-    team_members = StreamField([
-        ('team_member', _sblocks.TeamMemberBlock()),
-    ], null=True, blank=True)
-
-    reviews = StreamField([
-        ('review', _sblocks.ReviewBlock()),
-    ], null=True, blank=True)
+    top_section = _wfields.BaseStreamField(
+        [
+            ('heading', _bblocks.BaseCharBlock(form_classname="full title")),
+            ('paragraph', _bblocks.BaseRichTextBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    about_sections = _wfields.BaseStreamField(
+        [
+            ('about_section', _sblocks.AboutSectionBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    quote = _wfields.BaseStreamField(
+        [
+            ('quote', _sblocks.QuoteBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    team_members = _wfields.BaseStreamField(
+        [
+            ('team_member', _sblocks.TeamMemberBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    reviews = _wfields.BaseStreamField(
+        [
+            ('review', _sblocks.ReviewBlock()),
+        ],
+        null=True,
+        blank=True
+    )
 
     content_panels = Page.content_panels + [
-        StreamFieldPanel('top_section'),
-        StreamFieldPanel('about_sections'),
-        StreamFieldPanel('quote'),
-        StreamFieldPanel('team_members'),
-        StreamFieldPanel('reviews'),
+        FieldPanel('top_section'),
+        FieldPanel('about_sections'),
+        FieldPanel('quote'),
+        FieldPanel('team_members'),
+        FieldPanel('reviews'),
     ]
