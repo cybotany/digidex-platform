509f1c9c48a7e8e9d3801f81fb478567529bd4e7
Raphael <puello322@gmail.com>
2024-03-22 00:07:50 -0600

    Refactor models.py to use custom fields and blocks

--- digidex/base/models.py
+++ digidex/base/models.py
@@ -1,12 +1,13 @@
-from wagtail.core.models import Page
+from wagtail.models import Page
 from wagtail.admin.panels import StreamFieldPanel
-from wagtail.fields import StreamField
-from wagtail.blocks import RichTextBlock
+
+from base.fields import wagtail_fields as _wfields
+from base.blocks import basic_blocks as _bblocks
 
 # Base index page model
 class IndexPage(Page):
-    introduction = StreamField([
-        ('paragraph', RichTextBlock()),
+    introduction = _wfields.BaseStreamField([
+        ('paragraph', _bblocks.BaseRichTextBlock()),
     ], null=True, blank=True)
 
     content_panels = Page.content_panels + [
@@ -16,34 +17,26 @@ class IndexPage(Page):
     class Meta:
         abstract = True
 
-# For the Authentication app
 class AuthenticationIndexPage(IndexPage):
-    pass  # You can customize this class as needed.
+    pass
 
-# For the Authorization app
 class AuthorizationIndexPage(IndexPage):
     pass
 
-# For the Company app
 class CompanyIndexPage(IndexPage):
     pass
 
-# For the Ecommerce app
 class EcommerceIndexPage(IndexPage):
     pass
 
-# For the Persona app
 class PersonaIndexPage(IndexPage):
     pass
 
-# For the Privacy app
 class PrivacyIndexPage(IndexPage):
     pass
 
-# For the Solution app
 class SolutionIndexPage(IndexPage):
     pass
 
-# For the Support app
 class SupportIndexPage(IndexPage):
     pass
