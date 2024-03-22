63bfe56e2db4423a306cc03b5b62409ded0d6ae9
Raphael <puello322@gmail.com>
2024-03-21 23:42:20 -0600

    Refactor SupportIndexPage model in models.py

--- digidex/support/models.py
+++ digidex/support/models.py
@@ -1,17 +1,32 @@
-from wagtail.core.models import Page
-from wagtail.core.fields import StreamField
+from wagtail.models import Page
 from wagtail.admin.edit_handlers import StreamFieldPanel
 
+
+from base.blocks import basic_blocks as _bblocks
+from base.fields import wagtail_fields as _wfields
+from support import blocks as _sblocks
+
 class SupportIndexPage(Page):
-    intro_heading = blocks.CharBlock(required=True)
-    intro_text = blocks.TextBlock(required=True)
-    contact_options = StreamField([
-        ('contact_option', ContactOptionBlock()),
-    ], null=True, blank=True)
-    
-    faqs = StreamField([
-        ('faq', FAQBlock()),
-    ], null=True, blank=True)
+    intro_heading = _bblocks.BaseCharBlock(
+        required=True
+    )
+    intro_text = _bblocks.BaseTextBlock(
+        required=True
+    )
+    contact_options = _wfields.BaseStreamField(
+        [
+            ('contact_option', _sblocks.ContactOptionBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    faqs = _wfields.BaseStreamField(
+        [
+            ('faq', _sblocks.FAQBlock()),
+        ],
+        null=True,
+        blank=True
+    )
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('intro_heading'),
