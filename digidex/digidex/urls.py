6ed519bcde5f229d310dda95d25a3d1fe4658caa
Raphael <puello322@gmail.com>
2024-03-21 18:56:56 -0600

    Remove allauth.urls from urlpatterns

--- digidex/digidex/urls.py
+++ digidex/digidex/urls.py
@@ -13,8 +13,6 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
-
-    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
