444df95b3b8014bd3d383c76efe3af7c4adce411
Raphael <puello322@gmail.com>
2024-03-22 01:48:48 -0600

    Add FooterText model and settings to FooterNavigationSettings

--- digidex/navigation/models/footer.py
+++ digidex/navigation/models/footer.py
@@ -1,7 +1,11 @@
-from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from django.db import models
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PublishingPanel
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+from wagtail.models import DraftStateMixin, PreviewableMixin, RevisionMixin, TranslatableMixin
+from wagtail.snippets.models import register_snippet
 
-from base.fields import django_fields as _dfields
+from base.fields import django_fields as _dfields,\
+                        wagtail_fields as _wfields
 
 @register_setting
 class FooterNavigationSettings(BaseGenericSetting):
@@ -28,3 +32,26 @@ class FooterNavigationSettings(BaseGenericSetting):
             "Social settings",
         )
     ]
+
+@register_snippet
+class FooterText( DraftStateMixin, RevisionMixin, PreviewableMixin, TranslatableMixin, models.Model):
+    body = _wfields.BaseRichTextField(
+        max_length=150,
+    )
+
+    panels = [
+        FieldPanel("body"),
+        PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {"footer_text": self.body}
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name_plural = "Footer Text"
