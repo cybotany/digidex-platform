b66f2512f8aca7bd1ff50d8cd7ff69453c28f9a0
Raphael <puello322@gmail.com>
2024-03-21 22:10:24 -0600

    Delete unused blocks and their dependencies

--- digidex/company/blocks.py
+++ digidex/company/blocks.py
@@ -1,5 +1,52 @@
 from base.blocks import basic_blocks as _bblocks
 
+# Project specific blocks
+from base.blocks import basic_blocks as _bblocks,\
+                        composite_blocks as _cblocks,\
+                        layout_blocks as _lblocks
+
+class StatisticItemBlock(_bblocks.BaseStructBlock):
+    icon = _bblocks.BaseImageBlock()
+    number = _bblocks.BaseCharBlock(
+        help_text="Statistic number"
+    )
+    description = _bblocks.BaseCharBlock(
+        help_text="Statistic description"
+    )
+    style = _bblocks.BaseChoiceBlock(
+        required=False,
+        choices=[
+            ('default', 'Default'),
+            ('green', 'Green'),
+        ],
+        help_text="Statistic style",
+        default='default')
+
+    class Meta:
+        icon = 'pick'
+        template = 'blocks/statistic_item_block.html'
+
+
+class StatisticsGridBlock(_bblocks.BaseStructBlock):
+    statistics = _bblocks.BaseListBlock(
+        StatisticItemBlock(help_text="Add statistics")
+    )
+
+    class Meta:
+        icon = 'grid'
+        template = 'blocks/statistics_grid_block.html'
+
+
+class FeaturedSectionBlock(_bblocks.BaseStructBlock):
+    lottie = _cblocks.LottieBlock()
+    text_content = _bblocks.BaseTextBlock()
+    statistics_grid = StatisticsGridBlock()
+
+    class Meta:
+        icon = 'placeholder'
+        template = 'blocks/featured_section_block.html'
+
+
 class TeamMemberBlock(_bblocks.BaseStructBlock):
     name = _bblocks.BaseCharBlock(
         max_length=255
