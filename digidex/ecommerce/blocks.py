7fb9e8347283b14adac690b80fb683c4493a20ad
Raphael <puello322@gmail.com>
2024-03-21 23:59:55 -0600

    Refactor code to use modular imports and improve code organization

--- digidex/ecommerce/blocks.py
+++ digidex/ecommerce/blocks.py
@@ -1,5 +1,4 @@
-from wagtail.core import blocks
-from wagtail.images.blocks import ImageChooserBlock
+
 
 class FeatureBlock(blocks.StructBlock):
     icon = ImageChooserBlock(required=True)
--- digidex/ecommerce/models.py
+++ digidex/ecommerce/models.py
@@ -1,20 +1,35 @@
-from wagtail.core.models import Page
-from wagtail.core.fields import StreamField
+from wagtail.models import Page
 from wagtail.admin.edit_handlers import StreamFieldPanel
 
+from base.blocks import basic_blocks as _bblocks
+from base.fields import wagtail_fields as _wfields
+from ecommerce import blocks as _blocks
+
 class EcommerceIndexPage(Page):
-    top_heading = blocks.CharBlock(required=True)
-    categories = StreamField([
-        ('category_link', CategoryLinkBlock()),
-    ], null=True, blank=True)
-
-    products = StreamField([
-        ('product', ProductBlock()),
-    ], null=True, blank=True)
-
-    faqs = StreamField([
-        ('faq', FAQBlock()),
-    ], null=True, blank=True)
+    top_heading = _bblocks.BaseCharBlock(
+        required=True
+    )
+    categories = _wfields.BaseStreamField(
+        [
+            ('category_link', _blocks.CategoryLinkBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    products = _wfields.BaseStreamField(
+        [
+            ('product', _blocks.ProductBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+    faqs = _wfields.BaseStreamField(
+        [
+            ('faq', _blocks.FAQBlock()),
+        ],
+        null=True,
+        blank=True
+    )
 
     content_panels = Page.content_panels + [
         StreamFieldPanel('categories'),
