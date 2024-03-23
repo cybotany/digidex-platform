c51e1a96f98b806cd5867f75eb9b1b3f399ca726
Raphael <puello322@gmail.com>
2024-03-22 20:23:46 -0600

    Refactor base blocks and remove unused code

--- digidex/base/blocks/basic_blocks.py
+++ digidex/base/blocks/basic_blocks.py
@@ -4,14 +4,11 @@ from wagtail.images import blocks as img_blocks
 class BaseCharBlock(blocks.CharBlock):
     pass
 
-
 class BaseListBlock(blocks.ListBlock):
     pass
 
-
 class BaseChoiceBlock(blocks.ChoiceBlock):
     pass
-
 
 class BaseFloatBlock(blocks.FloatBlock):
     pass
@@ -19,14 +16,11 @@ class BaseFloatBlock(blocks.FloatBlock):
 class BaseBooleanBlock(blocks.BooleanBlock):
     pass
 
-
 class BaseIntegerBlock(blocks.IntegerBlock):
     pass
 
-
 class BaseTextBlock(blocks.TextBlock):
     pass
-
 
 class BaseURLBlock(blocks.URLBlock):
     pass
@@ -37,14 +31,23 @@ class BasePageBlock(blocks.PageChooserBlock):
 class BaseImageBlock(img_blocks.ImageChooserBlock):
     pass
 
-
 class BaseRichTextBlock(blocks.RichTextBlock):
     pass
-
 
 class BaseStructBlock(blocks.StructBlock):
     pass
 
-
 class BaseStreamBlock(blocks.StreamBlock):
+    pass
+
+class BaseBlock(BaseStructBlock):
+    pass
+
+class GridBlock(BaseStructBlock):
+    pass
+
+class ContentBlock(BaseStructBlock):
+    pass
+
+class SectionBlock(BaseStructBlock):
     pass
--- digidex/base/blocks/support/section.py
+++ digidex/base/blocks/support/section.py
@@ -1,11 +1,49 @@
-from base.blocks import layout_blocks as _lblocks
-from base.blocks.support import heading, lottie
+from base.blocks import basic_blocks as _bblocks
+
+class ContactHeadingBlock(_bblocks.BaseBlock):
+    heading = _bblocks.BaseRichTextBlock(
+        required=True,
+        help_text="Enter a heading for this section."
+    )
+    subtitle = _bblocks.BaseRichTextBlock(
+        required=True,
+        help_text="Enter a subtitle for this section."
+    )
 
 
+class ContactMethodBlock(_bblocks.BaseStructBlock):
+    icon = _bblocks.BaseImageBlock(
+        required=True
+    )
+    method = _bblocks.BaseCharBlock(
+        required=True
+    )
+    description = _bblocks.BaseCharBlock(
+        required=True
+    )
+    url = _bblocks.BaseURLBlock(
+        required=False
+    )
 
-class ContactSectionBlock(_lblocks.SectionBlock):
-    title = heading.ContactHeadingBlock()
-    lottie = lottie.ContactLottieBlock()
+
+class ContactLottieBlock(_bblocks.BaseStructBlock):
+    methods = _bblocks.BaseListBlock(
+        ContactMethodBlock(help_text="Add contact methods.")
+    )
+
+
+class ContactContactBlock(_bblocks.SectionBlock):
+    title = ContactHeadingBlock()
+    lottie = ContactLottieBlock()
+
+    class Meta:
+        icon = 'contact'
+        template = 'base/blocks/support/section.html'
+
+
+class ContactSectionBlock(_bblocks.SectionBlock):
+    title = ContactHeadingBlock()
+    lottie = ContactLottieBlock()
 
     class Meta:
         icon = 'contact'
