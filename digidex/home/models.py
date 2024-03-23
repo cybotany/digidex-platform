2d8257fa4c1eabbb9dec1e8ca37adafa91eaf270
Raphael <puello322@gmail.com>
2024-03-23 12:29:10 -0600

    Delete unused blocks and models

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -2,28 +2,25 @@ from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 
 from base.fields import wagtail
-from base.blocks.apps import home as hero
-from base.blocks.apps.solutions import section as solutions
+from base.blocks import basics as _blocks
 from base.models import basics as _models
 
 
 class HomePage(_models.BasePage):
-    hero = wagtail.BaseStreamField(
+    body = wagtail.BaseStreamField(
         [
-            ('hero', hero.HeroSectionBlock())
-        ],
-        null=True,
-        blank=True
-    )
-    solutions = wagtail.BaseStreamField(
-        [
-            ('solutions', solutions.SolutionSectionBlock())
-        ],
-        null=True,
-        blank=True
+            ('person', _blocks.BaseStructBlock([
+                ('first_name', _blocks.BaseCharBlock()),
+                ('surname', _blocks.BaseCharBlock()),
+                ('photo', _blocks.BaseImageBlock(required=False)),
+                ('biography', _blocks.BaseRichTextBlock()),
+            ])),
+            ('heading', _blocks.BaseCharBlock(form_classname="title")),
+            ('paragraph', _blocks.BaseRichTextBlock()),
+            ('image', _blocks.BaseImageBlock()),
+        ]
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel('hero'),
-        FieldPanel('solutions'),
+        FieldPanel('body'),
     ]
