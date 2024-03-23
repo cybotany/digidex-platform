7d4e97675b3bb6f5d3d541d8d37a0a487b721df9
Raphael <puello322@gmail.com>
2024-03-23 15:24:06 -0600

    Add call to action section to base page template and update navigation include

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -9,15 +9,14 @@ from base import models as _models
 class HomePage(_models.BasePage):
     body = _fields.BaseStreamField(
         [
-            ('person', _blocks.BaseStructBlock([
-                ('first_name', _blocks.BaseCharBlock()),
-                ('surname', _blocks.BaseCharBlock()),
-                ('photo', _blocks.BaseImageBlock(required=False)),
-                ('biography', _blocks.BaseRichTextBlock()),
-            ])),
-            ('heading', _blocks.BaseCharBlock(form_classname="title")),
-            ('paragraph', _blocks.BaseRichTextBlock()),
-            ('image', _blocks.BaseImageBlock()),
+            ('hero', _blocks.BaseStructBlock()),
+            ('solutions', _blocks.BaseStructBlock()),
+            ('company', _blocks.BaseStructBlock()),
+            ('features', _blocks.BaseStructBlock()),
+            ('reviews', _blocks.BaseStructBlock()),
+            ('support', _blocks.BaseStructBlock()),
+            ('faq', _blocks.BaseStructBlock()),
+            ('cta', _blocks.BaseStructBlock()),
         ]
     )
 
