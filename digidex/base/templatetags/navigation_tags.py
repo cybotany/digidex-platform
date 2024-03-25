028ff81ca3d81040640d97cfe039f36bef54ee83
Raphael <puello322@gmail.com>
2024-03-25 15:45:51 -0600

    Delete unused templates and update inclusion tag

--- digidex/base/templatetags/navigation_tags.py
+++ digidex/base/templatetags/navigation_tags.py
@@ -5,7 +5,7 @@ from base.models import footers
 
 register = template.Library()
 
-@register.inclusion_tag("navigation/includes/footer_text.html", takes_context=True)
+@register.inclusion_tag("base/includes/footer_text.html", takes_context=True)
 def get_footer_text(context):
     footer_text = context.get("footer_text", "")
 
