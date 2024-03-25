bd7e051992abe30fdc5d2f92e451ee609d1c28ee
Raphael <puello322@gmail.com>
2024-03-24 23:52:46 -0600

    Add and delete files

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .error_page import ErrorView
-from .company_page import CompanyView
-from .contact_page import ContactView
-from .landing_page import LandingView
-from .thank_you_page import ThankYouView
