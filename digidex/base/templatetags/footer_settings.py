1a55088eeec7b25570f03a65a47f5c770b9a878f
Raphael <puello322@gmail.com>
2024-03-27 12:50:04 -0600

    Add footer and navigation template tags

---
+++ digidex/base/templatetags/footer_settings.py
@@ -0,0 +1,17 @@
+from django import template
+from wagtail.models import Site
+from base.models import FooterSettings
+
+register = template.Library()
+
+@register.inclusion_tag("includes/footer.html", takes_context=True)
+def get_footer(context):
+    # Access the current site from the request
+    request = context.get('request')
+    site = Site.find_for_request(request) if request else None
+
+    footer_settings = FooterSettings.for_site(site) if site else FooterSettings.objects.first()
+
+    return {
+        'footer_settings': footer_settings,
+    }
