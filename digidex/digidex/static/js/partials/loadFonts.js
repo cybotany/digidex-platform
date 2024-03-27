3447d3c93e0ea9f94988b019432433366190d8d6
Raphael <puello322@gmail.com>
2024-03-26 23:55:54 -0600

    Refactor menu button selector and remove unused font loading code

--- digidex/digidex/static/js/partials/loadFonts.js
+++
@@ -1,7 +0,0 @@
-// loadFonts.js
-WebFont.load({
-    google: {
-      families: ["Space Grotesk:regular,500,600,700", "IBM Plex Mono:regular,italic,500,500italic,600,600italic,700,700italic"]
-    }
-  });
-  
