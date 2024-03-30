2da271351d06d2bb1600211c3bd13d1f1d53fa1b
Raphael <puello322@gmail.com>
2024-03-30 08:47:15 -0600

    Refactor home page templates and models

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -7,34 +7,33 @@ from base.blocks.page_content_block import PageContentBlock
 
 
 class HomePage(Page):
-    pass
-#    heading_title = models.CharField(
-#        max_length=255,
-#        blank=True,
-#        null=True
-#    )
-#    heading_paragraph = models.TextField(
-#        blank=True,
-#        null=True
-#    )
-#    body = StreamField(
-#        [
-#            ('content', PageContentBlock()),
-#        ],
-#        blank=True
-#    )
+    heading_title = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True
+    )
+    heading_paragraph = models.TextField(
+        blank=True,
+        null=True
+    )
+    body = StreamField(
+        [
+            ('content', PageContentBlock()),
+        ],
+        blank=True
+    )
 
-#    content_panels = Page.content_panels + [
-#        MultiFieldPanel(
-#            [
-#                FieldPanel('title'),
-#                FieldPanel('text'),
-#            ],
-#            heading="Page Heading",
-#        ),
-#        FieldPanel('body'),
-#    ]
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel('title'),
+                FieldPanel('text'),
+            ],
+            heading="Page Heading",
+        ),
+        FieldPanel('body'),
+    ]
 
-#    class Meta:
-#        verbose_name = "Home Page"
-#        verbose_name_plural = "Home Pages"
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
