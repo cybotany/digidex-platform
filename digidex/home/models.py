bde0dea03cdac8f48e369ddeac879fe8674d0a71
Raphael <puello322@gmail.com>
2024-03-29 23:14:09 -0600

    Commented out unused code in HomePage model

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -5,34 +5,36 @@ from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 
 from base.blocks.page_content_block import PageContentBlock
 
+
 class HomePage(Page):
-    heading_title = models.CharField(
-        max_length=255,
-        blank=True,
-        null=True
-    )
-    heading_paragraph = models.TextField(
-        blank=True,
-        null=True
-    )
-    body = StreamField(
-        [
-            ('content', PageContentBlock()),
-        ],
-        blank=True
-    )
+    pass
+#    heading_title = models.CharField(
+#        max_length=255,
+#        blank=True,
+#        null=True
+#    )
+#    heading_paragraph = models.TextField(
+#        blank=True,
+#        null=True
+#    )
+#    body = StreamField(
+#        [
+#            ('content', PageContentBlock()),
+#        ],
+#        blank=True
+#    )
 
-    content_panels = Page.content_panels + [
-        MultiFieldPanel(
-            [
-                FieldPanel('title'),
-                FieldPanel('text'),
-            ],
-            heading="Page Heading",
-        ),
-        FieldPanel('body'),
-    ]
+#    content_panels = Page.content_panels + [
+#        MultiFieldPanel(
+#            [
+#                FieldPanel('title'),
+#                FieldPanel('text'),
+#            ],
+#            heading="Page Heading",
+#        ),
+#        FieldPanel('body'),
+#    ]
 
-    class Meta:
-        verbose_name = "Home Page"
-        verbose_name_plural = "Home Pages"
+#    class Meta:
+#        verbose_name = "Home Page"
+#        verbose_name_plural = "Home Pages"
