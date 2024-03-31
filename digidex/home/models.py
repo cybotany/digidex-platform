1178a8a07f923251e16d3269b6738645c61fa6d0
Raphael <puello322@gmail.com>
2024-03-30 21:09:38 -0600

    Add hero heading, paragraph, and CTA links to HomePage model and update landing page template

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -7,20 +7,32 @@ from base.blocks.page_content_block import PageContentBlock
 
 
 class HomePage(Page):
-    heading = models.CharField(
+    hero_heading = models.CharField(
         max_length=255,
         blank=True,
         null=True
     )
-    paragraph = models.TextField(
+    hero_paragraph = models.TextField(
         blank=True,
         null=True
     )
-    body = StreamField(
-        [
-            ('content', PageContentBlock()),
-        ],
-        blank=True
+    hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Primary Hero CTA link",
+        help_text="Choose a page to link to for the Primary Call to Action",
+    )
+    alt_hero_cta_link = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+        verbose_name="Alternate Hero CTA link",
+        help_text="Choose a page to link to for the Alternate Call to Action",
     )
 
     content_panels = Page.content_panels + [
@@ -29,9 +41,15 @@ class HomePage(Page):
                 FieldPanel('heading'),
                 FieldPanel('paragraph'),
             ],
-            heading="Page Heading",
+            heading="Landing Page Hero Text",
         ),
-        FieldPanel('body'),
+        MultiFieldPanel(
+            [
+                FieldPanel('primary_hero_cta'),
+                FieldPanel('alternate_hero_cta'),
+            ],
+            heading="Landing Page Hero Buttons",
+        ),
     ]
 
     class Meta:
