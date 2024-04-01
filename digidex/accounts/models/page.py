fbd18001b42bfeb5161f53894259c8dbcabc6ccc
Raphael <puello322@gmail.com>
2024-04-01 08:00:03 -0600

    Add LoginPage and SignupPage models and templates

--- digidex/accounts/models/page.py
+++ digidex/accounts/models/page.py
@@ -2,3 +2,13 @@ from base.models.body import BasePage
 
 class AccountIndexPage(BasePage):
     pass
+
+
+class LoginPage(BasePage):
+    parent_page_types = ["accounts.AccountIndexPage"]
+    pass
+
+
+class SignupPage(BasePage):
+    parent_page_types = ["accounts.AccountIndexPage"]
+    pass
