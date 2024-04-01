525a56d40691c72eee50b70b049725b9d127471b
Raphael <puello322@gmail.com>
2024-04-01 17:36:55 -0600

    Delete header model and update login and signup URLs

--- digidex/digidex/settings/base.py
+++ digidex/digidex/settings/base.py
@@ -208,9 +208,10 @@ WAGTAILSEARCH_BACKENDS = {
     }
 }
 
-#LOGIN_URL = '/authentication/login/'
-#LOGIN_REDIRECT_URL = '/authentication/login/'
-#WAGTAIL_FRONTEND_LOGIN_URL = LOGIN_URL
+SIGNUP_URL = '/accounts/signup/'
+LOGIN_URL = '/accounts/login/'
+WAGTAIL_FRONTEND_LOGIN_URL = LOGIN_URL
+
 # ALLAUTH settings
 AUTH_USER_MODEL = 'accounts.User'
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
