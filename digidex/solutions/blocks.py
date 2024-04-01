63fe64f0effcfdcb97a4aceffb5d647a43153263
Raphael <puello322@gmail.com>
2024-03-31 19:45:32 -0600

    Delete base/models.py and update solutions/blocks.py

--- digidex/solutions/blocks.py
+++ digidex/solutions/blocks.py
@@ -1,26 +1,22 @@
-from wagtail import blocks
-
 from base import blocks as _blocks
 
+class SolutionBlock(_blocks.ContentBlock):
+    page = _blocks.BasePageBlock(
+        required=False,
+        help_text="Link to a page"
+    )
 
-class ListItemBlock(blocks.StructBlock):
-    text = blocks.CharBlock(required=True, max_length=255, help_text="List item text")
-
-
-class SolutionBlock(blocks.StructBlock):
-    heading = blocks.CharBlock(required=True, max_length=255, help_text="Solution heading")
-    description = blocks.RichTextBlock(required=True, help_text="Solution description")
-    list_items = blocks.ListBlock(ListItemBlock(), help_text="List of items for the solution")
-
-
-class FeatureBlock(blocks.StructBlock):
-    heading = blocks.CharBlock(required=True, max_length=255, help_text="Feature heading")
-    paragraph = blocks.RichTextBlock(required=True, help_text="Feature description")
-
+class LargeSolutionBlock(_blocks.ContentBlock):
+    value_propositions = _blocks.BaseListBlock(
+        _blocks.BaseItemBlock(),
+        help_text="List of value propositions for the solution"
+    )
 
 class SolutionsStreamBlock(_blocks.BaseStreamBlock):
-    pass
+    solution = SolutionBlock()
 
+class FeatureBlock(_blocks.ContentBlock):
+    pass
 
 class FeaturesStreamBlock(_blocks.BaseStreamBlock):
-    pass
+    feature = FeatureBlock()
