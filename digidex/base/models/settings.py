cf22244f7982d674beaad7be0108e4d22d6e3286
Raphael <puello322@gmail.com>
2024-04-01 21:46:53 -0600

    Add AboutUsSettings, NewsAndEventsSettings, and SocialMediaSettings models*** ***Update get_footer template tag to include about_us and news_and_events*** ***Update footer.html to use AboutUsSettings and SocialMediaSettings

--- digidex/base/models/settings.py
+++ digidex/base/models/settings.py
@@ -4,12 +4,68 @@ from wagtail.admin import panels
 from wagtail.contrib.settings import models as wg_settings
 
 @wg_settings.register_setting
+class AboutUsSettings(wg_settings.BaseGenericSetting):
+    company = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    contact = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    panels = [
+        panels.MultiFieldPanel(
+            [
+                panels.FieldPanel('company'),
+                panels.FieldPanel('contact'),
+            ],
+            "About Us Links",
+        ),
+    ]
+
+
+@wg_settings.register_setting
+class NewsAndEventsSettings(wg_settings.BaseGenericSetting):
+    solutions = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    blog = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+
+    panels = [
+        panels.MultiFieldPanel(
+            [
+                panels.FieldPanel('blog'),
+                panels.FieldPanel('solutions'),
+            ],
+            "News & Events Links",
+        ),
+    ]
+
+
+@wg_settings.register_setting
 class SocialMediaSettings(wg_settings.BaseGenericSetting):
-    twitter_url = models.URLField(
+    twitter = models.URLField(
         verbose_name="Twitter URL",
         blank=True
     )
-    github_url = models.URLField(
+    github = models.URLField(
         verbose_name="GitHub URL",
         blank=True
     )
@@ -17,8 +73,8 @@ class SocialMediaSettings(wg_settings.BaseGenericSetting):
     panels = [       
         panels.MultiFieldPanel(
             [
-                panels.FieldPanel("twitter_url"),
-                panels.FieldPanel("github_url"),
+                panels.FieldPanel("twitter"),
+                panels.FieldPanel("github"),
             ],
             "Social Media Section Links",
         )
--- digidex/base/templatetags/navigation_tags.py
+++ digidex/base/templatetags/navigation_tags.py
@@ -9,9 +9,14 @@ register = template.Library()
 def get_footer(context):
     request = context.get('request')
     site = models.Site.find_for_request(request) if request else None
+    
+    about_us = _settings.AboutUsSettings.for_site(site) if site else _settings.AboutUsSettings.objects.first()
+    news_and_events = _settings.NewsAndEventsSettings.for_site(site) if site else _settings.NewsAndEventsSettings.objects.first()
     social_media = _settings.SocialMediaSettings.for_site(site) if site else _settings.SocialMediaSettings.objects.first()
 
     return {
+        'about_us': about_us,
+        'news_and_events': news_and_events,
         'social_media': social_media,
     }
 
@@ -19,4 +24,3 @@ def get_footer(context):
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
     return models.Site.find_for_request(context["request"]).root_page
-
