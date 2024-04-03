a284e610618adaae4ec4ad500afa14d5e78c1efa
Raphael <puello322@gmail.com>
2024-04-03 16:17:12 -0600

    Refactor block classes and templates

--- digidex/base/blocks.py
+++ digidex/base/blocks.py
@@ -1,85 +1,30 @@
 from wagtail import blocks
-from wagtail.embeds import blocks as _embeds
-from wagtail.images import blocks as _images
-
-class BaseStructBlock(blocks.StructBlock):
-    pass
+from wagtail.embeds import blocks as embed_blocks
+from wagtail.images import blocks as img_blocks
 
 
-class BaseStreamBlock(blocks.StreamBlock):
-    pass
+class LinkPageBlock(blocks.PageChooserBlock):
+    classname = "link-block base-inline-block"
+    required = False
 
 
-class BaseImageBlock(_images.ImageChooserBlock):
-    pass
+class LinkURLBlock(blocks.URLBlock):
+    classname = "link-block base-inline-block"
+    required = False
 
 
-class BaseURLBlock(blocks.URLBlock):
-    required=False
+class HeadingBlock(blocks.CharBlock):
+    classname="heading"
+    required=True
 
 
-class BasePageChooserBlock(blocks.PageChooserBlock):
-    required=False
+class ParagraphBlock(blocks.RichTextBlock):
+    classname="paragraph"
+    required=True
 
 
-class LinkBlock(BaseStructBlock):
-    internal = blocks.PageChooserBlock(
-        classname="heading",
-        required=False
-    )
-    external = blocks.URLBlock(
-        classname="heading",
-        required=False
-    )
-
-
-class ItemBlock(BaseStructBlock):
-    text = blocks.CharBlock(
-        required=False,
-        max_length=255
-    )
-    link = LinkBlock(
-        required=False
-    )
-
-
-class ButtonBlock(ItemBlock):
-    pass
-
-
-class IconBlock(BaseStructBlock):
-    image = BaseImageBlock(
-        required=True,
-        help_text="Select an icon image"
-    )
-    item = ItemBlock()
-
-    class Meta:
-        icon = "image"
-
-
-class HeadingBlock(BaseStructBlock):
-    title = blocks.CharBlock(
-        classname="heading",
-        required=True
-    )
-
-    class Meta:
-        icon = "title"
-
-
-class ParagraphBlock(BaseStructBlock):
-    paragraph = blocks.RichTextBlock(
-        classname="paragraph",
-        required=True
-    )
-
-    class Meta:
-        icon = "pilcrow"
-
-
-class ImageBlock(BaseStructBlock):
-    image = BaseImageBlock(
+class ImageBlock(blocks.StructBlock):
+    image = img_blocks.ImageChooserBlock(
         required=True
     )
     caption = blocks.CharBlock(
@@ -94,15 +39,21 @@ class ImageBlock(BaseStructBlock):
         template = "base/blocks/image_block.html"
 
 
-class PageBodyBlock(BaseStreamBlock):
-    heading = HeadingBlock()
-    paragraph = ParagraphBlock()
-    image = ImageBlock()
-    embed = _embeds.EmbedBlock(
-        help_text="Insert a URL to embed. For example, https://www.youtube.com/watch?v=SGJFWirQ3ks",
-        icon="media",
+class LearnMoreBlock(blocks.StructBlock):
+    link = LinkPageBlock(
+        classname="link-block",
+        required=True
+    )
+    text = blocks.CharBlock(
+        classname="text-link",
+        default="Learn more",
+        required=True
+    )
+    icon = img_blocks.ImageChooserBlock(
+        classname="icon-link",
+        required=True
     )
 
     class Meta:
-        icon = "placeholder"
-        template = "base/blocks/page_body_block.html"
+        icon = "doc-full"
+        template = "base/blocks/learn_more_block.html"
