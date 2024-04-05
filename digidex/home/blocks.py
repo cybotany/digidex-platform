9085d4ce2fc48343f75e563fcf0a7d0319564f45
Raphael <puello322@gmail.com>
2024-04-05 17:50:03 -0600

    Refactor hero block and remove unused template

--- digidex/home/blocks.py
+++ digidex/home/blocks.py
@@ -1,15 +1,8 @@
 from base.blocks import basic, lottie
 
-class HeroBlock(basic.BasicBlock):
+class HeroGrid(basic.StructGrid):
     heading = basic.Heading()
     paragraph = basic.Paragraph()
     button = basic.ButtonBlock()
 
-    class Meta:
-        icon = "placeholder"
-        template = "home/blocks/hero_block.html"
-
-
-class HeroGrid(basic.StructGrid):
-    cta = HeroBlock()
     lottie = lottie.Lottie()
