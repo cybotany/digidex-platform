5f7a805fef37ae6d241be260c49f293dfb0ce117
Raphael <rafael.e.puello@gmail.com>
2024-04-07 15:19:09 -0600

    Refactor settings files, add staticfiles configuration, update AWS storage settings, and load dotenv in base.py and production.py

--- digidex/digidex/settings/dev.py
+++ digidex/digidex/settings/dev.py
@@ -2,8 +2,6 @@ from .base import *
 
 DEBUG = True
 
-SECRET_KEY = "django-insecure-@u8i(hsjlxkjbn)x97pn#lypz#)^0&%viyw4f$^e#vgn2pen+@"
-
 ALLOWED_HOSTS = ["*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
