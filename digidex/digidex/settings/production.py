cb6e1430a4fa9170ed9fb12358f06fe33175d168
Raphael <rafael.e.puello@gmail.com>
2024-04-07 15:58:09 -0600

    Update .gitignore and production.py settings

--- digidex/digidex/settings/production.py
+++ digidex/digidex/settings/production.py
@@ -2,13 +2,15 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'digidex.app', 'www.digidex.app']
+ALLOWED_HOSTS = ['localhost', 'digidex.app']
 
 AWS_ACCESS_KEY_ID = os.environ.get('SPACES_ACCESS_KEY', '')
 AWS_SECRET_ACCESS_KEY = os.environ.get('SPACES_SECRET_KEY', '')
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('SPACES_BUCKET_NAME', '')
-AWS_S3_ENDPOINT_URL = 'https://sfo3.digitaloceanspaces.com'
+AWS_S3_ENDPOINT_URL = os.environ.get('SPACES_ENDPOINT_URL', '')
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('SPACES_CUSTOM_DOMAIN', '')
+
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -16,7 +18,7 @@ AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 STATIC_ROOT = 'static/'
 MEDIA_ROOT = 'media/'
