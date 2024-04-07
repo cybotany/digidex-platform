acbc01753ca29740f591cb72df279f5e4f45b541
Raphael <rafael.e.puello@gmail.com>
2024-04-07 06:57:13 -0600

    Remove unused code and models for company, support, products, ecommerce, categories, and solutions

--- digidex/solutions/models.py
+++
@@ -1,9 +0,0 @@
-from wagtail import models as wt_models
-
-
-class SolutionsIndexPage(wt_models.Page):
-    parent_page_types = ["home.HomePage"]
-
-    class Meta:
-        verbose_name = "Solutions Index Page"
-        verbose_name_plural = "Solutions Index Pages"
