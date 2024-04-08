009552fba48a62155b288b4766457e096150a805
Raphael <rafael.e.puello@gmail.com>
2024-04-07 19:22:10 -0600

    Refactor settings files, update AWS storage settings, and load dotenv in base.py and production.py

--- digidex/digidex/settings/production.py
+++ digidex/digidex/settings/production.py
@@ -17,7 +17,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 AWS_DEFAULT_ACL = 'public-read'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 STATIC_ROOT = 'static/'
