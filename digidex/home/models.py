9523b91aa60d1d53eac05f9b8dd34c442a231102
Raphael <rafael.e.puello@gmail.com>
2024-04-20 03:59:50 -0600

    Refactor home app models to use Django models and update content panels in HomePage model

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -1,24 +1,42 @@
-from wagtail import models
-from wagtail import fields
+from django.db import models
+
 from wagtail.admin import panels
+from wagtail import models as wt_models
 
-from base import blocks
-
-class HomePage(models.Page):
-    body = fields.StreamField(
-        [
-            ('heading', blocks.BasicHeadingBlock()),
-            ('paragraph', blocks.BasicParagraphBlock()),
-            ('image', blocks.BasicImageBlock()),
-            ('document', blocks.BasicDocumentBlock()),
-        ],
-        null=True,
+class HomePage(wt_models.Page):
+    hero_heading = models.CharField(
+        max_length=255,
+        verbose_name="Hero Heading"
+    )
+    hero_paragraph = models.TextField(
         blank=True,
-        use_json_field=True
+        verbose_name="Hero Paragraph"
+    )
+    hero_primary_cta_text = models.CharField(
+        max_length=255,
+        verbose_name="Primary CTA Text"
+    )
+    hero_primary_cta_link = models.URLField(
+        blank=True,
+        verbose_name="Primary CTA Link"
+    )
+    hero_secondary_cta_text = models.CharField(
+        max_length=255,
+        verbose_name="Secondary CTA Text"
+    )
+    hero_secondary_cta_link = models.URLField(
+        blank=True,
+        verbose_name="Secondary CTA Link"
     )
 
-    content_panels = models.Page.content_panels + [
-        panels.FieldPanel('body'),
+
+    content_panels = wt_models.Page.content_panels + [
+        panels.FieldPanel('hero_heading'),
+        panels.FieldPanel('hero_paragraph'),
+        panels.FieldPanel('hero_primary_cta_text'),
+        panels.FieldPanel('hero_primary_cta_link'),
+        panels.FieldPanel('hero_secondary_cta_text'),
+        panels.FieldPanel('hero_secondary_cta_link'),
     ]
 
     class Meta:
