5e9cb1ad46f3d5da381695b6dd44f5aa399d62fd
Raphael <rafael.e.puello@gmail.com>
2024-04-20 19:04:16 -0600

    Refactor home app models to update hero fields in HomePage model

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -13,33 +13,22 @@ class HomePage(wt_models.Page):
         blank=True,
         verbose_name="Hero Paragraph"
     )
-    hero_primary_cta_text = models.CharField(
+    hero_cta_text = models.CharField(
         blank=True,
         max_length=255,
-        verbose_name="Primary CTA Text"
+        verbose_name="Hero CTA Text"
     )
-    hero_primary_cta_link = models.URLField(
+    hero_cta_link = models.URLField(
         blank=True,
-        verbose_name="Primary CTA Link"
-    )
-    hero_secondary_cta_text = models.CharField(
-        blank=True,
-        max_length=255,
-        verbose_name="Secondary CTA Text"
-    )
-    hero_secondary_cta_link = models.URLField(
-        blank=True,
-        verbose_name="Secondary CTA Link"
+        verbose_name="Hero CTA Link"
     )
 
 
     content_panels = wt_models.Page.content_panels + [
         panels.FieldPanel('hero_heading'),
         panels.FieldPanel('hero_paragraph'),
-        panels.FieldPanel('hero_primary_cta_text'),
-        panels.FieldPanel('hero_primary_cta_link'),
-        panels.FieldPanel('hero_secondary_cta_text'),
-        panels.FieldPanel('hero_secondary_cta_link'),
+        panels.FieldPanel('hero_cta_text'),
+        panels.FieldPanel('hero_cta_link'),
     ]
 
     class Meta:
