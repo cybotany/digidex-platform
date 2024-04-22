b1b51ec7f19247766467561070887be795555a5a
Raphael <rafael.e.puello@gmail.com>
2024-04-22 09:09:58 -0600

    Refactor home app: Added LottieFeature model and updated home_page.html template

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -2,6 +2,32 @@ from django.db import models
 
 from wagtail.admin import panels
 from wagtail import models as wt_models
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+class LottieFeature(wt_models.Orderable):
+    page = models.ForeignKey(
+        'HomePage', 
+        on_delete=models.CASCADE,
+        related_name='lottie_features'
+    )
+    icon = models.ForeignKey(
+        'wagtailimages.Image', 
+        on_delete=models.SET_NULL, 
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+    text = models.CharField(
+        max_length=25,
+        blank=True
+    )
+
+    panels = [
+        ImageChooserPanel('icon'),
+        panels.FieldPanel('text'),
+    ]
+
 
 class HomePage(wt_models.Page):
     hero_heading = models.CharField(
@@ -23,12 +49,12 @@ class HomePage(wt_models.Page):
         verbose_name="Hero CTA Link"
     )
 
-
     content_panels = wt_models.Page.content_panels + [
         panels.FieldPanel('hero_heading'),
         panels.FieldPanel('hero_paragraph'),
         panels.FieldPanel('hero_cta_text'),
         panels.FieldPanel('hero_cta_link'),
+        panels.InlinePanel('lottie_features', label="Lottie Features"),
     ]
 
     class Meta:
