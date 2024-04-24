dbe4db89a30e0a3a91406fab66e8d97fbcdf5442
Raphael <rafael.e.puello@gmail.com>
2024-04-24 15:44:44 -0600

    Refactor navigation_tags.py: Import wagtail.models in navigation_tags.py

--- digidex/base/models.py
+++ digidex/base/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 from wagtail import models as wt_models
 from wagtail.admin import panels
-from wagtail.contrib.settings import models as wt_settings
-from wagtail.snippets import models as wt_snippets
+from wagtail.contrib.models import register_setting, BaseGenericSetting
+from wagtail.snippets.models import register_snippet
 
 
-@wt_settings.register_setting
-class SiteSettings(wt_settings.BaseGenericSetting):
+@register_setting
+class SiteSettings(BaseGenericSetting):
     logo = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -17,94 +17,14 @@ class SiteSettings(wt_settings.BaseGenericSetting):
         related_name="+",
         help_text="Company Logo."
     )
-    company = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-    )
-    contact = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-    )
-    email = models.EmailField(
-        null=True,
-        blank=True,
-        help_text='Support Email Address'
-    )
-    phone_number = models.CharField(
-        max_length=255,
-        null=True,
-        blank=True,
-        help_text='Support Phone Number'
-    )
-    chat = models.URLField(
-        null=True,
-        blank=True,
-        help_text='URL to launch Support Chat'
-    )
-    solutions = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-    )
-    blog = models.ForeignKey(
-        'wagtailcore.Page',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-    )
-    twitter = models.URLField(
-        verbose_name="Twitter URL",
-        blank=True
-    )
-    github = models.URLField(
-        verbose_name="GitHub URL",
-        blank=True
-    )
 
     panels = [
-        panels.MultiFieldPanel(
-            [   panels.FieldPanel('logo'),
-                panels.FieldPanel('company'),
-                panels.FieldPanel('contact'),
-            ],
-            "About Us Links",
-        ),
-        panels.MultiFieldPanel(
-            [
-                panels.FieldPanel("email"),
-                panels.FieldPanel("phone_number"),
-                panels.FieldPanel("chat"),
-            ],
-            "Support Contact Information",
-        ),
-        panels.MultiFieldPanel(
-            [
-                panels.FieldPanel('blog'),
-                panels.FieldPanel('solutions'),
-            ],
-            "News & Events Links",
-        ),
-        panels.MultiFieldPanel(
-            [
-                panels.FieldPanel("twitter"),
-                panels.FieldPanel("github"),
-            ],
-            "Social Media Section Links",
-        )
+        panels.FieldPanel('logo'),
     ]
 
 
-@wt_snippets.register_snippet
-class HeaderAdvertisement(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+@register_snippet
+class NavigationBar(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
     url = models.URLField(
         null=True,
         blank=True
@@ -131,43 +51,162 @@ class HeaderAdvertisement(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt
         }
 
     class Meta(wt_models.TranslatableMixin.Meta):
-        verbose_name_plural = "Advertisements"
+        verbose_name_plural = "Navigation Bars"
 
 
-@wt_snippets.register_snippet
-class PageFooter(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
-    paragraph = models.TextField(
+@register_snippet
+class PageFooter(models.Model):
+    copyright = models.CharField(
         max_length=255,
-        null=True
+        blank=True
     )
-    copyright = models.TextField(
-        max_length=50,
-        null=True
+    credits = models.CharField(
+        max_length=255,
+        blank=True
     )
-    credit = models.TextField(
+    phone_number = models.CharField(
+        verbose_name="Support Phone Number",
         max_length=50,
-        null=True
+        blank=True
+    )
+    email = models.EmailField(
+        verbose_name="Support Email Address",
+        max_length=255,
+        blank=True
+    )
+    chat = models.URLField(
+        verbose_name="Support Chat URL",
+        max_length=255,
+        blank=True
+    )
+    blog = models.URLField(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    company = models.URLField(
+        max_length=255,
+        blank=True
+    )
+    solutions = models.URLField(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    support = models.URLField(
+        max_length=255,
+        blank=True
+    )
+    github = models.URLField(
+        verbose_name="GitHub URL",
+        max_length=255,
+        blank=True
+    )
+    twitter = models.URLField(
+        verbose_name="Twitter URL",
+        max_length=255,
+        blank=True
     )
 
     panels = [
-        panels.FieldPanel("paragraph"),
-        panels.FieldPanel("copyright"),
-        panels.FieldPanel("credit"),
+        panels.MultiFieldPanel(
+            [
+                panels.FieldPanel('copyright'),
+                panels.FieldPanel('credits'),
+            ],
+            "Main Footer Contents"),
+        panels.MultiFieldPanel(
+            [
+                panels.FieldPanel('phone_number'),
+                panels.FieldPanel('email'),
+                panels.FieldPanel('chat'),
+            ],
+            "Support Contact Information"),
+        panels.MultiFieldPanel(
+            [
+                panels.FieldPanel('blog'),
+                panels.FieldPanel('company'),
+                panels.FieldPanel('solutions'),
+                panels.FieldPanel('support'),
+            ],
+            "Useful Links"),
+        panels.MultiFieldPanel(
+            [
+                panels.FieldPanel("twitter"),
+                panels.FieldPanel("github"),
+            ],
+            "Social Media Links"),
+    ]
+
+    def __str__(self):
+        return "Footer Settings"
+
+    class Meta:
+        verbose_name = "Footer"
+        verbose_name_plural = "Footer Settings"
+
+
+@register_snippet
+class AdvertBanner(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+    url = models.URLField(
+        null=True,
+        blank=True
+    )
+    text = models.TextField(
+        max_length=255
+    )
+
+    panels = [
+        panels.FieldPanel("url"),
+        panels.FieldPanel("text"),
         panels.PublishingPanel(),
     ]
 
     def __str__(self):
-        return "Page Footer"
+        return self.text
 
     def get_preview_template(self, request, mode_name):
         return "base.html"
 
     def get_preview_context(self, request, mode_name):
         return {
-            "paragraph": self.paragraph,
-            "copyright": self.copyright,
-            "credit": self.credit,
+            "header_text": self.text,
         }
 
     class Meta(wt_models.TranslatableMixin.Meta):
-        verbose_name_plural = "Footer Contents"
+        verbose_name_plural = "Advert Banners"
+
+
+@register_snippet
+class CallToActionBanner(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+    url = models.URLField(
+        null=True,
+        blank=True
+    )
+    text = models.TextField(
+        max_length=255
+    )
+
+    panels = [
+        panels.FieldPanel("url"),
+        panels.FieldPanel("text"),
+        panels.PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return self.text
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {
+            "header_text": self.text,
+        }
+
+    class Meta(wt_models.TranslatableMixin.Meta):
+        verbose_name_plural = "Call To Action Banners"
--- digidex/base/templatetags/navigation_tags.py
+++ digidex/base/templatetags/navigation_tags.py
@@ -1,4 +1,5 @@
 from django import template
+
 from wagtail import models
 
 register = template.Library()
