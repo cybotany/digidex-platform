107eb78e76002f02a4b6b6af60af801b1271a014
Raphael <rafael.e.puello@gmail.com>
2024-04-24 09:59:36 -0600

    Refactor UserProfile model: Move it to a separate file and update related imports

--- digidex/accounts/models.py
+++ digidex/accounts/models.py
@@ -28,63 +28,6 @@ class User(AbstractUser):
     )
 
 
-class UserProfile(models.Model):
-    """
-    User profile model for storing additional user information.
-
-    Fields:
-        user (OneToOneField): A one-to-one reference to the User model.
-        bio (TextField): A text field for user biography, maximum length 500 characters.
-        location (CharField): A char field for user location, maximum length 30 characters.
-        avatar (ImageField): An image field for user's profile picture.
-        is_public (BooleanField): A boolean field to determine if the profile is public or private. Profile is private by default.
-        created_at (DateTimeField): The date and time when the profile was created.
-        last_modified (DateTimeField): The date and time when the profile was last modified.
-    """
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        help_text='The user associated with this profile.'
-    )
-    bio = models.TextField(
-        max_length=500,
-        blank=True,
-        help_text='A short biography of the user.'
-    )
-    location = models.CharField(
-        max_length=30,
-        blank=True,
-        help_text='The location of the user.'
-    )
-    is_public = models.BooleanField(
-        default=False,
-        help_text='Indicates if the profile should be publicly visible to the public or private. Profile is private by default.'
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name="Created At",
-        help_text="The date and time when the profile was created."
-    )
-    last_modified = models.DateTimeField(
-        auto_now=True,
-        verbose_name="Last Modified",
-        help_text="The date and time when the profile was last modified."
-    )
-
-    def __str__(self):
-        """
-        Returns a string representation of the user's profile.
-
-        Returns:
-            str: A string in the format "<username>'s Profile".
-        """
-        return f"{self.user.username}'s Profile"
-
-    class Meta:
-        verbose_name = "Profile"
-        verbose_name_plural = "Profiles"
-
-
 class UserProfilePage(wt_models.Page):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
