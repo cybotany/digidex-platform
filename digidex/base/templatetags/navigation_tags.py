44f285ced70622453a41e96d40aedf6b9041a66f
Raphael <rafael.e.puello@gmail.com>
2024-04-26 16:13:05 -0600

    Refactor navigation_tags.py to remove unused code

--- digidex/base/templatetags/navigation_tags.py
+++ digidex/base/templatetags/navigation_tags.py
@@ -1,15 +1,8 @@
 from django import template
 from wagtail.models import Site
-from wagtail.contrib.settings.context_processors import SettingsProxy
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
     return Site.find_for_request(context["request"]).root_page
-
-@register.simple_tag(takes_context=True)
-def get_navigation_settings(context):
-    site = Site.find_for_request(context['request'])
-    settings = SettingsProxy(site)
-    return settings.for_site(site).navigationsettings 
