14e9f49e88344509d785a1a4d1bc126771af70ea
Raphael <rafael.e.puello@gmail.com>
2024-04-26 13:48:03 -0600

    Refactor header templates and remove unused code

---
+++ digidex/base/models/navbar.py
@@ -0,0 +1,114 @@
+# base/models/header.py
+from django.db import models
+
+from wagtail import models as wt_models
+from wagtail.admin import panels
+from wagtail.snippets.models import register_snippet
+
+
+@register_snippet
+class NavbarMenu(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+    text = models.TextField(
+        max_length=255
+    )
+
+    panels = [
+        panels.FieldPanel("url"),
+        panels.FieldPanel("text"),
+        panels.PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return self.text
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {
+            "header_text": self.text,
+        }
+
+    class Meta(wt_models.TranslatableMixin.Meta):
+        verbose_name = "Navigation Bar"
+
+
+@register_snippet
+class Navbar(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+    text = models.TextField(
+        max_length=255
+    )
+
+    panels = [
+        panels.FieldPanel("url"),
+        panels.FieldPanel("text"),
+        panels.PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return self.text
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {
+            "header_text": self.text,
+        }
+
+    class Meta(wt_models.TranslatableMixin.Meta):
+        verbose_name = "Navigation Bar"
+
+
+@register_snippet
+class NavbarMenu(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+    text = models.TextField(
+        max_length=255
+    )
+
+    panels = [
+        panels.FieldPanel("url"),
+        panels.FieldPanel("text"),
+        panels.PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return self.text
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {
+            "header_text": self.text,
+        }
+
+    class Meta(wt_models.TranslatableMixin.Meta):
+        verbose_name = "Navigation Bar"
+
+
+@register_snippet
+class NavbarMenu(wt_models.DraftStateMixin, wt_models.RevisionMixin, wt_models.PreviewableMixin, wt_models.TranslatableMixin, models.Model):
+    text = models.TextField(
+        max_length=255
+    )
+
+    panels = [
+        panels.FieldPanel("url"),
+        panels.FieldPanel("text"),
+        panels.PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return self.text
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {
+            "header_text": self.text,
+        }
+
+    class Meta(wt_models.TranslatableMixin.Meta):
+        verbose_name = "Navigation Bar"
