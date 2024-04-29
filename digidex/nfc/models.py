6c65083da47f4246a25c788fce2b834bf6f6bebc
Raphael <rafael.e.puello@gmail.com>
2024-04-29 14:12:28 -0600

    Refactor NearFieldCommunicationTag model, rename digit field to digit_page in models.py, and update migrations in nfc app

--- digidex/nfc/models.py
+++ digidex/nfc/models.py
@@ -1,42 +1,6 @@
 from django.db import models
 
-from modelcluster.fields import ParentalKey
-
-from wagtail.admin.panels import (
-    FieldPanel,
-    FieldRowPanel,
-    InlinePanel,
-    MultiFieldPanel
-)
-from wagtail.fields import RichTextField
-from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
-from wagtail.contrib.forms.panels import FormSubmissionsPanel
-
-
-from inventory.models import DigitPage
-
-
-class FormField(AbstractFormField):
-    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
-
-
-class FormPage(AbstractEmailForm):
-    intro = RichTextField(blank=True)
-    thank_you_text = RichTextField(blank=True)
-
-    content_panels = AbstractEmailForm.content_panels + [
-        FormSubmissionsPanel(),
-        FieldPanel('intro'),
-        InlinePanel('form_fields', label="Form fields"),
-        FieldPanel('thank_you_text'),
-        MultiFieldPanel([
-            FieldRowPanel([
-                FieldPanel('from_address'),
-                FieldPanel('to_address'),
-            ]),
-            FieldPanel('subject'),
-        ], "Email"),
-    ]
+from inventory.models import Digit
 
 
 class NearFieldCommunicationTag(models.Model):
@@ -56,8 +20,8 @@ class NearFieldCommunicationTag(models.Model):
         db_index=True,
         verbose_name="Tag Serial Number"
     )
-    digit_page = models.ForeignKey(
-        DigitPage,
+    digit = models.ForeignKey(
+        Digit,
         blank=True,
         null=True,
         on_delete=models.CASCADE,
