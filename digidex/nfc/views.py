c8ed5deb4cab4c9a05e9c786228226af937bd522
Raphael <rafael.e.puello@gmail.com>
2024-04-28 19:01:38 -0600

    Add API files for Django app

--- digidex/nfc/views.py
+++ digidex/nfc/views.py
@@ -1,12 +1,29 @@
-from rest_framework import viewsets
+from django import views
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect
 
 from nfc.models import NearFieldCommunicationTag
-from nfc.serializers import NearFieldCommunicationTagSerializer
 
-class NearFieldCommunicationTagViewSet(viewsets.ModelViewSet):
 
-    queryset = NearFieldCommunicationTag.objects.all()
-    serializer_class = NearFieldCommunicationTagSerializer
+class LinkDigit(views.View):
+    template_name = "inventory/digit/creation-page.html"
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def get_object(self):
+        serial_number = self.kwargs.get('serial_number')
+        if not serial_number:
+            raise Http404("No serial number provided")
+        return get_object_or_404(NearFieldCommunicationTag, serial_number=serial_number)
+
+    def get(self, request, *args, **kwargs):
+        ntag = self.get_object()
+        linked_digit = ntag.get_digit()
+        if ntag.active and linked_digit:
+            return HttpResponseRedirect(linked_digit.get_absolute_url())
+        else:
+            FormClass, _ = self.get_form_and_model(ntag.use)
+            form = FormClass(**self.get_form_kwargs())
+            context = {
+                'form': form,
+                'ntag': ntag
+            }
+            return render(request, self.template_name, context)
