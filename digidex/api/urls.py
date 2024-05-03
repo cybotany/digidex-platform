46807f1dddf907f9a237fb3f835b05671ed25e69
Raphael <rafael.e.puello@gmail.com>
2024-05-03 15:14:59 -0600

    Add REST framework authentication and permission settings

--- digidex/api/urls.py
+++ digidex/api/urls.py
@@ -1,7 +1,11 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 
 from api.views import RegisterNearFieldCommunicationTag
 
 urlpatterns = [
-    path('register/ntag/', RegisterNearFieldCommunicationTag.as_view(), name='register-ntag'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('ntag/register/', RegisterNearFieldCommunicationTag.as_view(), name='register-ntag'),
 ]
