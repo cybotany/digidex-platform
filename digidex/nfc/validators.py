1de31f084a24aaebb06fc2536ce6374c3a17060e
Raphael <rafael.e.puello@gmail.com>
2024-05-06 15:04:38 -0600

    Add NFC tag model and validators for serial number field

--- digidex/nfc/validators.py
+++ digidex/nfc/validators.py
@@ -4,6 +4,6 @@ SERIAL_NUMBER_REGEX = '^([0-9A-Fa-f]{2}:){9}[0-9A-Fa-f]{2}$'
 
 serial_number_validator = RegexValidator(
     regex=SERIAL_NUMBER_REGEX,
-    message=_("Serial number must be in the format XX:XX:XX:XX:XX:XX:XX"),
+    message=("Serial number must be in the format XX:XX:XX:XX:XX:XX:XX"),
     code='invalid_serial_number'
 )
