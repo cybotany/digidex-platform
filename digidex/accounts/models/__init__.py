fa8bf1c298f1de0cee812932d7e01518aab29882
Raphael <rafael.e.puello@gmail.com>
2024-05-07 12:14:21 -0600

    Refactor user collection creation and linking in User model

--- digidex/accounts/models/__init__.py
+++ digidex/accounts/models/__init__.py
@@ -1,4 +1,2 @@
-from .user import User
-from .user_profile import UserProfile
-from .user_profile_page import UserProfilePage
-from .user_collection import UserCollection
+from .user import User, UserCollection
+from .user_profile import UserProfile, UserProfileIndexPage, UserProfilePage
