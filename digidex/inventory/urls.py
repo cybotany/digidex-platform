3e50af57fa47fcd74b479647ef007bf3292bde85
Raphael <rafael.e.puello@gmail.com>
2024-05-10 13:55:12 -0600

    Refactor inventory URLs to remove unused code and update app name

--- digidex/inventory/urls.py
+++ digidex/inventory/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from inventory import views
 
-app_name = 'inventory'
 urlpatterns = [
     path('link/ntag/<uuid:ntag_uuid>/', views.link_ntag_and_digit, name='link_ntag'),
     path('add/digit/<uuid:digit_uuid>/note/', views.add_digit_note, name='add_digit_note'),
