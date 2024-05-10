46ab1c4fb642ad3f756ab578c14baf160965256a
Raphael <rafael.e.puello@gmail.com>
2024-05-10 07:19:59 -0600

    Remove digitization URLs and views

--- digidex/nfc/views.py
+++ digidex/nfc/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from django.core.exceptions import ValidationError
+from django.contrib.auth.decorators import login_required
 
 from nfc.models import NearFieldCommunicationTag
+from inventory.forms import UserDigitizedObjectForm, UserDigitizedObjectJournalForm
+from inventory.models import UserDigitizedObject
 
 
 def route_ntag_url(request, _uuid):
@@ -17,3 +20,34 @@ def route_ntag_url(request, _uuid):
         return redirect(digit_page_url)
     except ValidationError as e:
         return HttpResponse(str(e), status=400)
+
+
+@login_required
+def link_ntag_and_digit(request, ntag_uuid):
+    user_profile = request.user.profile
+    if request.method == 'POST':
+        form = UserDigitizedObjectForm(request.POST)
+        if form.is_valid():
+            digitized_object = form.save(user_profile, commit=True)
+            ntag = get_object_or_404(NearFieldCommunicationTag, uuid=ntag_uuid)
+            ntag.digitized_object = digitized_object
+            ntag.save()
+            return redirect(digitized_object.get_associated_page_url())
+    else:
+        form = UserDigitizedObjectForm()
+
+    return render(request, "digitization/link_ntag_and_digit.html", {'form': form})
+
+@login_required
+def link_digit_and_journal(request, digit_uuid):
+    digitized_object = get_object_or_404(UserDigitizedObject, uuid=digit_uuid)
+
+    if request.method == 'POST':
+        form = UserDigitizedObjectJournalForm(request.POST, request.FILES)
+        if form.is_valid():
+            journal_entry = form.save(digitized_object, commit=True)
+            return redirect(journal_entry.get_associated_page_url())
+    else:
+        form = UserDigitizedObjectJournalForm()
+
+    return render(request, "digitization/link_digit_and_journal.html", {'form': form})
