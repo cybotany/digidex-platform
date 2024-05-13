59541b4219586549aaa7baac4c60d9db0c7dc766
Raphael <rafael.e.puello@gmail.com>
2024-05-13 07:35:26 -0600

    Refactor static file and media directories in dev.py

--- digidex/digidex/settings/dev.py
+++ digidex/digidex/settings/dev.py
@@ -6,6 +6,14 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 WAGTAILADMIN_BASE_URL = "http://localhost:8000"
 
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 SECURE_PROXY_SSL_HEADER = None
 
 SESSION_COOKIE_SECURE = False
