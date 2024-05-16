9d9ae04cdad67761bbaf7a0a83da67b302b7d7c7
Raphael <rafael.e.puello@gmail.com>
2024-05-16 09:43:58 -0600

    Update DJANGO_SETTINGS_MODULE to production in wsgi.py

--- digidex/digidex/wsgi.py
+++ digidex/digidex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digidex.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "digidex.settings.production")
 
 application = get_wsgi_application()
