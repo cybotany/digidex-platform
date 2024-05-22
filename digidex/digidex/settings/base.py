8684d0aba7f6bc404947068a8e1010a916118333
Raphael <rafael.e.puello@gmail.com>
2024-05-21 18:29:29 -0600

    Refactor static file paths for CSS files in base.html templates

--- digidex/digidex/settings/base.py
+++ digidex/digidex/settings/base.py
@@ -163,12 +163,7 @@ STATIC_URL = '{}/static/'.format(AWS_S3_CUSTOM_DOMAIN)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "digidex"),
-]
-
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
