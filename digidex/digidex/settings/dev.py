d0ff3dbf4799a63877bccfabc813ba47e9b4105c
Raphael <rafael.e.puello@gmail.com>
2024-05-22 08:00:12 -0600

    Refactor ALLOWED_HOSTS configuration to include additional IP address

--- digidex/digidex/settings/dev.py
+++ digidex/digidex/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '192.168.1.100', 'dev.digidex.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '192.168.1.100', 'dev.digidex.app', '10.0.0.218']
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
