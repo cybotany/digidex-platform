8100df861deaee323db0870e578c2a68fee7c9eb
Raphael <rafael.e.puello@gmail.com>
2024-06-08 15:57:53 -0600

    Refactor HTML class names for account elements

--- digidex/digidex/settings/production.py
+++ digidex/digidex/settings/production.py
@@ -74,7 +74,7 @@ if "EMAIL_HOST" in os.environ:
     EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    DEFAULT_FROM_EMAIL = 'no-reply@digidex.app'
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 SESSION_COOKIE_SECURE = True
 
