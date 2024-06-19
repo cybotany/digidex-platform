874b115db872ea295f11c93b07a47743f9d0757f
Raphael <rafael.e.puello@gmail.com>
2024-06-19 16:49:33 -0600

    Refactor user profile page to improve layout and organization

--- digidex/accounts/models.py
+++ digidex/accounts/models.py
@@ -1,8 +1,7 @@
 import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-from wagtail.models import Collection
 
 
 class User(AbstractUser):
@@ -13,27 +12,6 @@ class User(AbstractUser):
         db_index=True,
         verbose_name="User UUID"
     )
-    slug = models.SlugField(
-        unique=True,
-        db_index=True,
-        max_length=255,
-        verbose_name="User Slug"
-    )
-    collection = models.ForeignKey(
-        Collection,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='+'
-    )
-
-    @property
-    def formatted_date(self):
-        return self.date_joined.strftime('%B %d, %Y')
-    
-    @property
-    def formatted_name(self):
-        return self.username.title()
 
     def __str__(self):
-        return f"User: {self.formatted_name}"
+        return f"DigiDex User: {self.username}"
