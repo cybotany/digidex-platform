269ebf5f85c58bdac0eda2a9371700af6eeb22d4
Raphael <rafael.e.puello@gmail.com>
2024-06-20 13:29:58 -0600

    Refactor API URLs to include endpoints for Wagtail API

--- digidex/api/views/__init__.py
+++ digidex/api/views/__init__.py
@@ -1,2 +1,3 @@
 from .gbif import species_suggestions_view, species_backbone_view
 from .nfc import RegisterNearFieldCommunicationTag
+from .wagtail import api_router
---
+++ digidex/api/views/wagtail.py
@@ -0,0 +1,13 @@
+from wagtail.api.v2.views import PagesAPIViewSet
+from wagtail.images.api.v2.views import ImagesAPIViewSet
+from wagtail.documents.api.v2.views import DocumentsAPIViewSet
+from wagtail.contrib.redirects.api import RedirectsAPIViewSet
+from wagtail.api.v2.router import WagtailAPIRouter
+
+
+api_router = WagtailAPIRouter('wagtailapi')
+
+api_router.register_endpoint('pages', PagesAPIViewSet)
+api_router.register_endpoint('images', ImagesAPIViewSet)
+api_router.register_endpoint('documents', DocumentsAPIViewSet)
+api_router.register_endpoint('redirects', RedirectsAPIViewSet)
