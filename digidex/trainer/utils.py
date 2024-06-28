1dd40c006059945c55cb6403dc5f5d68343bb92b
Raphael <rafael.e.puello@gmail.com>
2024-06-27 18:14:48 -0600

    Refactor get_user_collection function to use wagtail.models.Collection

--- digidex/trainer/utils.py
+++ digidex/trainer/utils.py
@@ -1,6 +1,23 @@
-from home.utils import get_user_collection
+from wagtail.models import Collection
+
+def get_root_collection():
+    root_collection = Collection.get_first_root_node()
+    if not root_collection:
+        root_collection = Collection.add_root(name='Root')
+    return root_collection
+
+def get_user_collection():
+    root_collection = get_root_collection()
+    try:
+        user_collection = Collection.objects.get(name='Users')
+    except Collection.DoesNotExist:
+        user_collection = root_collection.add_child(name='Users')
+    return user_collection
 
 def create_trainer_collection(user):
     user_collection = get_user_collection()
-    trainer_collection = user_collection.add_child(name=user.username)
+    try:
+        trainer_collection = Collection.objects.get(name=user.username)
+    except Collection.DoesNotExist:
+        trainer_collection = user_collection.add_child(name=user.username)
     return trainer_collection
