930846882aa4583186442d157e284f3b155a1cf7
Raphael <rafael.e.puello@gmail.com>
2024-07-06 15:21:29 -0600

    Refactor inventory templates for better organization and maintainability

--- digidex/inventory/components.py
+++ digidex/inventory/components.py
@@ -5,9 +5,16 @@ from wagtail.admin.ui.components import Component
 
 class WelcomePanel(Component):
     order = 50
-    template_name = 'inventory/welcome_panel.html'
+    template_name = 'inventory/panels/welcome.html'
 
     def get_context_data(self, parent_context):
         context = super().get_context_data(parent_context)
         context['username'] = parent_context['request'].user.username
         return context
+
+class CatagoryPanel(Component):
+    template_name = 'inventory/panels/catagory.html'
+
+
+welcome_panel = WelcomePanel()
+catagory_panel = CatagoryPanel()
--- digidex/inventory/wagtail_hooks.py
+++ digidex/inventory/wagtail_hooks.py
@@ -1,8 +1,15 @@
 from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
 
 from inventory.models import Inventory, Category, Item, Link
 
-register_snippet(Inventory)
+class InventoryViewSet(SnippetViewSet):
+    model = Inventory
+    icon = 'folder-open-inverse'
+    list_display = ('name', 'created_at', 'last_modified')
+    search_fields = ('name', 'body')
+    fields = ('name', 'body')
+
+
 register_snippet(Category)
 register_snippet(Item)
-register_snippet(Link)
