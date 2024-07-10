79c740562130016515023a1b13568877ad318ba7
Raphael <rafael.e.puello@gmail.com>
2024-07-10 13:44:50 -0600

    Refactor navigation and link components for improved styling and responsiveness

--- digidex/inventory/views.py
+++ digidex/inventory/views.py
@@ -3,9 +3,18 @@ from django.shortcuts import render
 from inventory.components import build_user_navigation, build_top_panel
 
 def inventory(request):
+    template = 'inventory/index.html'
     top_panel = build_top_panel(request.user)
     navigation_panel = build_user_navigation(request.user)
-    panels = [navigation_panel, top_panel]
-    template = 'inventory/index.html'
+    context = {
+        'panels': [
+            navigation_panel,
+            top_panel
+        ]
+    }
  
-    return render(request, template, {'panels': panels})
+    return render(
+        request,
+        template,
+        context
+    )
