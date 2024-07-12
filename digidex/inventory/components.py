ab66214320a2596bc15e5184dad71191e828e1db
Raphael <rafael.e.puello@gmail.com>
2024-07-12 14:04:45 -0600

    Refactor inventory templates to improve code organization and maintainability

--- digidex/inventory/components.py
+++ digidex/inventory/components.py
@@ -1,6 +1,19 @@
+from django.urls import reverse
+
 from laces.components import Component
 
-from base.components import HeadingComponent, ParagraphComponent
+from base.components import (
+    SectionComponent,
+    BlockComponent,
+    HeadingComponent,
+    ParagraphComponent,
+    LinkComponent,
+    TextComponent,
+    CollectionComponent,
+    EmptyComponent,
+    ButtonComponent,
+)
+from inventory.models import InventoryIndex
 
 
 class ItemComponent(Component):
@@ -38,6 +51,19 @@ class ItemComponent(Component):
         }
 
 
+class ItemCollectionPanel(Component):
+    template_name = 'inventory/panels/items.html'
+
+    def __init__(self, items):
+        self.items = items
+        self.children = [ItemComponent(item) for item in items]
+
+    def get_context_data(self, parent_context=None):
+        return {
+            "children": self.children
+        }
+
+
 class CategoryComponent(Component):
     template_name = 'inventory/components/category.html'
 
@@ -49,3 +75,76 @@ class CategoryComponent(Component):
             "url": self.category.get('url', ''),
             "text": self.category.get('text', ''),
         }
+
+
+class CategoryCollectionPanel(Component):
+    template_name = 'inventory/panels/categories.html'
+
+    def __init__(self, categories):
+        self.categories = categories
+        self.children = [CategoryComponent(category) for category in categories]
+
+    def get_context_data(self, parent_context=None):
+        return {
+            "children": self.children
+        }
+
+
+class DashboardComponent(Component):
+    template_name = 'inventory/panels/dashboard.html'
+
+    def __init__(self, user):
+        self.user = user
+        self.inventory = InventoryIndex.objects.get(owner=user)
+        self.categories = self.inventory.get_categories()
+        self.party = self.inventory.get_party()
+        self.items = self.inventory.get_items()
+        self.panels = [
+            self.get_body_panel(),
+        ]
+
+    def get_context_data(self, parent_context=None):
+        return {
+            "panels": self.panels
+        }
+
+    def get_body_panel(self):
+        items = self.items
+        children = []
+        count_of_items = len(items)
+
+        if  count_of_items >= 1:
+            featured_item = items.pop(0)
+            featured_panel = self._get_featured_item_panel(featured_item)
+            children.append(featured_panel)
+
+            # Check if there are any items left
+            if items:
+                items_panel = self._get_items_panel(items)
+                children.append(items_panel)
+
+        else:
+            empty_component = self._get_empty_panel(assets="items")
+            children.append(empty_component)
+
+        panel = SectionComponent(
+            children=children
+        )
+        return panel
+
+
+    def _get_featured_item_panel(self, featured_item):
+        return FeaturedItemComponent(featured_item)
+
+
+    def _get_items_panel(self, items):
+        style = 'posts'
+        item_components = [ItemComponent(item) for item in items]
+        panel = CollectionComponent(
+            children=item_components,
+            style=style
+        )
+        return panel
+
+    def _get_empty_panel(self, asset):
+        return EmptyComponent(asset=asset)
