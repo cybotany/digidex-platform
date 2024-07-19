457f60e469684162adb842cf079d6c39685f1bb6
Raphael <rafael.e.puello@gmail.com>
2024-07-18 22:13:52 -0600

    Refactor inventory templates for better organization and maintainability

--- digidex/base/templatetags/navigation_tags.py
+++ digidex/base/templatetags/navigation_tags.py
@@ -1,24 +1,10 @@
-import os
-
 from django import template
-from django.utils.text import slugify
 
 from wagtail.models import Site
+
 
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
     return Site.find_for_request(context["request"]).root_page
-
-@register.simple_tag(takes_context=True)
-def get_user_page_url(context):
-    request = context['request']
-    user = request.user
-
-    if user.is_authenticated:
-        return os.path.join(
-            "/",
-            slugify(user.username)
-        )
-    return None
--- digidex/home/models.py
+++ digidex/home/models.py
@@ -5,8 +5,6 @@ from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 
-from home.components import NavigationComponent
-
 
 class HomePage(Page):
     subpage_types = [
@@ -22,15 +20,3 @@ class HomePage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body'),
     ]
-
-    def get_navigation(self, request):
-        return NavigationComponent(request.user)
-
-    def get_body(self, request):
-        pass
-
-    def get_context(self, request):
-        context = super().get_context(request)
-        context['navigation'] = self.get_navigation(request)
-        context['body'] = self.get_body(request)
-        return context
