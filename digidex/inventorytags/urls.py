a04f3f1220159dcb89e587eed04be43beeafddbc
Raphael <rafael.e.puello@gmail.com>
2024-07-22 16:23:07 -0600

    Refactor URL patterns to use root path and update link-related names for better organization and maintainability

--- digidex/inventorytags/urls.py
+++ digidex/inventorytags/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from inventorytags.views import link
 
 urlpatterns = [
-    path("<uuid:uuid>/", link, name="link"),
+    path("<uuid:uuid>/", link, name="link-tag"),
 ]
