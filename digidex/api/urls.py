72c13e894c35937fe6c7a93c88d6945495c654b2
Raphael <rafael.e.puello@gmail.com>
2024-07-21 23:22:30 -0600

    Refactor inventory URLs to use root path for better organization and maintainability

--- digidex/api/urls.py
+++ digidex/api/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('inventory/', include(inventory_router.urls)),
+    path('', include(inventory_router.urls)),
 ]
