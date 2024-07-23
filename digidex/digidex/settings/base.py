c3ca6bcd9d61035b3fed86b156b0d15ec1a09f8e
Raphael <rafael.e.puello@gmail.com>
2024-07-22 21:43:20 -0600

    Refactor template directory path for better organization and maintainability

--- digidex/digidex/settings/base.py
+++ digidex/digidex/settings/base.py
@@ -102,7 +102,9 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [
+            os.path.join(PROJECT_DIR, "templates"),
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
