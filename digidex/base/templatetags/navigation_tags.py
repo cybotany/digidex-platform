8bb2542c4ba93f1ee28043ce40b825684ec9ce04
Raphael <rafael.e.puello@gmail.com>
2024-07-26 15:47:48 -0600

    Refactor navigation links in header template

--- digidex/base/templatetags/navigation_tags.py
+++ digidex/base/templatetags/navigation_tags.py
@@ -4,32 +4,20 @@ from django.urls import reverse
 from wagtail.models import Site
 
 from base.models import SiteLogo, FooterParagraph, FooterCopyright
-from base.components import ParagraphComponent, ButtonComponent, LinkComponent
+from base.components import ParagraphComponent, ButtonComponent
 
 
 register = template.Library()
 
-def get_inventory(user):
-    from inventory.models import UserInventoryIndex
-    return UserInventoryIndex.objects.get(owner=user)
-
-
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
     return Site.find_for_request(context["request"]).root_page
-
 
 @register.simple_tag(takes_context=True)
 def get_site_logo(context):
     instance = SiteLogo.objects.first()
     digidex_logo = instance.logo if instance else ""
     return digidex_logo
-
-
-@register.simple_tag(takes_context=True)
-def get_site_root(context):
-    return Site.find_for_request(context["request"]).root_page
-
 
 @register.inclusion_tag("base/includes/footer/paragraph.html", takes_context=True)
 def get_footer_paragraph(context):
@@ -49,7 +37,6 @@ def get_footer_paragraph(context):
         "footer_paragraph": footer_paragraph,
     }
 
-
 @register.inclusion_tag("base/includes/footer/copyright.html", takes_context=True)
 def get_footer_copyright(context):
     footer_copyright = context.get("footer_copyright", "")
@@ -61,32 +48,6 @@ def get_footer_copyright(context):
     return {
         "footer_copyright": footer_copyright,
     }
-
-
-@register.inclusion_tag("base/includes/navigation/links.html", takes_context=True)
-def get_navigation_links(context):
-    links = []
-
-    request = context["request"]
-    if request.user.is_authenticated:
-        inventory = get_inventory(request.user)
-        #party = inventory.get_party()
-        #items = party.get_items()
-
-        items = []
-        for item in items:
-            links.append(
-                LinkComponent(
-                    url = item.url,
-                    text = item.name,
-                    style = 'nav'
-                )
-            )
-
-    return {
-        "links": links,
-    }
-
 
 @register.inclusion_tag("base/includes/navigation/buttons.html", takes_context=True)
 def get_navigation_buttons(context):
