e97664899945bf6d50ee93035e01e2ad3f768ff6
Raphael <rafael.e.puello@gmail.com>
2024-07-28 11:31:00 -0600

    Refactor inventory templates to remove footer inclusion

--- digidex/home/models.py
+++ digidex/home/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.core.exceptions import PermissionDenied
 from django.utils.translation import gettext_lazy as _
-from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 
 from wagtail.contrib.routable_page.models import RoutablePageMixin, path, re_path
-from wagtail.models import Page, Collection
+from wagtail.models import Page, Collection, Site
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
 
 from inventory.models import UserInventory, InventoryCategory, InventoryAsset
+from inventory.forms import UserInventoryForm, InventoryCategoryForm, InventoryAssetForm, DeletionConfirmationForm
 
 
 class HomePage(RoutablePageMixin, Page):
@@ -36,17 +40,85 @@ class HomePage(RoutablePageMixin, Page):
         return self.title
 
     @path('<slug:inventory_slug>/')
-    def user_inventory(self, request, inventory_slug):
+    def inventory_index(self, request, inventory_slug):
         inventory = get_object_or_404(UserInventory, slug=inventory_slug)		
         return self.render(
             request,
-            template='inventory/user_inventory_index.html',
+            template='inventory/index/index_page.html',
             context_overrides=inventory.get_template_context_data()
         )
 
-    @path('<slug:inventory_slug>/add/')
-    def add_user_category(self, request, inventory_slug):
-        pass
+    @path('<slug:inventory_slug>/delete/')
+    def delete_inventory_index(self, request, inventory_slug):
+        inventory = get_object_or_404(UserInventory, slug=inventory_slug)
+        if request.user != inventory.owner:
+            return PermissionDenied
+
+        if request.method == "POST":
+            form = DeletionConfirmationForm(request.POST)
+            if form.is_valid():
+                inventory.delete()
+                site = Site.find_for_request(request)
+                if site is not None:
+                    home_page_url = site.root_page.url
+                else:
+                    home_page_url = reverse('/')
+                return HttpResponseRedirect(home_page_url)
+        else:
+            form = DeletionConfirmationForm()
+
+        return self.render(
+            request,
+            template='inventory/index/delete_index.html',
+            context={
+                'inventory': inventory
+            }
+        )
+
+    @path('<slug:inventory_slug>/update/')
+    def update_inventory_index(self, request, inventory_slug):
+        inventory = get_object_or_404(UserInventory, slug=inventory_slug)
+        if request.user != inventory.owner:
+            return PermissionDenied
+
+        if request.method == "POST":
+            form = UserInventoryForm(request.POST, instance=inventory)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(inventory.get_url())
+        else:
+            form = UserInventoryForm(instance=inventory)
+        
+        return self.render(
+            request,
+            template='inventory/index/update_index.html',
+            context={
+                'form': form,
+                'inventory': inventory
+            }
+        )
+
+    @path('<slug:inventory_slug>/add-category/')
+    def add_inventory_category(self, request, inventory_slug):
+        inventory = get_object_or_404(UserInventory, slug=inventory_slug)
+        if request.user != inventory.owner:
+            return PermissionDenied
+
+        if request.method == "POST":
+            form = InventoryCategoryForm(request.POST)
+            if form.is_valid():
+                category = form.save(commit=False)
+                category.inventory = inventory
+                category.save()
+                return HttpResponseRedirect(inventory.get_url())
+        else:
+            form = InventoryCategoryForm()
+        
+        return self.render(
+            request,
+            template='inventory/index/add_category.html',
+            context={'form': form}
+        )
 
     class Meta:
         verbose_name = _('homepage')
--- digidex/inventory/forms.py
+++ digidex/inventory/forms.py
@@ -7,21 +7,83 @@ class UserInventoryForm(forms.ModelForm):
     class Meta:
         model = UserInventory
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'text-field base-input',
+                    'placeholder': 'Enter a name for the inventory'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'text-field textarea',
+                    'placeholder': "Enter the inventory's description"
+                }
+            ),
+        }
+
 
 
 class InventoryCategoryForm(forms.ModelForm):
     class Meta:
         model = InventoryCategory
         fields = ['name', 'description']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'text-field base-input',
+                    'placeholder': 'Enter a name for the category'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'text-field textarea',
+                    'placeholder': "Enter the category's description"
+                }
+            ),
+        }
+
 
 
 class InventoryAssetForm(forms.ModelForm):
     class Meta:
         model = InventoryAsset
         fields = ['name', 'category','description']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'text-field base-input',
+                    'placeholder': 'Enter a name for the asset'
+                }
+            ),
+            'category': forms.Select(
+                attrs={
+                    'class': 'select-field base-input',
+                    'placeholder': 'Select a category'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'text-field textarea',
+                    'placeholder': "Enter the asset's description"
+                }
+            ),
+        }
+
 
     def __init__(self, *args, **kwargs):
         inventory = kwargs.pop('inventory', None)
         super().__init__(*args, **kwargs)
         if inventory:
             self.fields['category'].queryset = InventoryCategory.objects.filter(inventory=inventory)
+
+
+class DeletionConfirmationForm(forms.Form):
+    confirmation = forms.BooleanField(
+        required=True,
+        widget=forms.CheckboxInput(
+            attrs={
+                'class': 'base-radio',
+            }
+        )
+    )
