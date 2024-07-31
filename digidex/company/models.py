688e14b7df3adfe66a0bc2a069eabce691dde278
Raphael <rafael.e.puello@gmail.com>
2024-07-31 17:27:19 -0600

    Refactor inventory templates to use consistent naming conventions and update asset and index templates

--- digidex/company/models.py
+++ digidex/company/models.py
@@ -1,9 +1,11 @@
+from django import forms
 from django.db import models
 from django.contrib.auth import get_user_model
 
 from modelcluster.fields import ParentalManyToManyField
 from wagtail.models import Page
-from wagtail.admin.panels import FieldPanel
+from wagtail.images import get_image_model_string
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.snippets.models import register_snippet
 
 
@@ -22,9 +24,17 @@ class TeamMember(models.Model):
         max_length=255,
         blank=True
     )
+    image = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     panels = [
         FieldPanel('user'),
+        FieldPanel('role'),
     ]
 
     def __str__(self):
@@ -37,8 +47,16 @@ class TeamMember(models.Model):
 class CompanyPage(Page):
     parent_page_types = ["home.HomePage"]
 
+    intro = models.CharField(
+        max_length=250,
+        blank=True
+    )
     team_members = ParentalManyToManyField(
         TeamMember,
         blank=True
     )
 
+    content_panels = Page.content_panels + [
+        FieldPanel('intro'),
+        FieldPanel('team_members', widget=forms.CheckboxSelectMultiple),
+    ]
