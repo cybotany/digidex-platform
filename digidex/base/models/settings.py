643c8afb67882e380520f425f07c9caa76a4c51a
Raphael <rafael.e.puello@gmail.com>
2024-08-01 16:49:08 -0600

    Refactor navigation settings model and migrations

--- digidex/base/models/settings.py
+++ digidex/base/models/settings.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from wagtail.images import get_image_model
+from wagtail.images import get_image_model_string
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 
@@ -8,35 +8,51 @@ from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 @register_setting
 class NavigationSettings(BaseGenericSetting):
     logo = models.ForeignKey(
-        get_image_model(),
+        get_image_model_string(),
         on_delete=models.CASCADE,
+        blank=True,
+        null=True,
         related_name='+'
     )
     chat_link = models.URLField(
-        blank=True
+        blank=True,
+        null=True
     )
     email_address = models.EmailField(
-        blank=True
+        blank=True,
+        null=True
     )
     phone_number = models.CharField(
-        max_length=255,
-        blank=True
+        max_length=15,
+        blank=True,
+        null=True
     )
     twitter_url = models.URLField(
         verbose_name="Twitter URL",
-        blank=True
+        blank=True,
+        null=True
     )
     github_url = models.URLField(
         verbose_name="GitHub URL",
-        blank=True
+        blank=True,
+        null=True
     )
     mastodon_url = models.URLField(
         verbose_name="Mastodon URL",
-        blank=True
+        blank=True,
+        null=True
     )
 
     panels = [
         FieldPanel("logo"),
+        MultiFieldPanel(
+            [
+                FieldPanel("chat_link"),
+                FieldPanel("email_address"),
+                FieldPanel("phone_number"),
+            ],
+            "Contact settings",
+        ),
         MultiFieldPanel(
             [
                 FieldPanel("twitter_url"),
