72bfa4cf1d57eced4113e0c78fd503163c5a84d3
Raphael <rafael.e.puello@gmail.com>
2024-08-01 16:20:31 -0600

    Refactor templates to use consistent naming conventions and update asset and index templates

--- digidex/company/models.py
+++ digidex/company/models.py
@@ -35,6 +35,7 @@ class TeamMember(models.Model):
     panels = [
         FieldPanel('user'),
         FieldPanel('role'),
+        FieldPanel('image'),
     ]
 
     def __str__(self):
@@ -51,12 +52,71 @@ class CompanyIndexPage(Page):
         max_length=250,
         blank=True
     )
+    our_mission = models.TextField(
+        blank=True
+    )
+    our_vision = models.TextField(
+        blank=True
+    )
+    our_values = models.TextField(
+        blank=True
+    )
+    team_subtitle = models.CharField(
+        max_length=255,
+        blank=True
+    )
+    team_heading = models.CharField(
+        max_length=255,
+        blank=True
+    )
     team_members = ParentalManyToManyField(
         TeamMember,
         blank=True
     )
 
+    def get_body_header(self):
+        return {
+            'title': self.title,
+            'intro': self.intro if self.intro else 'Needs to be defined'
+        }
+
+    def get_about_us(self):
+        return {
+            'mission': self.our_mission if self.our_mission else 'Needs to be defined',
+            'vision': self.our_vision if self.our_vision else 'Needs to be defined',
+            'values': self.our_values if self.our_values else 'Needs to be defined'
+        }
+
+    def get_our_team(self):
+        return {
+            'subtitle': self.team_subtitle if self.team_subtitle else 'The Team',
+            'heading': self.team_heading if self.team_heading else 'Meet the people behind the company',
+            'members': self.team_members.all()
+        }
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['header'] = self.get_body_header()
+        context['about'] = self.get_about_us()
+        context['team'] = self.get_our_team()
+        return context
+
     content_panels = Page.content_panels + [
         FieldPanel('intro'),
-        FieldPanel('team_members', widget=forms.CheckboxSelectMultiple),
+        MultiFieldPanel(
+            [
+                FieldPanel('our_mission'),
+                FieldPanel('our_vision'),
+                FieldPanel('our_values'),
+            ],
+            heading="About Us Section"
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('our_mission'),
+                FieldPanel('our_vision'),
+                FieldPanel('team_members', widget=forms.CheckboxSelectMultiple),
+            ],
+            heading="Our Team Section"
+        ),
     ]
