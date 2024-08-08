15703f292d5cefc345330c4be735726cafbf0e8d
Raphael <rafael.e.puello@gmail.com>
2024-08-08 12:37:32 -0600

    `Add label field to NearFieldCommunicationTag model`

--- digidex/inventory/wagtail_hooks.py
+++ digidex/inventory/wagtail_hooks.py
@@ -6,15 +6,37 @@ from inventory.models import NearFieldCommunicationTag
 
 class NearFieldCommunicationTagViewSet(SnippetViewSet):
     model = NearFieldCommunicationTag
+    icon = "tag"
+    menu_label = "NFC Tags"
+    menu_name = "ntags"
+    menu_order = 300
+    url_namespace = "ntag_views"
+    url_prefix = "ntags"
+    add_to_admin_menu = True
+    list_filter = {
+        "label": ["icontains"],
+        "type": ["exact"],
+        "form": ["exact"]
+    }
 
     panels = [
         MultiFieldPanel([
+            FieldPanel("owner"),
             FieldPanel("label"),
             FieldPanel("type"),
             FieldPanel("form"),
         ], heading="NFC Tag Information"),
         InlinePanel("records", label="NFC Tag Records"),
     ]
+
+    def get_queryset(self, request):
+        """
+        Filter the queryset to only show instances where the owner is the current user.
+        """
+        qs = super().get_queryset(request)
+        if qs is None:
+            qs = self.model.objects.all()
+        return qs.filter(owner=request.user)
 
 
 register_snippet(NearFieldCommunicationTagViewSet)
